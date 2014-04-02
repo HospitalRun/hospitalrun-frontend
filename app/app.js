49f40b5ef690cc857bf2b7afda936eef2219d983
John Kleinschmidt <jk@cure.org>
2014-04-02 17:21:21 -0400

    Added logic to sync config db down to local pouchDB

--- app/app.js
+++ app/app.js
@@ -3,22 +3,28 @@ import CouchAuth from "hospitalrun/utils/couch-auth";
 import CouchSerializer from "hospitalrun/utils/couch-serializer";
 
 var App = Ember.Application.extend({
-  LOG_ACTIVE_GENERATION: true,
-  LOG_MODULE_RESOLVER: true,
-  LOG_TRANSITIONS: true,
-  LOG_TRANSITIONS_INTERNAL: true,
-  LOG_VIEW_LOOKUPS: true,
-  modulePrefix: 'hospitalrun', // TODO: loaded via config
-  Resolver: Resolver['default'],
+    LOG_ACTIVE_GENERATION: true,
+    LOG_MODULE_RESOLVER: true,
+    LOG_TRANSITIONS: true,
+    LOG_TRANSITIONS_INTERNAL: true,
+    LOG_VIEW_LOOKUPS: true,
+    modulePrefix: 'hospitalrun', // TODO: loaded via config
+    Resolver: Resolver['default']
 });
 
 App.initializer({
-  name: 'authentication',
-  initialize: function(container, application) {
-    container.register('authenticators:custom', CouchAuth);
-    application.register('serializer:couchdb', CouchSerializer);
-    Ember.SimpleAuth.setup(container, application);
-  }
+    name: 'authentication',
+    initialize: function(container, application) {
+        container.register('authenticators:custom', CouchAuth);
+        application.register('serializer:couchdb', CouchSerializer);
+        Ember.SimpleAuth.setup(container, application);
+        application.deferReadiness();
+        PouchDB.replicate(document.location.href+'/db/config','config', {
+            complete: function() {
+                application.advanceReadiness();
+            }
+        });
+    }
 });
 
 export default App;
