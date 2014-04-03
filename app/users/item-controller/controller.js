42f0c4ac3c98955619a0a08dc3c367b0409b4c7a
John Kleinschmidt <jk@cure.org>
2014-04-03 17:18:29 -0400

    Removed unneeded code.

--- app/users/item-controller/controller.js
+++ app/users/item-controller/controller.js
@@ -4,27 +4,6 @@ export default Ember.ObjectController.extend({
     
     isEditing: false,
     
-    userRoles: [
-        {name: "Admin", roles: [
-            'admin', 'user'
-        ]},
-        {name: "Business Office", roles: [
-            'business_office', 
-            'user']
-        },
-        {name: "Data Entry", roles: [
-            'data_entry',
-            'user'
-        ]},
-        {name: "Doctor", roles: ['doctor', 'user']},
-        {name: "Hospital Manager", roles: ['hospital_manager', 'user']},
-        {name: "Imaging Technician", roles: ['imaging_tech', 'user']},
-        {name: "Lab Technician", roles: ['lab_tech', 'user']},
-        {name: "Medical Records Officer", roles: ['medrec_officer', 'user']},
-        {name: "Nurse", roles: ['nurse', 'user']},
-        {name: "Patient Administration", roles: ['patient_admin', 'user']},
-        {name: "Pharmacist", roles: ['pharmacist', 'user']},
-    ],
     actions: {
         cancelUpdate: function() {
             this.set('isEditing', false);
