3c1a691dbeba1cbe821a50787c8d64279b10a58a
John Kleinschmidt <jk@cure.org>
2014-04-04 17:17:11 -0400

    Added logic to use google_auth or regular login.
    Google auth is not complete (yet).

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,29 @@
 var ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin, {
+    actions: {
+        authenticateSession: function() {
+            console.log("in authsession, currently this:");
+            console.dir(this);            
+            if (document.location.href.indexOf("?") > 0) {
+                console.log("HEY SOMEONE SENT US A GIFT:"+document.location.href);                
+                this.get('session').authenticate('app:authenticators:custom', {query:document.location.search});
+            } else {
+                this.store.find('config','use_google_auth').then(function(record) {
+                    console.log("Found value:");
+                    console.log(record.get('value'));
+                    if (record.get('value')) {
+                        console.log("go use google auth");
+                        //Redirect to the google auth login
+
+                    } else {
+                        console.log("use traditional auth");
+                        this._super();
+                    }
+
+                    window.location.replace('/auth/google');
+                });
+            }
+        }
+    }
 });
 
 export default ApplicationRoute;
