5653b4b9c933b50900e13bb1da00d3c083d131ff
John Kleinschmidt <jk@cure.org>
2014-04-04 17:16:31 -0400

    Removed unneeded code.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,4 @@
-var IndexRoute = Ember.Route.extend({
-    model: function() {
-        return this.store.find('config');
-    },
-    afterModel: function(configs) {        
-        var use_google_auth = configs.findBy('id', 'use_google_auth').get('value');
-        if (use_google_auth) {
-            alert('google auth');            
-        } else {
-           alert('NOT google auth');
-        }
-    }
+var IndexRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
 });
 
 export default IndexRoute;
