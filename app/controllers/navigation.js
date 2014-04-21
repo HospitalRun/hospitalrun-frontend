95f26d08337e3f6a1e6a2c18154c562d99c306ae
John Kleinschmidt <jk@cure.org>
2014-04-21 11:08:33 -0400

    Added navigation to app.

---
+++ app/controllers/navigation.js
@@ -0,0 +1,15 @@
+export default Ember.Controller.extend({
+    allowSearch: false,
+    searchRoute: null,    
+    actions: {
+        search: function() {
+            if (this.allowSearch && this.searchRoute) {
+                this.transitionToRoute(this.searchRoute,  {
+                    queryParams: {
+                        searchText: this.searchText
+                    }
+                });
+            }
+        }
+    }
+});
