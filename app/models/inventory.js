703063b20c25d752a184cab267b69c25b9849e6c
John Kleinschmidt <jk@cure.org>
2014-04-21 11:11:36 -0400

    Added crossreference id to inventory model.

--- app/models/inventory.js
+++ app/models/inventory.js
@@ -2,7 +2,8 @@ var Inventory = DS.Model.extend({
     description: DS.attr('string'),
     keywords: DS.attr(),
     name: DS.attr('string'),
-    quantity: DS.attr('number')
+    quantity: DS.attr('number'),
+    crossreference: DS.attr('string')
 });
 
 export default Inventory;
