d1fed9e3f682b662b964049872bb0307c2098bab
John Kleinschmidt <jk@cure.org>
2014-04-21 12:20:16 -0400

    Added navigation to app.

---
+++ app/views/application.js
@@ -0,0 +1,8 @@
+export default Ember.View.extend({
+  currentPathDidChange: function() {
+    Ember.run.next( this, function() {
+      this.$("ul.nav li:has(>a.active)").addClass('active');
+      this.$("ul.nav li:not(:has(>a.active))").removeClass('active');
+    });
+  }.observes('controller.currentPath')
+});
