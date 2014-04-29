66aac88880db06725f48063829c5adafb0a06893
John Kleinschmidt <jk@cure.org>
2014-04-29 16:52:02 -0400

    Removed logging.

--- app/components/smart-prescription.js
+++ app/components/smart-prescription.js
@@ -1,8 +1,6 @@
 export default Ember.TextArea.extend(Ember.TargetActionSupport, {
     valueDidChange: Ember.observer('value', function() {
         var value = Ember.get(this, 'value');        
-        console.log("Value changed: ",value);
-        console.log("Value #2 ", this.get('value'));
         this.triggerAction({
             action: 'search'            
         });
