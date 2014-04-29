763e1c8bce7775c640040669d9daf2c64481f8a6
John Kleinschmidt <jk@cure.org>
2014-04-29 16:35:51 -0400

    Removed unused files

--- tests/test_loader.js
+++
@@ -1,6 +0,0 @@
-// TODO: load based on params
-Ember.keys(requirejs._eak_seen).filter(function(key) {
-  return (/\-test/).test(key);
-}).forEach(function(moduleName) {
-  require(moduleName, null, null, true);
-});
