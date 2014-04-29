e2468e3ead5bca9743f87b82c9f9820d7c4d6483
John Kleinschmidt <jk@cure.org>
2014-04-29 17:11:02 -0400

    Upgraded ember-cli to 0.0.25

---
+++ tests/helpers/start-app.js
@@ -0,0 +1,27 @@
+var Application = require('hospitalrun/app')['default'];
+var Router = require('hospitalrun/router')['default'];
+
+export default function startApp(attrs) {
+  var App;
+
+  var attributes = Ember.merge({
+    // useful Test defaults
+    rootElement: '#ember-testing',
+    LOG_ACTIVE_GENERATION:false,
+    LOG_VIEW_LOOKUPS: false
+  }, attrs); // but you can override;
+
+  Router.reopen({
+    location: 'none'
+  });
+
+  Ember.run(function(){
+    App = Application.create(attributes);
+    App.setupForTesting();
+    App.injectTestHelpers();
+  });
+
+  App.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
+
+  return App;
+}
