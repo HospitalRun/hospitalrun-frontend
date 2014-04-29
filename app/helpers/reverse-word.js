ebb9758253866a2546576a7abfc1c6e3985307b0
John Kleinschmidt <jk@cure.org>
2014-04-29 16:35:06 -0400

    Removed unused helper

--- app/helpers/reverse-word.js
+++
@@ -1,8 +0,0 @@
-// Please note that Handlebars helpers will only be found automatically by the
-// resolver if their name contains a dash (reverse-word, translate-text, etc.)
-// For more details: http://stefanpenner.github.io/ember-app-kit/guides/using-modules.html
-
-export default Ember.Handlebars.makeBoundHelper(function(word) {
-  return word.split('').reverse().join('');
-});
-
