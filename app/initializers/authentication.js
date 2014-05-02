3411c12eb86c446ade41272cbb64e44c365a9320
John Kleinschmidt <jk@cure.org>
2014-05-02 15:06:06 -0400

    Changed back to ember-simple-auth 0.2.1 because of problems with newer ember-simple-auth.

--- app/initializers/authentication.js
+++ app/initializers/authentication.js
@@ -7,10 +7,9 @@ export default {
     
     initialize: function(container, application) {
         container.register('authenticators:custom', CustomAuth);
-        container.register('authorizer:custom', CustomAuthorizer);
         application.register('serializer:couchdb', CouchSerializer);        
         Ember.SimpleAuth.setup(container, application, {
-            authorizerFactory: 'authorizer:custom'
+            authorizer: CustomAuthorizer
         });
     }    
 };
