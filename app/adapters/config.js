aa65c1893a986d642a965e51bc0fe2b2c4f9e819
John Kleinschmidt <jk@cure.org>
2014-05-06 15:07:44 -0400

    Fixed saving oauth configs.

--- app/adapters/config.js
+++ app/adapters/config.js
@@ -1,4 +1,47 @@
-export default DS.PouchDBAdapter.extend({
-    namespace: 'hospitalrun-config',
-    databaseName: 'config'
+import PouchAdapterUtils from "hospitalrun/mixins/pouch-adapter-utils";
+
+export default DS.PouchDBAdapter.extend(PouchAdapterUtils, {
+    databaseName: 'config',
+    
+    _mapQuery: function(doc, emit) {
+        if (doc._id) {
+            emit(doc._id, null);
+        }
+    },
+
+    findQuery: function(store, type, query, options) {
+        if (!query.exactKeys) {
+            return this._super(store, type, query, options);
+        } else {
+            var self = this,
+            keys = [],
+            queryKeys = [],
+            queryParams = {
+                reduce: false,
+                include_docs: true
+            };
+            
+            queryParams.keys = query.exactKeys.map(function(key) {
+                return self._idToPouchId(key, 'config');
+            });
+            return new Ember.RSVP.Promise(function(resolve, reject){
+                self._getDb().then(function(db){
+                    try {
+                        db.query(self._mapQuery, queryParams, function(err, response) {
+                            if (err) {
+                                self._pouchError(reject)(err);
+                            } else {
+                                if (response.rows) {
+                                    var data = Ember.A(response.rows).mapBy('doc');
+                                    Ember.run(null, resolve, data);
+                                }
+                            }
+                        });
+                    } catch (err){
+                        self._pouchError(reject)(err);
+                    }
+                }, self._pouchError(reject));
+            }, "findQuery in config-pouchdb-adapter");
+        }
+    }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,68 @@
 var ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin, {
     use_google_auth: false,
+    
+    oauth_config_keys: [
+        'consumer_key',
+        'consumer_secret',
+        'oauth_token',
+        'token_secret'
+    ],
+
+    _get_oauth_configs: function() {
+        var configRecords = {},
+            route = this,
+            store = this.store;            
+        return new Ember.RSVP.Promise(function(resolve, reject){
+            store.find('config',{exactKeys: route.oauth_config_keys}).then(function(records) {
+                Ember.run(null, resolve, records);
+            },function(error){
+                Ember.run(null, reject, error);
+            });
+        });
+    },
+        
+    _save_oauth_config: function(queryParams) {        
+        var store = this.store,
+            route = this,
+            configRecord,
+            configValue;
+        
+        this._get_oauth_configs().then(function(records) {
+            route.oauth_config_keys.forEach(function(key) {
+                switch (key) {
+                    case 'consumer_key': {
+                        configValue = queryParams.k;
+                        break;
+                    }
+                    case 'consumer_secret': {
+                        configValue = queryParams.s1;
+                        break;
+                    }
+                    case 'oauth_token': {
+                        configValue = queryParams.t;
+                        break;
+                    }
+                    case 'token_secret': {
+                        configValue = queryParams.s2;
+                        break;
+                    }
+                }
+
+                configRecord = records.findBy('id', key);
+                if (!configRecord) {
+                    configRecord = store.createRecord('config', {
+                        id: key,
+                        value: configValue
+                    });
+                } else {
+                    configRecord.set('value', configValue);
+                }
+
+                configRecord.save();                            
+
+            });
+        });
+    },
 
     actions: {
         authenticateSession: function() {
@@ -17,65 +80,7 @@ var ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin
                 google_auth: true,
                 params: params.queryParams,
             });
-            var store = this.store,
-                route = this,
-                config_keys = [
-                    'consumer_key',
-                    'consumer_secret',
-                    'oauth_token',
-                    'token_secret'
-                ];
-
-            store.findByIds('config',config_keys).then(function(records) {
-                records.forEach(function(record) {
-                    console.log("record is:"+record,record);
-                    var key = record.id;
-                    switch (key) {
-                        case 'consumer_key': {
-                            record.set('value', params.queryParams.k);
-                            break;
-                        }
-                    }
-                });
-                
-/*            var configRecord = store.createRecord('config', {
-                id: 'consumer_key',
-                value: params.queryParams.k
-            });
-            configRecord.save().then(function(){
-                console.log("HEY SAVED consumer_key");
-                configRecord = store.createRecord('config', {
-                    id: 'consumer_secret',
-                    value: params.queryParams.s1
-                });
-                configRecord.save().then(function(){
-                    console.log("HEY SAVED consumer_secret");
-                    configRecord = store.createRecord('config', {
-                        id: 'oauth_token',
-                        value: params.queryParams.t
-                    });
-                    configRecord.save().then(function(){
-                        console.log("HEY SAVED oauth_token");
-                        configRecord = store.createRecord('config', {
-                            id: 'token_secret',
-                            value: params.queryParams.s2
-                        });
-                        configRecord.save();                        
-                    }, function(reason){
-                        console.log("HEY could not save oauth_token",reason);
-                    });
-
-                }, function(reason){
-                    console.log("HEY could not save consumer_secret",reason);
-                });
-            }, function(reason){
-                console.log("HEY could not save consumer_key",reason);
-            });            
-  */              
-            },function(error){
-                console.log("error",error);
-            });
-            
+            this._save_oauth_config(params.queryParams);
         } else {
             return this.store.find('config');
         }
