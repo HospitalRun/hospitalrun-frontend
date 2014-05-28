a0aa8256e62530e74fe27dc309d2d39076240bdf
John Kleinschmidt <jk@cure.org>
2014-05-28 11:43:20 -0400

    Moved navigation controller from model function to setupController since model doesn't always fire.

--- app/inventory/route.js
+++ app/inventory/route.js
@@ -1,8 +1,12 @@
 var InventoryRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
     model: function() {
+        return this.store.find('inventory');
+    },
+    
+    setupController: function(controller, model) { 
         this.controllerFor('navigation').set('allowSearch',true);
         this.controllerFor('navigation').set('searchRoute','inventory.search');
-        return this.store.find('inventory');
+        this._super(controller, model);
     }
 });
 
