390a34be29d2be60f3a4c8c3289c86aed4c91417
John Kleinschmidt <jk@cure.org>
2014-06-03 16:24:38 -0400

    Fix for search

--- app/medication/route.js
+++ app/medication/route.js
@@ -19,7 +19,7 @@ export default Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
     
     model: function() {
         this.controllerFor('navigation').set('allowSearch',true);
-        this.controllerFor('navigation').set('searchRoute','medication.search');
+        this.controllerFor('navigation').set('searchRoute','/medication/search');
         return this.store.find('medication', {
             mapResults: this._mapViewResults,
             fieldMapping: this.fieldMapping
