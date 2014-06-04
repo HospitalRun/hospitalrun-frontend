61b56d8b3bf956f60926294d057e9ca7b267bfe4
John Kleinschmidt <jk@cure.org>
2014-06-04 09:37:37 -0400

    New abstract route for search.

---
+++ app/routes/abstract-search-route.js
@@ -0,0 +1,21 @@
+export default Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
+    searchKeys: null,    
+    searchModel: null,
+    searchText: null,
+    model: function(params) {
+        this.set('searchText', params.search_text);
+        var queryParams = {
+            containsValue: {
+                value: params.search_text,
+                keys: this.searchKeys                            
+            }
+        };
+        return this.store.find(this.searchModel, queryParams);
+    },    
+    
+    setupController: function(controller, model) {
+        controller.set('model', model);
+        controller.set('searchText', this.get('searchText'));
+    }    
+    
+});
