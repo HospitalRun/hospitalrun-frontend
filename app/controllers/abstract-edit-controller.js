41ce49b683207821ada7ae8d5a1c6cae0e4c27ee
John Kleinschmidt <jk@cure.org>
2014-06-04 09:36:45 -0400

    New abstract controller for editing items with a modal

---
+++ app/controllers/abstract-edit-controller.js
@@ -0,0 +1,44 @@
+export default Ember.ObjectController.extend({
+    isUpdateDisabled: function() {
+        if (!Ember.isNone(this.get('isValid'))) {
+            return !this.get('isValid');
+        } else {
+            return false;
+        }
+    }.property('isValid'),
+    
+    
+    buttonText: function() {
+        if (this.get('isNew')) {
+            return 'Add';
+        } else {
+            return 'Update';
+        }
+    }.property('isNew'),
+    
+    actions: {
+        cancel: function() {
+            var cancelledItem = this.get('model');
+            if (this.get('isNew')) {
+                cancelledItem.deleteRecord();
+            } else {
+                cancelledItem.rollback();
+            }
+            this.send('closeModal');
+        },
+        
+        update: function() {            
+            this.get('model').save().then(function(record) {
+                this.send('closeModal');
+                this.afterUpdate(record);
+            }.bind(this));                
+        }
+    },
+    
+    /**
+     * Override this function to perform logic after record update
+     * @param record the record that was just updated.
+     */
+    afterUpdate: function() {
+    }
+});
