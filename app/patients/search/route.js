48806d9ffcade787e51308c59c604fd1feeaa788
John Kleinschmidt <jk@cure.org>
2014-06-04 16:01:26 -0400

    Changed to use abstract classes.

--- app/patients/search/route.js
+++ app/patients/search/route.js
@@ -1,28 +1,9 @@
-export default Ember.Route.extend({
-    actions: {
-        allPatients: function() {
-            this.transitionTo('patients');
-        }
-    },
-    
-    searchText: null,
-    model: function(params) {
-        this.set('searchText', params.search_text);
-        var queryParams = {
-            containsValue: {
-                value: params.search_text,
-                keys: [
-                '_id',
-                'firstName',
-                'lastName'
-            ]},                            
-        };
-        return this.store.find('patient', queryParams);
-    },    
-    
-    setupController: function(controller, model) {
-        controller.set('model', model);
-        controller.set('searchText', this.get('searchText'));
-    }    
-    
+import AbstractSearchRoute from 'hospitalrun/routes/abstract-search-route';  
+export default AbstractSearchRoute.extend({
+    moduleName: 'patients',
+    searchKeys: [
+        'firstName',
+        'lastName'
+    ],
+    searchModel: 'patient'    
 });
