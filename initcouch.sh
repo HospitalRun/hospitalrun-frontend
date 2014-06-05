b1ed98672aafc4438658779e5986d033f7524133
John Kleinschmidt <jk@cure.org>
2014-06-05 12:58:03 -0400

    Changed to properly setup security on the DBs and to set the config for oauth. Also, removed the CORS stuff since we don't need it any more.

--- initcouch.sh
+++ initcouch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 HOST="http://127.0.0.1:5984"
+curl -X PUT $HOST/_users/_security -d '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": []}}'
 curl -X PUT $HOST/config
+curl -X PUT $HOST/config/_security -d '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": []}}'
 curl -X PUT $HOST/main
-curl -X PUT $HOST/_config/httpd/enable_cors -d '"true"'
-curl -X PUT $HOST/_config/cors/origins -d '"*"'
-curl -X PUT $HOST/_config/cors/credentials -d '"true"'
-curl -X PUT $HOST/_config/cors/methods -d '"GET, PUT, POST, HEAD, DELETE"'
+curl -X PUT $HOST/main/_security -d '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": ["user"]}}'
+curl -X PUT $HOST/_config/http/authentication_handlers -d '"{couch_httpd_oauth, oauth_authentication_handler}, {couch_httpd_auth, proxy_authentification_handler}, {couch_httpd_auth, cookie_authentication_handler}, {couch_httpd_auth, default_authentication_handler}"'
+curl -X PUT $HOST/_config/couch_httpd_oauth/use_users_db -d '"true"'
 curl -X PUT $HOST/_config/admins/hradmin -d '"test"'
 
