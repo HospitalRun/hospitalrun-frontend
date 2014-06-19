3526eca388540135f317e71c59778c0ecc086922
John Kleinschmidt <jk@cure.org>
2014-06-19 15:15:04 -0400

    Mixin to determine if update button should be disabled.

---
+++ app/mixins/is-update-disabled.js
@@ -0,0 +1,9 @@
+export default Ember.Mixin.create({
+    isUpdateDisabled: function() {
+        if (!Ember.isNone(this.get('isValid'))) {
+            return !this.get('isValid');
+        } else {
+            return false;
+        }
+    }.property('isValid')
+});
