241953ad1c0ab4ab9aff95e546034c90d49d9a0c
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-07-08 16:48:58 -0400

    New typeahead for inventory items

---
+++ app/components/inventory-typeahead.js
@@ -0,0 +1,23 @@
+import TypeAhead from "hospitalrun/components/type-ahead";
+export default TypeAhead.extend({
+    displayKey: 'name', 
+    setOnBlur: true,
+    
+    mappedContent: function() {
+        var content = this.get('content'),
+            mapped;
+        if (content) {
+            if (content instanceof DS.RecordArray) {
+                mapped = content.map(function(item) {
+                    var returnObj = {};
+                    returnObj.name = '%@ (%@ available)'.fmt(item.get('name'), item.get('quantity'));
+                    returnObj[this.get('selectionKey')] = item;
+                    return returnObj;
+                }.bind(this));                
+            }
+            return mapped;
+        } else {
+            return [];
+        }        
+    }.property('content')
+});
