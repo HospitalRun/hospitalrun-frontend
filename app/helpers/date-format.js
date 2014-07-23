5bad09017673ee9327603e01b9e66ea217a11d74
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-07-23 16:24:12 -0400

    Added support to specify date format

--- app/helpers/date-format.js
+++ app/helpers/date-format.js
@@ -1,3 +1,9 @@
-export default Ember.Handlebars.makeBoundHelper(function(date) {
-    return moment(date).format('l');
+export default Ember.Handlebars.makeBoundHelper(function(date, options) {
+    if (!Ember.isEmpty(date)) {
+        var dateFormat = 'l';
+        if (options && options.hash.format) {
+            dateFormat = options.hash.format;
+        }
+        return moment(date).format(dateFormat);
+    }
 });
