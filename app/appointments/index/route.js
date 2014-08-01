4f2257e04410056f1209176d6f665527e008f0a1
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-08-01 15:55:20 -0400

    Start of creating visit from appointment

--- app/appointments/index/route.js
+++ app/appointments/index/route.js
@@ -1,4 +1,15 @@
 import AbstractIndexRoute from 'hospitalrun/routes/abstract-index-route';
 export default AbstractIndexRoute.extend({
-    pageTitle: 'Upcoming'
+    pageTitle: 'Upcoming',
+    
+    actions: {
+        createVisit: function(appointment) {
+            var visitProps = appointment.getProperties('startDate', 'endDate', 'location', 'patient'),
+                visit;            
+            visitProps.visitType = appointment.get('appointmentType');
+            visitProps.examiner = appointment.get('provider');
+            visit = this.get('store').createRecord('visit', visitProps);
+            this.transitionTo('visits.edit', visit);
+        }
+    }
 });
