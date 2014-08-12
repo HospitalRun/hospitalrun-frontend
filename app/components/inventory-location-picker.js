12dfcf0d85ddd0029cfa05542fc4c2b706453cec
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-08-12 13:20:53 -0400

    New inventory location picker
    Picker to allow spanning an inventory request over multiple locations
    for the specified quantity

---
+++ app/components/inventory-location-picker.js
@@ -0,0 +1,71 @@
+export default Ember.Component.extend({
+    doingSetup: false,
+    label: null,
+    locationPickers: null,
+    locationList: null,    
+    quantityRequested: null,
+    
+    locationChange: function() {    
+        var doingSetup = this.get('doingSetup'),
+            locationList = this.get('locationList'),
+            locationPickers = this.get('locationPickers'),
+            quantityRequested = this.get('quantityRequested'),
+            quantitySatisfiedIdx = -1;
+        if (!doingSetup) {
+            locationPickers.reduce(function(previousValue, item, index) {
+                var selectedLocation = item.get('selectedLocation'),
+                    returnValue;
+                if (Ember.isEmpty(selectedLocation)) {                
+                    returnValue = previousValue;                
+                } else {
+                    returnValue = (previousValue + selectedLocation.get('quantity'));
+                }
+                if (quantitySatisfiedIdx === -1 && returnValue >= quantityRequested) {
+                    quantitySatisfiedIdx = index;
+                }
+                return returnValue;
+            },0);
+            if (quantitySatisfiedIdx > -1 ) {
+                if (locationPickers.get('length') > (quantitySatisfiedIdx + 1)) {
+                    locationPickers = locationPickers.slice(0, quantitySatisfiedIdx+1);
+                    this.set('locationPickers', locationPickers);
+                }
+            } else {            
+                locationPickers.addObject(Ember.Object.create());
+            }        
+            this._setupLocationPickers(locationPickers, locationList);
+        }
+    },
+    
+    _setup: function() {
+        var locationList = this.get('locationList'),
+            locationPickers = [],
+            quantityRequested = this.get('quantityRequested');
+        this.set('doingSetup', true);
+        locationList.reduce(function(previousValue, location) {
+            if (previousValue < quantityRequested) {
+                locationPickers.addObject(Ember.Object.create());
+            }
+            return (previousValue + location.get('quantity'));
+        },0);
+        this._setupLocationPickers(locationPickers, locationList, true);
+        this.set('locationPickers', locationPickers);
+        this.set('doingSetup', false);
+    }.on('init'), 
+
+    
+    _setupLocationPickers: function(locationPickers, locationList, setInitialLocation) {
+        locationPickers.reduce(function(previousValue, item) {
+            var selectedLocation = item.get('selectedLocation');
+            item.set('subLocationList',previousValue);            
+            if (!previousValue.contains(selectedLocation) || setInitialLocation) {
+                item.set('selectedLocation', previousValue.get('firstObject'));
+            }
+            item.set('label', 'And');
+            return previousValue.filter(function(location) {
+                return (item.get('selectedLocation.id') !== location.get('id'));
+            });
+        }, locationList);
+        locationPickers.get('firstObject').set('label', this.get('label'));        
+    }
+});
