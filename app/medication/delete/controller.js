1caf60bc4fd0d76953cd81849a25948a247de49b
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-08-22 13:41:31 -0400

    Changed delete to make sure medication is removed from visit
    Remove from visit before deleting medication.

--- app/medication/delete/controller.js
+++ app/medication/delete/controller.js
@@ -1,4 +1,17 @@
 import AbstractDeleteController from 'hospitalrun/controllers/abstract-delete-controller';    
 export default AbstractDeleteController.extend({
-     title: 'Delete Request'
+    title: 'Delete Request',
+    
+    actions: {
+        delete: function() {
+            var visit = this.get('visit'),
+                medications = visit.get('medication');
+            medications.removeObject(this.get('model'));
+            visit.save().then(function() {
+                this.get('model').destroyRecord().then(function() {
+                    this.send('modalClose');
+                }.bind(this));
+            }.bind(this));
+        }
+    }
 });
