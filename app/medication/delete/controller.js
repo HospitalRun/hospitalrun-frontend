0a518c30f0940de6d954b70128a9d91005fd3f9a
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-08-25 16:01:11 -0400

    Fixed delete with new async medications

--- app/medication/delete/controller.js
+++ app/medication/delete/controller.js
@@ -4,12 +4,13 @@ export default AbstractDeleteController.extend({
     
     actions: {
         delete: function() {
-            var visit = this.get('visit'),
-                medications = visit.get('medication');
-            medications.removeObject(this.get('model'));
-            visit.save().then(function() {
-                this.get('model').destroyRecord().then(function() {                    
-                    this.send('closeModal');
+            var visit = this.get('visit');            
+            visit.get('medication').then(function(medications) {
+                medications.removeObject(this.get('model'));
+                visit.save().then(function() {
+                    this.get('model').destroyRecord().then(function() {                    
+                        this.send('closeModal');
+                    }.bind(this));
                 }.bind(this));
             }.bind(this));
         }
