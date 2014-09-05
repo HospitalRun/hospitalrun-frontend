4cf97867009403c93ee2ff8b430e4ec071d6203f
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-09-04 23:38:31 -0400

    updated permissions

--- app/mixins/user-session.js
+++ app/mixins/user-session.js
@@ -3,143 +3,255 @@ export default Ember.Mixin.create({
         appointments: [
             'Data Entry',
             'Hospital Manager',
+            'Medical Records Officer',
             'Patient Administration', 
             'Social Worker', 
             'System Administrator'
         ],
         add_appointment: [
             'Data Entry',
-            'Patient Administration',  'Social Worker', 'System Administrator'
+            'Medical Records Officer',
+            'Patient Administration',  
+            'Social Worker', 
+            'System Administrator'
         ],
         add_diagnosis: [
             'Data Entry',
-            'Doctor', 'Patient Administration', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Patient Administration', 
+            'System Administrator'
         ],
         add_medication: [
             'Data Entry',
-            'Doctor', 'Pharmacist', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Pharmacist', 
+            'System Administrator'
         ],
         add_patient: [
             'Data Entry',
-            'Patient Administration', 'Social Worker', 'Doctor', 'System Administrator'
+            'Doctor', 
+            'Medical Records Officer',
+            'Patient Administration', 
+            'Social Worker', 
+            'System Administrator'
         ],
         add_lab: [
             'Data Entry',
-            'Doctor', 'Lab Technician', 'System Administrator'
+            'Doctor', 
+            'Medical Records Officer',
+            'Lab Technician', 
+            'System Administrator'
         ],
         add_imaging: [
             'Data Entry',
-            'Doctor', 'Imaging Technician', 'System Administrator'
+            'Doctor', 
+            'Imaging Technician', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         add_inventory_request: [
             'Data Entry',
-            'Inventory Manager', 'Nurse Manager',
+            'Inventory Manager', 
+            'Medical Records Officer',
+            'Nurse Manager',
             'Pharmacist',
             'System Administrator'
         ],        
         add_inventory_item: [
             'Data Entry',
-            'Inventory Manager', 'System Administrator'
+            'Inventory Manager',
+            'Medical Records Officer',
+            'System Administrator'
         ],
         add_inventory_purchase: [
             'Data Entry',
-            'Inventory Manager', 'System Administrator'
+            'Inventory Manager', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         add_invoice: [
             'Data Entry',
-            'Business Office', 'System Administrator' 
+            'Business Office', 
+            'Medical Records Officer',
+            'System Administrator' 
         ],
         add_procedure: [
             'Data Entry',
-            'Doctor', 'Nurse', 'Nurse Manager', 'Patient Administration', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Nurse', 
+            'Nurse Manager', 
+            'Patient Administration', 
+            'System Administrator'
         ],
         add_user: [
             'System Administrator'
         ],
         add_visit: [
             'Data Entry',
-            'Doctor', 'Nurse', 'Nurse Manager', 'Patient Administration', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Nurse', 
+            'Nurse Manager', 
+            'Patient Administration', 
+            'System Administrator'
         ],        
         add_vitals: [
             'Data Entry',
-            'Doctor', 'Nurse', 'Nurse Manager', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Nurse', 
+            'Nurse Manager', 
+            'System Administrator'
         ],        
         adjust_inventory_location: [
-            'Inventory Manager', 'System Administrator'
+            'Inventory Manager',
+            'Medical Records Officer',
+            'System Administrator'
         ],        
         complete_imaging: [
-            'Imaging Technician', 'System Administrator'
+            'Imaging Technician', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         complete_lab: [
-            'Lab Technician', 'System Administrator'
+            'Lab Technician',
+            'Medical Records Officer',
+            'System Administrator'
         ],
         delete_appointment: [
-            'Patient Administration', 'Social Worker', 'System Administrator'
+            'Medical Records Officer',
+            'Patient Administration', 
+            'Social Worker', 
+            'System Administrator'
         ],
         delete_diagnosis: [
-            'Doctor', 'Patient Administration', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Patient Administration', 
+            'System Administrator'
         ],        
         delete_inventory_item: [
-            'Inventory Manager', 'System Administrator'
+            'Inventory Manager', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         delete_inventory_purchase: [
-            'Inventory Manager', 'System Administrator'
+            'Inventory Manager', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         delete_imaging: [
-            'Doctor', 'System Administrator'
+            'Doctor', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         delete_lab: [
-            'Doctor', 'System Administrator'
+            'Doctor', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         delete_medication: [
-            'Doctor', 'System Administrator'
+            'Doctor', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         delete_patient: [
-            'Patient Administration', 'System Administrator'
+            'Medical Records Officer',
+            'Patient Administration', 
+            'System Administrator'
         ],
         delete_procedure: [
-            'Doctor', 'Nurse', 'Nurse Manager', 'Patient Administration', 'System Administrator'
+            'Doctor', 
+            'Medical Records Officer',
+            'Nurse', 
+            'Nurse Manager', 
+            'Patient Administration', 
+            'System Administrator'
         ],
         delete_vitals: [
-            'Doctor', 'Nurse', 'Nurse Manager', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Nurse', 
+            'Nurse Manager', 
+            'System Administrator'
         ],
         delete_visit: [
-            'Doctor', 'Nurse', 'Nurse Manager', 'Patient Administration', 'System Administrator'
+            'Doctor',
+            'Medical Records Officer',
+            'Nurse', 
+            'Nurse Manager', 
+            'Patient Administration', 
+            'System Administrator'
         ],
         delete_user: [
             'System Administrator'
         ],
         fulfill_inventory: [
-            'Inventory Manager', 'System Administrator'
+            'Inventory Manager',
+            'Medical Records Officer',
+            'System Administrator'
         ],
         fulfill_medication: [
-            'Pharmacist', 'System Administrator'
+            'Medical Records Officer',
+            'Pharmacist', 
+            'System Administrator'
         ],
         imaging: [
             'Data Entry',
-            'Imaging Technician', 'Doctor', 'System Administrator'
+            'Doctor', 
+            'Hospital Manager',
+            'Imaging Technician', 
+            'Medical Records Officer',
+            'System Administrator'
         ],
         labs: [
             'Data Entry',
-            'Lab Technician', 'Doctor', 'System Administrator'
+            'Doctor', 
+            'Hospital Manager',
+            'Lab Technician',
+            'Medical Records Officer',
+            'System Administrator'
         ],
         medication: [
             'Data Entry',
-            'Pharmacist', 'Doctor', 'System Administrator'
+            'Doctor', 
+            'Hospital Manager',
+            'Medical Records Officer',
+            'Pharmacist', 
+            'System Administrator'
         ],
         inventory: [
             'Data Entry',
-            'Inventory Manager', 'Nurse Manager', 
+            'Hospital Manager',
+            'Inventory Manager',
+            'Medical Records Officer',
+            'Nurse Manager', 
             'Pharmacist',
             'System Administrator'
         ],
         patients: [
             'Data Entry',
-            'Doctor', 'Nurse', 'Nurse Manager', 'Patient Administration', 'Social Worker', 'System Administrator'
+            'Doctor', 
+            'Hospital Manager',
+            'Medical Records Officer',
+            'Nurse', 
+            'Nurse Manager', 
+            'Patient Administration', 
+            'Social Worker', 
+            'System Administrator'
         ],
         visits: [
             'Data Entry',
-            'Doctor', 'Nurse',  'Nurse Manager', 'Patient Administration', 'System Administrator'
+            'Doctor', 
+            'Hospital Manager',
+            'Medical Records Officer',
+            'Nurse Manager', 
+            'Nurse',  
+            'Patient Administration', 
+            'System Administrator'
         ],
         users: [
             'System Administrator'
