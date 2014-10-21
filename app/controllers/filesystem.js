5c065d19c40564a52a08725aab323c1eb774a8e3
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-10-21 17:16:22 -0400

    Move filer init to intializer; new functions
    added deleteFile and isFileSystemEnabled

--- app/controllers/filesystem.js
+++ app/controllers/filesystem.js
@@ -1,69 +1,65 @@
 export default Ember.Controller.extend({
-    filerPromise: null,
-    filer: null,
-    size: (1024*1024*1024*8), //8GB max size,
-    
+    filer: null, //Injected via initializer
     _onError: function(e) {
         console.log('Filer filesystem error: '+e);
     },
-            
-    /**
-     * Get the filer object and initialize it if it is not already initialized
-     * @param {Promise} returns a Promise that resolves once the filer object is initialized.
-     */
-    _getFiler: function() {
-        if(Ember.isEmpty(this.filer)) {
-            if (Ember.isEmpty(this.filerPromise)) {
-                this.filerPromise = new Ember.RSVP.Promise(function(resolve, reject){
-                    var size = this.get('size');
-                    this.filer = new Filer();
-                    this.filer.init({persistent: true, size: size}, function() {
-                        console.log("FILER HAS BEEN INITIED");
-                        resolve(this.filer);
-                    }.bind(this), function(error) {
-                        reject(error);
-                    });
-                }.bind(this));
-            }
-            return this.filerPromise;            
-        } else {
-            return Ember.RSVP.resolve(this.filer);
-        }
-        
-    },
-    
+
     /**
      * Add the specified file to the local filesystem
      * @param {File} file the file to save.
      * @param {String} path the file path to save the file to.
-     * @param {Promise} returns a Promise that resolves once the file is saved.
+     * @returns {Promise} returns a Promise that resolves once the file is saved.
      */
     addFile: function(file, path) {
         return new Ember.RSVP.Promise(function(resolve, reject){
             var currentDate = new Date(),
-                fileName = file.name || '',          
+                filer = this.get('filer'),
+                fileName = file.name || '',
                 newFileName = path+currentDate.getTime()+fileName;
+            if (Ember.isEmpty(filer)) {
+                reject('Local filesystem unavailable, please use Google Chrome browser');
+            }
             if (Ember.isEmpty(fileName) && !Ember.isEmpty(file.type)) {
                 var typeParts = file.type.split('/');
                 if (typeParts.length > 1) {
                     newFileName += '.'+ typeParts[1];
                 }
             }
-            this._getFiler().then(function(filer) {
-                filer.mkdir(path, false, function() {
-                    filer.write(newFileName, {data: file, type: file.type}, function(fileEntry) {
-                        console.log("SUCCESS SAVING FILE TO: "+newFileName);
-                        resolve(fileEntry);
-                    }, function(e) {
-                        reject(e);
-                    });
+            filer.mkdir(path, false, function() {
+                filer.write(newFileName, {data: file, type: file.type}, function(fileEntry) {
+                    resolve(fileEntry);
                 }, function(e) {
                     reject(e);
                 });
-            }.bind(this));
+            }, function(e) {
+                reject(e);
+            });
         }.bind(this));    
     },
     
+    /** 
+     * Delete the specified file
+     * @param {String} filePath path of file to delete.
+     * @returns {Promise} returns a Promise that resolves once the file is deleted.
+     */
+    deleteFile: function(filePath) {
+        return new Ember.RSVP.Promise(function(resolve, reject){
+            var filer = this.get('filer');
+            try {
+                filer.rm(filePath, function() {
+                    resolve();
+                }, reject);
+            } catch(ex) {
+                reject(ex);
+            }
+        }.bind(this));
+    },
+    
+    /**
+     * Convert specified file to a data url
+     * @param {File} file to convert
+     * @return {String} the data url for the file
+     */
     fileToDataURL: function(file) {
         return new Ember.RSVP.Promise(function(resolve){
             var reader = new FileReader();
@@ -72,5 +68,10 @@ export default Ember.Controller.extend({
             };
             reader.readAsDataURL(file);        
         });        
-    }
+    },
+    
+    isFileSystemEnabled: function() {
+        var filer = this.get('filer');
+        return !(Ember.isEmpty(filer));
+    }.property('filer')
 });
