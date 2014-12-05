eb8a6416528c466181024b77549c15a931060a03
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-12-05 13:49:02 -0500

    Finished visit report

--- app/patients/reports/controller.js
+++ app/patients/reports/controller.js
@@ -92,16 +92,19 @@ export default AbstractReportController.extend(VisitTypes, {
     reportTypes: [{
         name: 'Visit',
         value: 'visit'
-    }],    
-
-    _addTotalsRow: function(label, summaryCost, summaryQuantity) {
-        if (summaryQuantity > 0) {
-            this._addReportRow({
-                totalCost: label +  this.numberFormat(summaryCost),
-                quantity: label + this.numberFormat(summaryQuantity),
-                unitCost: label + this.numberFormat(summaryCost/summaryQuantity)
-            }, true);
-        }        
+    }],
+    
+    _addContactToList: function(phone, email, prefix, contactList) {
+        var contactArray = [];
+        if (!Ember.isEmpty(email) || !Ember.isEmpty(phone)) {
+            if (!Ember.isEmpty(phone)) {
+                contactArray.push(phone);
+            }                
+            if (!Ember.isEmpty(email)) {
+                contactArray.push(email);
+            }
+            contactList.push(prefix+contactArray.join(', '));
+        }
     },
     
     _contactListToString: function(patient) {
@@ -110,70 +113,54 @@ export default AbstractReportController.extend(VisitTypes, {
             contactDesc,
             contactList = [],
             email = patient.get('email'),
-            phone = patient.get('phone');            
-            if (!Ember.isEmpty(email) || !Ember.isEmpty(phone)) {
-                if (!Ember.isEmpty(phone)) {
-                    contactArray.push(phone);
-                }                
-                if (!Ember.isEmpty(email)) {
-                    contactArray.push(email);
-                }
-                contactList.push('Primary: '+contactArray.join(' , '));
-            }
-            if (!Ember.isEmpty(additionalContacts)) {
-                additionalContacts.forEach(function(contact) {
-                    contactArray = [];
-                    contactDesc = '';
-                    if (!Ember.isEmpty(contact.phone)) {
-                        contactArray.push(contact.phone);
-                    }
-                    if (!Ember.isEmpty(contact.email)) {
-                        contactArray.push(contact.email);
-                    }                    
-                    if (!Ember.isEmpty(contact.name) && !Ember.isEmpty(contact.relationship)) {
-                        if (!Ember.isEmpty(contact.name)) {
-                            contactDesc += contact.name;
-                        } 
-                        if (!Ember.isEmpty(contact.relationship)) {
-                            if (!Ember.isEmpty(contactDesc)) {
-                                contactDesc += ' - ';
-                            }
-                            contactDesc += contact.relationship;                            
+            phone = patient.get('phone');
+        this._addContactToList(phone, email, 'Primary: ', contactList);
+        if (!Ember.isEmpty(additionalContacts)) {
+            additionalContacts.forEach(function(contact) {
+                contactArray = [];
+                contactDesc = '';                   
+                if (!Ember.isEmpty(contact.name) && !Ember.isEmpty(contact.relationship)) {
+                    if (!Ember.isEmpty(contact.name)) {
+                        contactDesc += contact.name;
+                    } 
+                    if (!Ember.isEmpty(contact.relationship)) {
+                        if (!Ember.isEmpty(contactDesc)) {
+                            contactDesc += ' - ';
                         }
-                        contactDesc += ': ';
+                        contactDesc += contact.relationship;                            
                     }
-                    contactList.push(contactDesc+contactArray.join(' , '));
-                });
-            }
+                    contactDesc += ': ';
+                }
+                this._addContactToList(contact.phone, contact.email, contactDesc, contactList);
+            }.bind(this));
+        }
         return contactList.join(';\n');
     },
     
     _diagnosisListToString: function(diagnoses) {
-        var diagnosisList = '';
-        if (!Ember.isEmpty(diagnoses)) {
-            diagnoses.forEach(function(diagnosis, idx) {
-                if(idx > 0) {
-                    diagnosisList += ', ';
-                }
-                diagnosisList += diagnosis.description + '('+this._dateFormat(diagnosis.date)+')';
-            }.bind(this));
-        }
-        return diagnosisList;
+        return this._listToString(diagnoses, 'description', 'date');
     },
-
+    
     /**
-     * Filter the records by the specified date and the record's start and (optional) end dates.
+     * Filter the records by the specified dates and the record's start and (optional) end dates.
      * @param {Array} records to filter.
-     * @param {Date} the date to filter by.
      */
-    _filterByDate: function(records, dateToFilterBy) {
+    _filterByDate: function(records) {
+        var filterEndDate = this.get('endDate'),
+            filterStartDate = this.get('startDate');
+        if (Ember.isEmpty(filterStartDate)) {
+            return records;
+        }
         return records.filter(function(record) {
-            var endDate = record.get('endDate'),
-            startDate = record.get('startDate');        
-            var compareDate = moment(dateToFilterBy);
-            return ((Ember.isEmpty(endDate) || compareDate.isSame(endDate, 'day') || 
-                     compareDate.isBefore(endDate, 'day')) &&
-                    (Ember.isEmpty(startDate) || compareDate.isSame(startDate, 'day') || compareDate.isAfter(startDate, 'day')));
+            var recordEndDate,
+                recordStartDate = moment(record.get('startDate'));
+            if (!Ember.isEmpty(record.get('endDate'))) {
+                recordEndDate = moment(record.get('endDate'));
+            }
+            return (recordStartDate.isSame(filterStartDate, 'day') || 
+                    recordStartDate.isAfter(filterStartDate, 'day')) && 
+                (Ember.isEmpty(recordEndDate) || Ember.isEmpty(filterEndDate) || 
+                 recordEndDate.isSame(filterEndDate, 'day') || recordEndDate.isBefore(filterEndDate, 'day'));
         });
     },
     
@@ -202,6 +189,7 @@ export default AbstractReportController.extend(VisitTypes, {
         this.set('showReportResults', true);
         this._setReportHeaders();
         this._setReportTitle();
+        this._generateExport();
     },
     
     _generateVisitReport: function() {
@@ -211,14 +199,10 @@ export default AbstractReportController.extend(VisitTypes, {
                 'primaryDiagnosis','secondaryDiagnosis'
             ),
             visits = this.get('model');
+        visits = this._filterByDate(visits);
         for (var filter in visitFilters) {
             if (!Ember.isEmpty(visitFilters[filter])) {
                 switch (filter) {
-                    case 'visitDate': {
-                        //filter by visit date
-                        visits = this._filterByDate(visits, visitFilters[filter]);
-                        break;                    
-                    }
                     case 'diagnosis': {
                         visits = this._filterByLike(visits, 'diagnosisList',  visitFilters[filter]);
                         break;
@@ -242,24 +226,24 @@ export default AbstractReportController.extend(VisitTypes, {
         } else {
             this._finishVisitReport(visits);
         }
-
     },
     
     _haveLikeValue: function(valueToCompare, likeCondition) {
          return (valueToCompare.toLowerCase().indexOf(likeCondition.toLowerCase()) > -1);
     },
     
-    _procedureListToString: function(procedures) {
-        var procedureList = '';
-        if (!Ember.isEmpty(procedures)) {
-            procedures.forEach(function(procedure, idx) {
-                if(idx > 0) {
-                    procedureList += ', ';
-                }
-                procedureList += procedure.get('description') + '('+this._dateFormat(procedure.get('procedureDate'))+')';
+    _listToString: function(items, descField, dateField) {
+        var itemList = [];
+        if (!Ember.isEmpty(items)) {
+            itemList = items.map(function(item) {
+                return Ember.get(item, descField) + '('+this._dateFormat(Ember.get(item, dateField))+')';
             }.bind(this));
         }
-        return procedureList;        
+        return itemList.join(',\n');
+    },
+    
+    _procedureListToString: function(procedures) {
+        return this._listToString(procedures, 'description', 'procedureDate');      
     },
     
     _setReportTitle: function() {
