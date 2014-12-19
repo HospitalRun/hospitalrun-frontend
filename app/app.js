87ab65b67df37cd7a35fdd114deaa60888c3ba9c
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-12-19 08:30:03 -0500

    Upgraded to ember-cli 0.1.4 and ember 1.7.1

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({    
-    modulePrefix: 'hospitalrun', // TODO: loaded via config
-    Resolver: Resolver
+var App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver: Resolver
 });
 
-loadInitializers(App, 'hospitalrun');
+loadInitializers(App, config.modulePrefix);
 
 window.onerror = function(errorMsg, url, lineNumber, colno, error) {
     var errorMessage = "Error Occurred: "+errorMsg;
