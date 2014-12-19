356cdd84ffde436e37dda765d25f2f83b40eb9d3
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-12-19 16:49:40 -0500

    Moved most of vendor to bower_components
    Except for vendor directories that aren’t available via bower

--- vendor/pouchdb/lib/plugins/config-localstorage-down.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  name: 'localstorage',
-  valid: function () {
-    return 'localStorage' in window;
-  },
-  use_prefix: true
-};
--- vendor/pouchdb/lib/plugins/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var adapterConfig = require('adapter-config');
-var adapterName = adapterConfig.name;
-var adapter = require('./levelalt');
-
-window.PouchDB.adapter(adapterName, adapter);
-window.PouchDB.preferredAdapters.push(adapterName);
