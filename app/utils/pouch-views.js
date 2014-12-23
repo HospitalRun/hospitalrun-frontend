895fdb20f5c4762ac4996aec58776a94c3142da6
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-12-22 23:02:13 -0500

    Added more views/indexes

--- app/utils/pouch-views.js
+++ app/utils/pouch-views.js
@@ -1,25 +1,23 @@
 /* global emit */
 function createDesignDoc(name, mapFunction) {
-  var ddoc = {
-    _id: '_design/' + name,
-    views: {
-    }
-  };
-  ddoc.views[name] = { map: mapFunction.toString() };
-  return ddoc;
+    var ddoc = {
+        _id: '_design/' + name,
+        views: {
+        }
+    };
+    ddoc.views[name] = { map: mapFunction.toString() };
+    return ddoc;
 }
 
 
 function appointmentsByDate(doc) {
     var doctype,
         uidx;
-
     if (doc._id && (uidx = doc._id.indexOf("_")) > 0) {
         doctype = doc._id.substring(0, uidx);
         if(doctype === 'appointment') {
             var endDate = doc.endDate,
                 startDate = doc.startDate;
-
             if (endDate && endDate !== '') {
                 endDate = new Date(endDate);
                 if (endDate.getTime) {
@@ -32,14 +30,63 @@ function appointmentsByDate(doc) {
                     startDate = startDate.getTime(); 
                 }
             }
-            emit([startDate, endDate]);
+            emit([startDate, endDate, doc._id]);
         }
     }
 }
 
+function imagingByStatus(doc) {
+    var doctype,
+        uidx;    
+    if (doc._id && (uidx = doc._id.indexOf("_")) > 0) {
+        doctype = doc._id.substring(0, uidx);
+        if(doctype === 'imaging') {
+            emit([doc.status, doc._id]);
+        }
+    }    
+}
+
+function inventoryPurchaseByExpirationDate(doc) {
+    var doctype,
+        uidx;
+    if (doc._id && (uidx = doc._id.indexOf("_")) > 0) {
+        doctype = doc._id.substring(0, uidx);
+        if (doctype === 'inv-purchase') {
+            var expirationDate = doc.expirationDate;
+            if (expirationDate && expirationDate !== '') {
+                expirationDate = new Date(expirationDate);
+                if (expirationDate.getTime) {
+                    expirationDate = expirationDate.getTime();
+                }
+            }
+            emit([expirationDate, doc._id]); 
+        }   
+    }
+}
+
+function inventoryRequestByStatus(doc) {
+    var doctype,
+        uidx;    
+    if (doc._id && (uidx = doc._id.indexOf("_")) > 0) {
+        doctype = doc._id.substring(0, uidx);
+        if(doctype === 'inv-request') {
+            emit([doc.status, doc._id]);
+        }
+    }    
+}
+
 var designDocs = [{
     name: 'appointments_by_date',
-    function: appointmentsByDate
+    function: appointmentsByDate    
+}, {
+    name: 'imaging_by_status',
+    function: imagingByStatus
+}, {
+    name: 'inventory_purchase_by_expiration_date',
+    function: inventoryPurchaseByExpirationDate
+}, {
+    name: 'inventory_request_by_status',
+    function: inventoryRequestByStatus
 }];
 
 export default function(db) {
