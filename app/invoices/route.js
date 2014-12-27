9be59b688da737f12b280d995e8ef3349d8cf15f
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-12-27 13:03:39 -0500

    Removed fieldMapping/mapResults
    Relationships are handled via Ember Data’s belongsTo, hasMany, etc.

--- app/invoices/route.js
+++ app/invoices/route.js
@@ -1,19 +1,11 @@
 import AbstractModuleRoute from 'hospitalrun/routes/abstract-module-route';
-import InvoiceMapping from 'hospitalrun/mixins/invoice-mapping';
 
-export default AbstractModuleRoute.extend(InvoiceMapping, {
+export default AbstractModuleRoute.extend({
     addCapability: 'add_invoice',
     currentScreenTitle: 'Invoices',
     editTitle: 'Edit Invoice',    
     newTitle: 'New Invoice',
     moduleName: 'invoices',
     newButtonText: '+ new invoice',
-    sectionTitle: 'Invoices',
-    
-    model: function() {
-        return this.store.find('invoice', {
-            mapResults: this._mapViewResults,
-            fieldMapping: this.fieldMapping
-        });
-    }    
+    sectionTitle: 'Invoices'
 });
--- app/medication/search/route.js
+++ app/medication/search/route.js
@@ -4,19 +4,5 @@ export default AbstractSearchRoute.extend({
     searchKeys: [
         'prescription'
     ],
-    searchModel: 'medication',
-    
-    /**
-     * Get the query params to run against the store find function.
-     * By default this function will return a query that does a "contains"
-     * search against all of the searchKeys defined for this route.
-     * You can override this function if you need additional/different parameters.
-     * @param searchText string containing text to search for.     
-     */
-    getQueryParams: function(searchText) {
-        var queryParams = this._super(searchText);
-        queryParams.mapResults = this._mapViewResults;
-        queryParams.fieldMapping = this.fieldMapping;
-        return queryParams;
-    }
+    searchModel: 'medication'
 });
