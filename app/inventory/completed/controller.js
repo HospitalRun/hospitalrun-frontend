66aae47f019b9687b32e1a60e76583595257c468
John Kleinschmidt <kleinschmidtorama@gmail.com>
2014-12-29 15:58:11 -0500

    Removed descending sort due to conflict w/paging

--- app/inventory/completed/controller.js
+++ app/inventory/completed/controller.js
@@ -1,7 +1,4 @@
 import AbstractPagedController from 'hospitalrun/controllers/abstract-paged-controller';
 export default AbstractPagedController.extend({
-    startKey: [],
-    sortProperties: ['dateCompleted'],
-    // Sort in descending order
-    sortAscending: false
+    startKey: []
 });
