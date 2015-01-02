9e5d71c2dbbe8202999f22061f1f534bdb00ae9c
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-01-02 15:37:43 -0500

    Removed unneeded code

--- app/patients/reports/route.js
+++ app/patients/reports/route.js
@@ -2,10 +2,6 @@ import AbstractIndexRoute from 'hospitalrun/routes/abstract-index-route';
 export default AbstractIndexRoute.extend({
     pageTitle: 'Patient Report',
 
-    afterModel: function() {
-        this.set('inventoryItems', this.modelFor('inventory'));
-    },
-    
     model: function() {
         return this.store.find('visit');
     }
