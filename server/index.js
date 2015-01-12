dc7bc1f5a22fbed6083341e3ec6cfbbd7699b40d
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-01-12 11:16:35 -0500

    Fixed user crud on ember serve

--- server/index.js
+++ server/index.js
@@ -12,11 +12,5 @@ var globSync   = require('glob').sync;
 var routes     = globSync('./routes/**/*.js', { cwd: __dirname }).map(require);
 
 module.exports = function(app) {
-  routes.forEach(function(route) { route(app); });
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
-  
+  routes.forEach(function(route) { route(app); });  
 };
