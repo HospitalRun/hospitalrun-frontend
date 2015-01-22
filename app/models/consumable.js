706c78a60e2678dd5028068b9b82684be905b451
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-01-22 14:32:34 -0500

    Allow consumables to not be inventory items.

--- app/models/consumable.js
+++ app/models/consumable.js
@@ -1,34 +1,13 @@
 import AbstractModel from 'hospitalrun/models/abstract';
-import Ember from 'ember';
 
 export default AbstractModel.extend({
     inventoryItem: DS.belongsTo('inventory'),
+    name: DS.attr('string'),
     quantity: DS.attr('number'),
     
     validations: {
-        inventoryItemTypeAhead: {
-            acceptance: {
-                accept: true,
-                if: function(object) {
-                    if (!object.get('isDirty')) {
-                        return false;
-                    }
-                    var itemName = object.get('inventoryItem.name'),
-                        itemTypeAhead = object.get('inventoryItemTypeAhead');
-                    if (Ember.isEmpty(itemName) || Ember.isEmpty(itemTypeAhead)) {
-                        //force validation to fail
-                        return true;
-                    } else {
-                        var typeAheadName = itemTypeAhead.substr(0, itemName.length);
-                        if (itemName !== typeAheadName) {
-                            return true;
-                        }
-                    }
-                    //Inventory item is properly selected; don't do any further validation
-                    return false;
-                }, 
-                message: 'Please select a valid inventory item'
-            }
+        name: {
+            presence: true
         },    
         
         quantity: {
