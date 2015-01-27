8d9a8a612473612c210072255ba369b4cb504513
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-01-27 10:50:49 -0500

    Removed unused code

--- app/procedures/equipment-item/controller.js
+++
@@ -1,7 +0,0 @@
-import Ember from "ember";
-export default Ember.ObjectController.extend({
-    canDelete: function() {
-        var canDeletePurchase = this.parentController.get('canAddProcedure');        
-            return canDeletePurchase;
-    }.property()    
-});
