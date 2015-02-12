0c5490b94f4976856e01981c1234b275cbf5495b
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-02-12 16:26:02 -0500

    Changed to use addChildToVisit to add new proc.

--- app/procedures/edit/controller.js
+++ app/procedures/edit/controller.js
@@ -14,6 +14,7 @@ export default AbstractEditController.extend(ChargeActions, PatientSubmodule, {
     canAddCharge: function() {        
         return this.currentUserCan('add_charge');
     }.property(),
+    chargePricingCategory: 'Procedure',
     
     anesthesiaTypes: Ember.computed.alias('controllers.visits.anesthesiaTypes'),
     anesthesiologistList: Ember.computed.alias('controllers.visits.anesthesiologistList'),
@@ -88,22 +89,15 @@ export default AbstractEditController.extend(ChargeActions, PatientSubmodule, {
     
     beforeUpdate: function() {
         if (this.get('isNew')) {
-            this.set('newProcedure', true);         
-        }
-        return Ember.RSVP.Promise.resolve();
+            return this.addChildToVisit(this.get('model'), 'procedures');
+        } else {
+            return Ember.RSVP.Promise.resolve();
+        }        
     },
     
-    afterUpdate: function(procedure) {
+    afterUpdate: function() {
         var alertTitle = 'Procedure Saved',
             alertMessage = 'The procedure record has been saved.';
-        if (this.get('newProcedure')) {
-            this.get('visitProcedures').then(function(list) {
-                list.addObject(procedure);
-                this.get('editController').send('update');
-                this.displayAlert(alertTitle, alertMessage);                
-            }.bind(this));            
-        } else {
-            this.displayAlert(alertTitle, alertMessage);
-        }
+        this.displayAlert(alertTitle, alertMessage);
     }
 });
