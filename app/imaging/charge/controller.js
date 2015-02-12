71fffd962c0d94de4652b5ab7d5242626f4f1170
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-02-12 17:13:47 -0500

    Price list filtering out object types.

--- app/imaging/charge/controller.js
+++ app/imaging/charge/controller.js
@@ -6,5 +6,5 @@ export default ProcedureChargeController.extend({
     cancelAction: 'closeModal',
     newPricingItem: false,
     requestingController: Ember.computed.alias('controllers.imaging/edit'),
-    pricingList: Ember.computed.alias('controllers.imaging/edit.pricingList'),    
+    pricingList: Ember.computed.alias('controllers.imaging/edit.chargesPricingList')    
 });
