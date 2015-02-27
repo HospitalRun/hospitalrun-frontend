81f1996cf02da881dbfb9505466c81c8c81cb00c
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-02-27 16:10:28 -0500

    Removed unneeded attributes

--- app/models/payment.js
+++ app/models/payment.js
@@ -1,8 +1,6 @@
 import AbstractModel from "hospitalrun/models/abstract";
 
 export default AbstractModel.extend({
-    paymentId: DS.attr(),
-    invoiceId: DS.attr('string'),
     amount: DS.attr('number'),
     paymentType: DS.attr('string'),
     notes: DS.attr('string')
