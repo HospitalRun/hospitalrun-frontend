0016efb9c8d0d870368a75e2d7230d3a630ab89e
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-03-09 11:29:25 +0800

    Fix for visit history and history last seen

--- app/models/visit.js
+++ app/models/visit.js
@@ -10,6 +10,7 @@ export default AbstractModel.extend({
     endDate:  DS.attr('date'),  //if visit type is outpatient, startDate and endDate are equal 
     examiner: DS.attr('string'),
     history: DS.attr('string'),
+    historySince: DS.attr('string'), //History since last seen
     imaging: DS.hasMany('imaging', {async: true}),
     labs: DS.hasMany('lab', {async: true}),
     location: DS.attr('string'),
