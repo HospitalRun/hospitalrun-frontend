16875d8b82259e10f74b0eb2502d383e71c56e97
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-04-01 16:55:32 -0400

    Component to display array values on newlines

---
+++ app/components/array-display.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+export default Ember.Component.extend({
+    
+    isArray: function() {
+        var content = this.get('content');
+        return Ember.isArray(content);
+    }.property('content')
+});
