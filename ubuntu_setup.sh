44af250aedac030ef603b8b2aa8a23fdf92f5dd2
Pradeep Bhaskaran <pradeep.bhaskran@oasishospital.org>
2015-05-18 11:03:52 +0400

    Setup enhancements

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,17 +1,21 @@
-sudo apt-get install couchdb -y
+
+#wget https://github.com/OasisHospital/frontend/raw/master/ubuntu_setup.sh | bash
+
+
+#sudo apt-get install couchdb -y
 
 #node cleanup
-cd ~
-sudo rm -rf ~/.nvm
-sudo rm -rf ~/.npm
-exit
+#cd ~
+#sudo rm -rf ~/.nvm
+#sudo rm -rf ~/.npm
+#exit
 
-sudo apt-get remove nodejs
+sudo apt-get remove nodejs -y
 sudo ls /etc/apt/sources.list.d
 sudo rm -i /etc/apt/sources.list.d/nodesource.list
 sudo ls /etc/apt/sources.list.d
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install nodejs
 sudo apt-get install npm
@@ -27,7 +31,12 @@ sudo apt-get install git
 git clone https://github.com/OasisHospital/frontend.git
 git clone https://github.com/OasisHospital/server.git
 
-cp server/config-example.js frontend/server/config.js
+cp server/config-example.js server/config.js  
+
+sed -i 's/COUCH ADMIN USER GOES HERE/hradmin/g' server/config.js
+sed -i 's/COUCH ADMIN PASSWORD GOES HERE/test/g' server/config.js
+
+cp server/config.js frontend/server/config.js
 
 cd frontend/
 
@@ -35,21 +44,14 @@ sudo npm install -g ember-cli@0.1.4
 sudo npm install -g bower
 
 
-
-
-#161  ember serve --environment=production
-  
-
 npm install
 bower install
 sh initcouch.sh
 
-ember serve
+#ember serve
+#ember serve --environment=production
 
- 
-
-ember serve
-ember build --environment production --output-path prod
+#ember build --environment production --output-path prod
 
 #npm install ember-cli-babel --save
 #ember build --environment production --output-path prod
