6a42c5eab34349a733101d481d1a63099e1af7bc
Joel Glovier <jglovier@github.com>
2015-05-29 19:47:47 -0400

    move to navigation controller

--- app/controllers/navigation.js
+++ app/controllers/navigation.js
@@ -2,6 +2,25 @@ import Ember from "ember";
 import ProgressDialog from "hospitalrun/mixins/progress-dialog";
 import UserSession from "hospitalrun/mixins/user-session";
 export default Ember.Controller.extend(ProgressDialog, UserSession,{
+    indexLinks: [
+        'Appointments',
+        'Labs',
+        'Imaging',
+        'Inventory',
+        'Medication',
+        'Patients',
+        'Users'
+    ],
+
+    setupPermissions: function() {
+        var permissions = this.get('defaultCapabilities');
+        for(var capability in permissions) {
+            if (this.currentUserCan(capability)) {
+                this.set('userCan_'+capability, true);
+            }
+        }
+    }.on('init'),
+
     needs: 'application',
     allowSearch: false,
     currentSearchText: null,
@@ -21,7 +40,7 @@ export default Ember.Controller.extend(ProgressDialog, UserSession,{
     showUsers: function() {
         return this.currentUserCan('users');
     }.property('session.isAuthenticated'),
-    
+
     actions: {
         search: function() {
             if (this.allowSearch && this.searchRoute) {
@@ -29,7 +48,7 @@ export default Ember.Controller.extend(ProgressDialog, UserSession,{
                     currentSearchText = this.get('currentSearchText'),
                     textToFind = this.get('searchText');
                 if (currentSearchText !== textToFind || currentRouteName.indexOf('.search') === -1) {
-                    this.set('searchText','');                
+                    this.set('searchText','');
                     this.set('progressMessage','Searching for '+textToFind+'.  Please wait...');
                     this.showProgressModal();
                     this.transitionToRoute(this.searchRoute+"/"+textToFind);
