add0325cde3951f8140f0589f4cc757e2a8afe01
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-07-20 16:13:10 -0400

    Added location and distribution unit to inventory listing
    Resolves HospitalRun/frontend#121

--- app/inventory/item-controller/controller.js
+++ app/inventory/item-controller/controller.js
@@ -1,5 +1,6 @@
-import Ember from "ember";
-export default Ember.ObjectController.extend({
+import Ember from 'ember';
+import LocationName from 'hospitalrun/mixins/location-name';
+export default Ember.ObjectController.extend(LocationName, {
     canDelete: function() {
         return this.parentController.get('canDeleteItem');
     }.property(),
@@ -7,6 +8,20 @@ export default Ember.ObjectController.extend({
     canEdit: function() {
         return this.parentController.get('canAddItem');
     }.property(),
+    
+    displayLocations: function() {
+        var locations = this.get('availableLocations'),
+            returnLocations = [];
+        locations.forEach(function(currentLocation) {
+            var aisleLocationName = currentLocation.get('aisleLocation'),
+                locationName = currentLocation.get('location'),
+                displayLocationName = this.formatLocationName(locationName, aisleLocationName);
+            if (!Ember.isEmpty(displayLocationName)) {
+                returnLocations.push(displayLocationName);
+            }
+        }.bind(this));
+        return returnLocations.toString();
+    }.property('availableLocations'),
     
     showAdd: function() {
         var canAddPurchase = this.parentController.get('canAddPurchase');
