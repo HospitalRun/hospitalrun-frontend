e755ac5e7e02ad295e743da4bc1b44bf995fb23d
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-08-04 09:53:59 -0400

    Change to use ember-radio-buttons addon instead.

--- app/components/ext-radio.js
+++ app/components/ext-radio.js
@@ -2,8 +2,18 @@ import Ember from "ember";
 export default Ember.Component.extend({     
     includeOtherOption: false,
     otherOptionLabel: null,
-    name: 'radio',
     showInline: false,
-    radioLabelPath: 'content.label',
-    radioValuePath: 'content.value'
+
+    haveLabel: function() {
+        var firstRadio = this.get('content.firstObject');
+        return !Ember.isEmpty(firstRadio.label);
+    }.property('content'),
+    
+    radioClass: function() {
+        if (this.get('showInline')) {
+            return 'radio-inline';
+        } else {
+            return 'radio';
+        }
+    }.property('showInline')
 });
