2881c35f5e5713024fec1787f7a9b4c657c25ee5
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-08-19 13:06:32 -0400

    Fixed patient visit retrieval

--- app/mixins/patient-visits.js
+++ app/mixins/patient-visits.js
@@ -4,7 +4,7 @@ export default Ember.Mixin.create(PouchDbMixin, {
     getPatientVisits: function(patient) {
         return new Ember.RSVP.Promise(function(resolve, reject) {
             var maxValue = this.get('maxValue'),
-                patientId = 'patient_'+patient.get('id');
+                patientId = patient.get('id');
             this.store.find('visit', {
                 options: {
                     startkey: [patientId, null, null, null, 'visit_'],
