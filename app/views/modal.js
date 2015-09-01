4cf9bc7ecf5cbc594a2a1979e980dd8677fc27c5
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-09-01 11:49:22 -0400

    Added modal-dialog component so that modals behave.

--- app/views/modal.js
+++ app/views/modal.js
@@ -1,19 +1,4 @@
 import Ember from "ember";
 export default Ember.View.extend({
-    layoutName: 'modal',
-    
-    didInsertElement: function() {
-        var $modal = this.$('.modal').modal();
-
-        $modal.on('hidden.bs.modal', function () {
-            this.get('controller').send('closeModal');
-        }.bind(this));
-    },
-
-    willDestroyElement: function() {
-        this.$('.modal').modal('hide');
-        //jquery fixes
-        $('body').removeClass('modal-open');
-        $('.modal-backdrop').remove();
-    }
+    layoutName: 'modal'
 });
