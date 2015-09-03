3e5963f6a91164c49c577d41c919a4a5dd363914
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-09-03 16:06:54 -0400

    Fixed creating new visits.

--- app/visits/edit/route.js
+++ app/visits/edit/route.js
@@ -1,8 +1,18 @@
 import AbstractEditRoute from 'hospitalrun/routes/abstract-edit-route';
 import ChargeRoute from 'hospitalrun/mixins/charge-route';
+import Ember from 'ember';
 export default AbstractEditRoute.extend(ChargeRoute, {
     editTitle: 'Edit Visit',
     modelName: 'visit',
     newTitle: 'New Visit',
-    pricingCategory: 'Ward'
+    pricingCategory: 'Ward',
+    
+     getNewData: function() {
+        return Ember.RSVP.resolve({
+            visitType: 'Admission',
+            startDate: new Date(),
+            status: 'Admitted'
+        });
+    }
+    
 });
