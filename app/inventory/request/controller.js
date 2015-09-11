34858eedfd46dd724732b8322f6a7e95d70639ea
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-09-11 10:45:04 -0400

    Changed to display message after fulfilling inventory request
    Added performFulfillRequest function so that we can wait until
    inventory request is fulfilled before proceeding.

--- app/inventory/request/controller.js
+++ app/inventory/request/controller.js
@@ -1,8 +1,10 @@
 import AbstractEditController from 'hospitalrun/controllers/abstract-edit-controller';
+import FulfillRequest from 'hospitalrun/mixins/fulfill-request'; 
+import InventoryLocations from 'hospitalrun/mixins/inventory-locations'; //inventory-locations mixin is needed for fulfill-request mixin!
 import InventorySelection from 'hospitalrun/mixins/inventory-selection';
 import Ember from 'ember';
 
-export default AbstractEditController.extend(InventorySelection, {    
+export default AbstractEditController.extend(FulfillRequest, InventoryLocations, InventorySelection, {    
     needs: ['inventory','pouchdb'],
     
     cancelAction: 'allRequests',
@@ -142,7 +144,7 @@ export default AbstractEditController.extend(InventorySelection, {
                 var updateViaFulfillRequest = this.get('updateViaFulfillRequest');
                 if (updateViaFulfillRequest) {
                     this.updateLookupLists();
-                    this.send('fulfillRequest', this.get('model'));
+                    this.performFulfillRequest(this.get('model')).then(this.afterUpdate.bind(this));
                 } else {
                     var isNew = this.get('isNew'),
                         requestedItems = this.get('requestedItems');
@@ -191,8 +193,13 @@ export default AbstractEditController.extend(InventorySelection, {
         }
     },
     
-    afterUpdate: function() {        
-        this.send('allRequests');
+    afterUpdate: function() {
+        var updateViaFulfillRequest = this.get('updateViaFulfillRequest');
+        if (updateViaFulfillRequest) {
+            this.displayAlert('Request Fulfilled', 'The inventory request has been fulfilled.', 'allRequests');
+        } else {
+            this.displayAlert('Request Updated', 'The inventory request has been updated.');
+        }
     }, 
 
     beforeUpdate: function() {
--- app/inventory/request/route.js
+++ app/inventory/request/route.js
@@ -11,6 +11,10 @@ export default AbstractEditRoute.extend({
     },
     
     actions: {
+        allRequests: function(model) {
+            this.controller.send('allRequests', model);
+        },
+        
         removeItem: function(model) {
             this.controller.send('removeItem', model);
         }
