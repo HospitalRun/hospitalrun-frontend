eecd3f806046e6662d9b3464ac87e63d03db5b8e
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-09-18 11:18:49 -0400

    Fixed patient admission and discharge buttons.

--- app/patients/index/controller.js
+++ app/patients/index/controller.js
@@ -3,6 +3,14 @@ import PatientVisits from 'hospitalrun/mixins/patient-visits';
 export default AbstractPagedController.extend(PatientVisits, {
     addPermission: 'add_patient',
     deletePermission: 'delete_patient',
+    canAdmitPatient: function() {
+        return this.currentUserCan('admit_patient');
+    }.property(),
+    
+    canDischargePatient: function() {
+        return this.currentUserCan('discharge_patient');
+    }.property(),
+    
     startKey: [],    
     actions: {
         admitPatient: function(patient) {
