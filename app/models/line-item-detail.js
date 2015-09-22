a43b50785be05c6de25b33e09526f426366483e1
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-09-22 16:32:44 -0400

    Removed discount from line item details and removed observers
    Simplify discount calculation.

--- app/models/line-item-detail.js
+++ app/models/line-item-detail.js
@@ -4,10 +4,7 @@ import Ember from 'ember';
 import NumberFormat from 'hospitalrun/mixins/number-format';
 
 export default AbstractModel.extend(NumberFormat,{
-    amountOwed: DS.attr('number'),
     department: DS.attr('string'),
-    discount: DS.attr('number'),
-    discountPercentage: DS.attr('number'),
     expenseAccount: DS.attr('string'),
     name: DS.attr('string'),
     price: DS.attr('number'),
@@ -17,49 +14,14 @@ export default AbstractModel.extend(NumberFormat,{
     quantity: DS.attr('number'),
     total: DS.attr('number'),
     
-    displayDiscount: function() {
-        var discount = this.get('discount');
-        if (!Ember.isEmpty(discount)) {
-            this.set('discountPercentage');            
-        }
-        return this.get('discount');
-    }.property('discount'),
-    
-    _calculateAmountOwed: function() {
-        var amountOwed,
-            discount = this.get('discount'),
-            total = this.get('total');
-        if (this._validNumber(total)) {
-            if (this._validNumber(discount)) {            
-                amountOwed = this._numberFormat((total - discount), true);
-            } else {
-                amountOwed = this._numberFormat(total, true);
-            }            
-        }
-        this.set('amountOwed', amountOwed);
-    },
-    
-    _calculateTotal: function() {
-        var discountPercentage = this.get('discountPercentage'),
-            price = this.get('price'),
+    amountOwed: function() {
+        var price = this.get('price'),
             quantity = this.get('quantity'),
-            total;
+            total = 0;
         if (this._validNumber(price) && this._validNumber(quantity)) {
             total = this._numberFormat((price * quantity), true);
-            if (!Ember.isEmpty(discountPercentage)) {
-                var discount = this._numberFormat((discountPercentage * total), true);
-                this.set('discount', discount);
-            }
         }
-        this.set('total', total);
-    },
-    
-    amountOwedChanged: function() {
-        Ember.run.debounce(this, this._calculateAmountOwed,150);
-    }.observes('discount', 'total'),
-   
-    totalChanged: function() {
-        Ember.run.debounce(this, this._calculateTotal,300);
-    }.observes('price','quantity')
+        return total;
+    }.property('price','quantity')
 
 });
