8079c39b3d051158f65f5c591e78efad5535c5f6
zinyando <brizdigital@gmail.com>
2015-10-05 15:53:23 +0200

    Patients route tests

--- tests/acceptance/patients-test.js
+++ tests/acceptance/patients-test.js
@@ -13,11 +13,52 @@ module('Acceptance | patients', {
 });
 
 
-test('visiting /patients', function(assert) {
+test('visiting /patients route', function(assert) {
   loadPouchDump('default');
   authenticateUser();
   visit('/patients');
+
   andThen(function() {
     assert.equal(currentURL(), '/patients');
+    const noPatientsFound = find('[data-test-selector="no-patients-found"]');
+    assert.equal(noPatientsFound.text().trim(), 'No patients found. Create a new patient record?', 'no records found');
+    const newPatientButton = find('button:contains(+ new patient)');
+    assert.equal(newPatientButton.length, 1, 'Add new patient button is visible');
+  });
+  click('button:contains(+ new patient)');
+  andThen(function() {
+    assert.equal(currentURL(),'/patients/edit/new');
   });
 });
+
+test('View reports', function(assert) {
+  loadPouchDump('default');
+  authenticateUser();
+  visit('/patients/reports');
+
+  andThen(function() {
+    var generateReportButton = find('button:contains(Generate Report)');
+    assert.equal(currentURL(), '/patients/reports');
+    assert.equal(generateReportButton.length, 1, 'Generate Report button is visible');
+    const reportType = find('[data-test-selector="select-report-type"]');
+    assert.equal(reportType.length, 1, 'Report type select is visible');
+    const reportStartDate = find('[data-test-selector="select-report-start-date"]');
+    const reportEndDate = find('[data-test-selector="select-report-end-date"]');
+    assert.equal(reportStartDate.length, 1, 'Report start date select is visible');
+    assert.equal(reportEndDate.length, 1, 'Report end date select is visible');
+    //assert.equal(reportType.text().trim(), 'Admissions Detail', 'Default value selected"');
+  });
+  fillIn('[data-test-selector="select-report-type"]', 'Visit');
+  andThen(function() {
+    //assertions
+  });
+});
+
+// test('Adding a new patient record', function(assert) {
+//   loadPouchDump('default');
+//   authenticateUser();
+//   visit('/patients/edit/new');
+//   andThen(function() {
+//     assert.equal(currentURL(), '/patients/edit/new');
+//   });
+// });
