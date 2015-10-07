4d28e3e275e6586128b08587e9d0a8c6a48db406
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-10-07 13:07:00 -0400

    Put back navigation highlighting

--- app/views/application.js
+++ app/views/application.js
@@ -1,12 +1,11 @@
 import Ember from "ember";
 export default Ember.View.extend({
-    // TODO(taras): Fix this
-    // currentPathDidChange: function() {
-    //     Ember.run.next( this, function() {
-    //         this.$("ul.nav li:has(>a.active)").addClass('active');
-    //         this.$("ul.nav li:not(:has(>a.active))").removeClass('active');
-    //     });
-    // }.observes('controller.currentPath'),
+    currentPathDidChange: function() {
+         Ember.run.next( this, function() {
+             this.$("ul.nav li:has(>a.active)").addClass('active');
+             this.$("ul.nav li:not(:has(>a.active))").removeClass('active');
+         });
+    }.observes('controller.currentPath'),
 
     willInsertElement: function() {
         $("#apploading").remove();
