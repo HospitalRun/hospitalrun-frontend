7fbbcb9aced33f1d8b1e3e4d23a2dcaf7e75c40d
Taras Mankovski <tarasm@gmail.com>
2015-10-07 09:29:01 -0400

    Replaced ember-cli-mirage in fravor of simpler ember-cli-fake-server

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from 'hospitalrun/tests/helpers/start-app';
+import FakeServer, { stubRequest } from 'ember-cli-fake-server';
 
 module('Acceptance | login', {
   beforeEach: function() {
+    FakeServer.start();
     this.application = startApp();
   },
 
   afterEach: function() {
+    FakeServer.stop();
     Ember.run(this.application, 'destroy');
   }
 });
@@ -17,14 +20,16 @@ test('visiting / redirects user to login', function(assert) {
   loadPouchDump('default');
   visit('/');
 
-  server.post('/db/_session', function(db, request){
+  stubRequest('post', '/db/_session', function(request){
     assert.equal(request.requestBody, "name=hradmin&password=test", 'credential are sent to the server');
-    return {"ok":true,"name":"hradmin","roles":["System Administrator","admin","user"]};
+    request.ok({"ok":true,"name":"hradmin","roles":["System Administrator","admin","user"]});
   });
-  server.post('/chkuser', function(db, request){
+
+  stubRequest('post', '/chkuser', function(request){
     assert.equal(request.requestBody, "name=hradmin", "username is sent to /chkuser");
-    return {"prefix":"p1","role":"System Administrator"};
+    request.ok({"prefix":"p1","role":"System Administrator"});
   });
+
   andThen(function() {
     assert.equal(currentURL(), '/login');
   });
--- tests/helpers/server.js
+++
@@ -1,8 +0,0 @@
-export function session(server) {
-  server.post('/db/_session', function(){
-    return {"ok":true,"name":"hradmin","roles":["System Administrator","admin","user"]};
-  });
-  server.post('/chkuser', function(){
-    return {"prefix":"p1","role":"System Administrator"};
-  });
-}
