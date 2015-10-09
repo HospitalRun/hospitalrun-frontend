3fdea6f3f93391eb00819dda3da33e8dc48cd1f0
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-10-09 10:26:04 -0400

    Added selectObjectMap so that selects can use object as value selected.

--- app/utils/select-values.js
+++ app/utils/select-values.js
@@ -4,11 +4,22 @@ function selectValuesMap (value) {
     value: value
   };
 }
+
 import Ember from 'ember';
 export default {
+  /**
+   * Map an objects into a format so that selects can use object as value for select
+   * @param {object} object the object to map
+   * @returns {object} the mapped object
+   */
+  selectObjectMap: function (selectValue) {
+    return Ember.Object.create({
+      selectObject: selectValue
+    });
+  },
+
   // Map an array of strings to objects with id and value set to the string values
   // so that the array can be used for em-select
-
   selectValuesMap: selectValuesMap,
 
   selectValues: function (array) {
