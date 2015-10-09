3dd64c52f2c4fadbca08d13696514ac5a4a65c9b
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-10-09 10:31:54 -0400

    Fixed select-or-typeahead to properly display values on select

--- app/components/select-or-typeahead.js
+++ app/components/select-or-typeahead.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
+import SelectValues from 'hospitalrun/utils/select-values';
 export default Ember.Component.extend({
   name: 'select-or-typeahead',
   className: null,
   hint: true,
   label: null,
   list: null,
-  optionLabelPath: 'content',
-  optionValuePath: 'content',
+  optionLabelPath: 'value',
+  optionValuePath: 'id',
   property: null,
   prompt: ' ',
   selection: null,
@@ -14,9 +15,16 @@ export default Ember.Component.extend({
   typeAheadType: null,
 
   content: function () {
-    var list = this.get('list');
+    var list = this.get('list'),
+      optionLabelPath = this.get('optionLabelPath'),
+      optionValuePath = this.get('optionValuePath');
+
     if (!Ember.isEmpty(list) && list.get) {
-      return list.get('value');
+      var contentList = list.get('value');
+      if (optionLabelPath === 'value' && optionValuePath === 'id') {
+        contentList = contentList.map(SelectValues.selectValuesMap);
+      }
+      return contentList;
     }
   }.property('list'),
 
