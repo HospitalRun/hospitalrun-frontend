21e976b42b94a994d0e17433f151dad5bcf38e27
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-10-14 10:49:47 -0400

    Moved print view to component

--- app/components/print-this.js
+++ app/components/print-this.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-export default Ember.View.extend({
+export default Ember.Component.extend({
   didInsertElement: function() {
-    this._super();
     Ember.run.scheduleOnce('afterRender', this, function() {
       window.print();
     });
