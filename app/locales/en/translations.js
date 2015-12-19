2f89d5c7de678935ef8a14eb71a7a0867849ead2
billybonks <sebastienstettler@gmail.com>
2015-12-20 03:42:15 +0800

    Install i18n

---
+++ app/locales/en/translations.js
@@ -0,0 +1,11 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+};
