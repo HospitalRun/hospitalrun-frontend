6d846574e4c5116181edb035bdaec58c7c8d2bd2
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-02-03 10:55:07 -0500

    Removed no longer needed files

--- app/invoices/payment-item/controller.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-export default Ember.ObjectController.extend({
-    canRemovePayment: function() {        
-        return (this.get('type') === 'Deposit');
-    }.property()
-});
