39940edccd31c8df483d28f53fd4b1d0eb90a6fb
Christian Fleschhut <weltrekordhalter@gmail.com>
2016-03-02 01:08:25 +0100

    Add more german translations

--- app/locales/de/translations.js
+++ app/locales/de/translations.js
@@ -42,6 +42,58 @@ export default {
     },
     about: 'Über HospitalRun'
   },
+  user: {
+    plus_new_user: '+ Neuer Benutzer',
+    users_page_tile: 'Benutzerliste'
+  },
+  admin: {
+    address_options: 'Adressoptionen',
+    lookup_lists: 'Nachschlagelisten',
+    load_db: 'Datenbank laden',
+    users: 'Benutzer',
+    address: {
+      address1_label: 'Adresse 1 Kennzeichen',
+      address2_label: 'Adresse 2 Kennzeichen',
+      address3_label: 'Adresse 3 Kennzeichen',
+      address4_label: 'Adresse 4 Kennzeichen',
+      include1_label: 'Einbezug 1 Kennzeichen',
+      include2_label: 'Einbezug 2 Kennzeichen',
+      include3_label: 'Einbezug 3 Kennzeichen',
+      include4_label: 'Einbezug 4 Kennzeichen',
+      titles: {
+        options_saved: 'Optionen gespeichert'
+      },
+      messages: {
+        address_saved: 'Die Adressoptionen wurden gespeichert'
+      }
+    },
+    lookup: {
+      anesthesia_types: 'Anästhesiearten',
+      anesthesiologists: 'Anästhesiologe',
+      billing_categories: 'Abrechnungskategorien',
+      clinic_list: 'Kliniken',
+      country_list: 'Länder',
+      diagnosis_list: 'Diagnosen',
+      cpt_code_list: 'CPT Codes',
+      expense_account_list: 'Ausgabekonten',
+      aisle_location_list: 'Gänge-Verzeichnis',
+      warehouse_list: 'Warenhaus',
+      inventory_types: 'Inventartypen',
+      imaging_pricing_types: 'Bildgebungs-Preiskategorien',
+      lab_pricing_types: 'Labor-Preiskategorien',
+      patient_status_list: 'Patientenstatus',
+      physician_list: 'Ärzte',
+      procedure_list: 'Abläufe',
+      procedure_locations: 'Ablaufs-Orte',
+      procedure_pricing_types: 'Ablaufs-Preiskategorien',
+      radiologists: 'Radiologen',
+      unit_types: 'Einheiten',
+      vendor_list: 'Anbieter',
+      visit_location_list: 'Einsatzorte',
+      visit_types: 'Besuchsarten',
+      ward_pricing_types: 'Krankenstations-Preistypen'
+    }
+  },
   labels: {
     name: 'Name',
     patient: 'Patient',
@@ -63,11 +115,35 @@ export default {
     completed: 'Erledigt',
     id: 'Id-Nr',
     sex: 'Geschlecht',
-    age: 'Alter'
+    age: 'Alter',
+    username: 'Username',
+    email: 'E-Mail',
+    role: 'Rolle',
+    delete: 'Entfernen',
+    user_can_add_new_value: 'Benutzer kann neue Werte hinzufügen',
+    value: 'Wert',
+    lookup_type: 'Nachschlagebereich',
+    import_file: 'Datei importieren',
+    file_load_successful: 'Datei erfolgreich geladen',
+    file_to_Load: 'Datei laden',
+    start_time: 'Startzeit',
+    end_time: 'Endzeit',
+    doc_read: 'Dokument gelesen',
+    doc_written: 'Dokument geschrieben',
+    display_name: 'Angezeigter Name',
+    password: 'Passwort',
+    edit_user: 'Benutzer bearbeiten',
+    new_user: 'Neuer Benutzer',
+    delete_user: 'Benutzer löschen'
   },
   messages: {
     no_items_found: 'Keine Einträge gefunden.',
-    create_new_record: 'Neuen Eintrag erstellen?'
+    create_new_record: 'Neuen Eintrag erstellen?',
+    create_new_user: 'Neuen Benutzer hinzufügen?',
+    no_users_found: 'Keine Benutzer gefunden.',
+    are_you_sure_delete: 'Wollen Sie den Benutzer sicher löschen?',
+    user_has_been_saved: 'Der Benutzer wurde gespeichert.',
+    user_saved: 'Benutzer gespeichert'
   },
   buttons: {
     complete: 'Abschließen',
@@ -75,7 +151,12 @@ export default {
     return_button: 'Zurück',
     add: 'Hinzufügen',
     update: 'Aktualisieren',
-    ok: 'Okay'
+    ok: 'Okay',
+    delete: 'Entfernen',
+    new_user: 'Neuer Benutzer',
+    add_value: 'Wert hinzufügen',
+    import: 'Importieren',
+    load_file: 'Datei laden'
   },
   login: {
     messages: {
