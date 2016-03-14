321e16931fc3872628f4edf28f570696718ee234
Eoin Bailey <eoin.bailey@microsoft.com>
2016-03-14 15:35:18 -0700

    #345 alternative way to manage computed properties

--- app/admin/lookup/edit/controller.js
+++ app/admin/lookup/edit/controller.js
@@ -4,25 +4,7 @@ export default Ember.Controller.extend(IsUpdateDisabled, {
   editController: Ember.inject.controller('admin/lookup'),
   showUpdateButton: true,
 
-  title: function() {
-    var isNew = this.get('model.isNew');
-    if (isNew) {
-      return this.get('i18n').t('admin.lookup.edit.controller.add_title');
-    } else {
-      return this.get('i18n').t('admin.lookup.edit.controller.edit_title');
-    }
-  }.property('i18n.locale', 'model.isNew'),
-
   updateButtonAction: 'update',
-
-  updateButtonText: function() {
-    var isNew = this.get('model.isNew');
-    if (isNew) {
-      return this.get('i18n').t('admin.lookup.edit.controller.update_button_text_add');
-    } else {
-      return this.get('i18n').t('admin.lookup.edit.controller.update_button_text_update');
-    }
-  }.property('i18n.locale', 'model.isNew'),
 
   actions: {
     cancel: function() {
