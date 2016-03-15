257d796ee1d6e067a3226ebe2d47290f610d8b07
tangollama <tangollama@gmail.com>
2016-03-15 13:18:34 -0700

    Patient Note object.
    Now, we’re associating to a visit always.

---
+++ app/models/patient-note.js
@@ -0,0 +1,54 @@
+import AbstractModel from 'hospitalrun/models/abstract';
+import DS from 'ember-data';
+
+export default AbstractModel.extend({
+  //if the note was written by one person but dictated / given on behalf of another, otherwise, this and createdBy are the same
+  attribution: DS.attr('string'),  
+  content: DS.attr('string'),
+  createdBy: DS.attr('string'),
+  date: DS.attr('date'),
+  //custom list of noteTypes of mixins/patient-note-types
+  noteType: DS.attr(),
+  //who is this note about?
+  patient: DS.belongsTo('patient', {
+    async: false
+  }),  
+  //if this note is related to a visit, make sure it's noted.
+  visit: DS.belongsTo('visit', {
+    async: false
+  }),  
+  //if this note is related to an appointment, make sure it's noted.
+  appointment: DS.belongsTo('appointment', {
+    async: false
+  }),
+  //if this note is related to an imaging request, make sure it's noted.
+  imaging: DS.belongsTo('imaging', {
+    async: false
+  }),
+  //if this note is related to a lab, make sure it's noted.
+  lab: DS.belongsTo('lab', {
+    async: false
+  }),
+  //if this note is related to a medication request, make sure it's noted.
+  medication: DS.belongsTo('medication', {
+    async: false
+  }),
+  //if this note is related to a procedure, make sure it's noted.
+  procedure: DS.belongsTo('procedure', {
+    async: false
+  }),
+  validations: {
+    patient: {
+      presence: true
+    },
+    visit: {
+      presence: true
+    },
+    noteType: {
+      presence: true
+    },
+    content: {
+      presence: true
+    }
+  }
+});
