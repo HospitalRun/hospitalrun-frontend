0b545df33449cfd1cdd9333d215fa38a78e6a949
tangollama <tangollama@gmail.com>
2016-03-17 08:30:22 -0700

    corrections to labels and acceptance tests

---
+++ tests/acceptance/patient-notes-test.js
@@ -0,0 +1,79 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'hospitalrun/tests/helpers/start-app';
+
+module('Acceptance | patient notes', {
+  beforeEach: function() {
+    this.application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(this.application, 'destroy');
+  }
+});
+
+function tabTest(tabName, tabTitle) {
+  click(`[data-test-selector=${tabName}]`);
+  andThen(function() {
+    findWithAssert(`.active .panel-title:contains(${tabTitle})`);
+  });
+}
+
+test('visiting /patients new note route', function(assert) {
+  runWithPouchDump('default', function() {
+    authenticateUser();
+    visit('/patients');
+    
+    visit('/patients/edit/new');
+    andThen(function() {
+      assert.equal(currentURL(), '/patients/edit/new');
+    });
+
+    fillIn('.test-first-name input', 'John');
+    fillIn('.test-last-name input', 'Doe');
+    click('.panel-footer button:contains(Add)');
+    waitToAppear('.modal-dialog');
+    andThen(function() {
+      assert.equal(find('.modal-title').text(), 'Patient Saved', 'Patient record has been saved');
+      assert.equal(find('.modal-body').text().trim(), 'The patient record for John Doe has been saved.', 'Record has been saved');
+    });
+    click('button:contains(Close)');
+    waitToAppear('.patient-summary');
+
+    andThen(function() {
+      findWithAssert('.patient-summary');
+    });
+    
+    andThen(function() {
+      tabTest('appointments-tab', 'Visits');
+      click('button:contains(New Visit)');
+    });
+    
+    andThen(() => {
+      assert.equal(currentURL(), '/visits/edit/new', 'Now in add visiting information route');
+    });
+    click('.panel-footer button:contains(Add)');
+    waitToAppear('.modal-dialog');
+    andThen(() => {
+      assert.equal(find('.modal-title').text(), 'Visit Saved', 'New visit has been saved');
+    });
+    click('button:contains(Ok)');
+    andThen(() => {
+      findWithAssert('button:contains(New Note)');
+      findWithAssert('button:contains(New Procedure)');
+      findWithAssert('button:contains(New Medication)');
+      findWithAssert('button:contains(New Lab)');
+      findWithAssert('button:contains(New Imaging)');
+      findWithAssert('button:contains(New Vitals)');
+      findWithAssert('button:contains(Add Item)');
+    });
+    
+    andThen(function() {
+      assert.equal(find('button:contains(New Note)').length, 1, 'Add Note button in visible');
+      click('button:contains(New Note)');
+    });
+    andThen(function() {
+      assert.equal(find('label:contains(Note)').length, 1, 'Notes modal appeared.');
+    });
+  });
+});
