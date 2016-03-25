53f73ba7ea77734d53bff74c536c30812295418c
tangollama <tangollama@gmail.com>
2016-03-25 16:51:24 -0400

    jslinting errors in travis build
    Not happening locally… hmmm…

--- app/helpers/html-line-break.js
+++ app/helpers/html-line-break.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 export default Ember.Helper.helper(function([text]) {
-  if (text !== null && typeof(text) != 'undefined') {
+  if (text !== null && typeof text !== 'undefined') {
     return new Ember.Handlebars.SafeString(text.replace(/\n/g, '<br>'));
   } else {
     return null;
