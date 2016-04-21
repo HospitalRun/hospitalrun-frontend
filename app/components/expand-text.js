19b0e06b34bc9b3d9542c7e9a2218c98e257a7b2
Eddie Sholl <eddie.sholl@gmail.com>
2016-04-21 22:16:58 +1000

    Encapsulate rather than inherit EmText

--- app/components/expand-text.js
+++ app/components/expand-text.js
@@ -1,7 +1,8 @@
+import Ember from 'ember';
 import EmText from 'ember-rapid-forms/components/em-text';
 import textExpansion from '../utils/text-expansion';
 
-export default EmText.extend({
+export default Ember.Component.extend({
   expansions: {
     foo: 'bar',
     fox: 'bar',
@@ -10,12 +11,14 @@ export default EmText.extend({
     long: 'ggggggggggggggggggsssssssssss wwwwwwwwwwwwwh          ddddddd #abc dsdddeghsfiuoh3yvq83y489vq3n4yv8 8943rvyq3 vyq3yrvq'
   },
 
+  userText: '',
+
   didInsertElement: function() {
     try {
       const feedbackDiv = document.createElement('div');
       feedbackDiv.style.position = 'absolute';
-      const textarea = this.$()[0];
-      this.set('textarea', this.$()[0]);
+      const textarea = this.$()[0].getElementsByTagName('textarea')[0];
+      this.set('textarea', textarea);
       const textPos = textarea.getBoundingClientRect();
       const fbStyle = feedbackDiv.style;
       fbStyle.top = textPos.bottom + 'px';
@@ -28,9 +31,10 @@ export default EmText.extend({
       fbStyle.paddingLeft = '5px';
       fbStyle.visibility = 'hidden';
 
-      document.body.appendChild(feedbackDiv);
-
       this.set('feedbackDiv', feedbackDiv);
+      this.get('feedbackText');
+      this.get('activeExpansionSite');
+
     }
     catch (e) {
       console.log('didInsert ' + e);
@@ -40,15 +44,21 @@ export default EmText.extend({
   keyUp: function(k) {
     const textArea = k.target;
     const text = textArea.value;
-    this.set('text', text);
+    Ember.run.once(this, 'set', 'userText', text);
+
+    if (k.keyCode === 13) {
+      console.log('pressed enter');
+    }
   },
 
-  activeExpansionSite: Ember.computed('text', function() {
+  activeExpansionSite: Ember.computed('userText', function() {
 
+    const userText = this.get('userText');
     const textarea = this.get('textarea');
+    if (!textarea) { return null; }
     const cursorLoc = textarea.selectionStart;
-    const subjects = textExpansion.findExpansionSubjects(text);
-    const sites = textExpansion.findExpansionSites(text, subjects);
+    const subjects = textExpansion.findExpansionSubjects(userText);
+    const sites = textExpansion.findExpansionSites(userText, subjects);
 
     return sites.find(s => {
       const endIndex = s.index + s.match.length;
@@ -60,7 +70,6 @@ export default EmText.extend({
   possibleSwaps: Ember.computed('activeExpansionSite', 'expansions', function() {
     const activeSite = this.get('activeExpansionSite');
 
-
     if (activeSite) {
       const expansions = this.get('expansions');
       return Object.keys(expansions)
@@ -71,26 +80,46 @@ export default EmText.extend({
     }
   }),
 
-  feedbackText: Ember.computed('possibleSwaps', function() {
+  feedbackText: Ember.computed('possibleSwaps', 'activeExpansionSite', 'userText', function() {
       const div = this.get('feedbackDiv');
+      var result = '';
+      if (!div) { return null; }
       const possibleSwaps = this.get('possibleSwaps');
-
+      const expansions = this.get('expansions');
       if (possibleSwaps) {
+        const activeSite = this.get('activeExpansionSite');
+
           div.style.visibility = 'visible';
           if (possibleSwaps.length === 1) {
             const swapFrom = possibleSwaps[0];
             const swapTo = expansions[swapFrom];
-            div.innerHTML = `Press Enter to replace '${activeSite.term}' with '${swapTo}'`;
+            result = `Press Enter to replace '${activeSite.term}' with '${swapTo}'`;
           } else if (possibleSwaps.length > 1) {
-            div.innerHTML = 'Possible expansions: ' + possibleSwaps.join(', ');
+            result = 'Possible expansions: ' + possibleSwaps.join(', ');
           }
           else {
-            div.innerHTML = 'No expansion terms match ' + activeSite.term;
+            result = 'No expansion terms match ' + activeSite.term;
           }
-          div.innerHTML += '   ' + JSON.stringify(activeSite);
       }
       else {
         div.style.visibility = 'hidden';
       }
+
+      return result;
+    }),
+
+    feedbackDivStyle: Ember.computed('feedbackText', function() {
+      const feedbackText = this.get('feedbackText');
+      const visiblility = feedbackText ? 'visible' : 'hidden';
+      const textArea = this.get('textarea');
+
+      if (textArea) {
+        const textPos = textArea.getBoundingClientRect();
+        var styleString = `top: ${textPos.bottom}px; left: ${textPos.left}px; width: ${textArea.offsetWidth}px;
+        background-color: lightyellow; border-style: solid; border-width: 1px; border-radius: 3px;
+        position: absolute; padding-left: 5px; visibility: ${visiblility}`;
+
+        return styleString;
+      }
     })
-})
+});
