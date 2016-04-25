6f3a0730aea855d01935e8096ddbe1e97377b36e
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-04-25 16:32:13 -0400

    Changed to use history API for app urls.

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'hospitalrun',
     environment: environment,
     baseURL: '/',
-    locationType: 'hash', // Auto incompatible with google login right now
+    locationType: 'history',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
