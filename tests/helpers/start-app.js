6a1b1e329fa393b2e974a9c20116105391cbfe08
billybonks <sebastienstettler@gmail.com>
2016-06-02 15:54:21 +0800

    Test for missing translations
    Stubs the translation function that do the translation and
    throw an error if it is missing
    
    uses deprecated api, happy to change but atleast it is only one
    instance
    
    All Feature tests must run startApp in the before each hook

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -8,6 +8,19 @@ import './select';
 import './select-date';
 import './wait-to-appear';
 
+function createTranslationWrapper(original, context) {
+  function t(str, data) {
+    let result = original.call(context, str, data);
+    if (result.indexOf && result.indexOf('Missing translation') > -1) {
+      throw new Error(result);
+    }
+
+    return result.string || result;
+  }
+
+  return t;
+}
+
 export default function startApp(attrs) {
   let application;
 
@@ -19,6 +32,9 @@ export default function startApp(attrs) {
     application.setupForTesting();
     application.injectTestHelpers();
   });
+
+  let translationService = application.__container__.lookup('service:i18n');
+  application.__container__.lookup('service:i18n').t = createTranslationWrapper(translationService.t, translationService);
 
   return application;
 }
