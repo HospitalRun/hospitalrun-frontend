2d3e506a3482fbb00c2902c88d497a784b0a0b01
billybonks <sebastienstettler@gmail.com>
2016-07-01 15:56:10 +0200

    Add translations for ember-validations

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -2,6 +2,29 @@ export default {
   dashboard: {
     title: 'What would you like to do?'
   },
+  errors: {
+    inclusion: 'is not included in the list',
+    exclusion: 'is reserved',
+    invalid: 'is invalid',
+    confirmation: 'doesn\'t match {{attribute}}',
+    accepted: 'must be accepted',
+    empty: 'can\'t be empty',
+    blank: 'can\'t be blank',
+    present: 'must be blank',
+    tooLong: 'is too long (maximum is {{count}} characters)',
+    tooShort: 'is too short (minimum is {{count}} characters)',
+    wrongLength: 'is the wrong length (should be {{count}} characters)',
+    notANumber: 'is not a number',
+    notAnInteger: 'must be an integer',
+    greaterThan: 'must be greater than {{count}}',
+    greaterThanOrEqualTo: 'must be greater than or equal to {{count}}',
+    equalTo: 'must be equal to {{count}}',
+    lessThan: 'must be less than {{count}}',
+    lessThanOrEqualTo: 'must be less than or equal to {{count}}',
+    otherThan: 'must be other than {{count}}',
+    odd: 'must be odd',
+    even: 'must be even'
+  },
   navigation: {
     imaging: 'Imaging',
     inventory: 'Inventory',
