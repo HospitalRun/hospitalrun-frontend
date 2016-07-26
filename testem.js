f5db866f1dc88106e068651a0dcf2f0468356eed
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-07-26 16:19:25 -0400

    Trying testing in Travis CI with Chrome instead of PhantomJS

--- testem.js
+++ testem.js
@@ -4,7 +4,7 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+    "Chrome"
   ],
   "launch_in_dev": [
     "PhantomJS",
