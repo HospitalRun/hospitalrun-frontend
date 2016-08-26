7fa94b9c8735641d1835ebada37a13fbcd2f1fec
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-08-26 13:43:03 -0400

    Fixed views to not show archive records.

--- app/utils/pouch-views.js
+++ app/utils/pouch-views.js
@@ -90,7 +90,7 @@ function generateView(viewDocType, viewBody) {
   return 'function(doc) {' +
   'var doctype,' +
   'uidx;' +
-  'if (doc._id && (uidx = doc._id.indexOf("_")) > 0 && !doc.archived) {' +
+  'if (doc._id && (uidx = doc._id.indexOf("_")) > 0 && !doc.data.archived) {' +
   'doctype = doc._id.substring(0, uidx);' +
   'if(doctype === "' + viewDocType + '") {' +
   viewBody +
