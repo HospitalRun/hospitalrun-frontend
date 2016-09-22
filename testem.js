c620552df9fd755e5f36c4ad4466987dd6f6fa38
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-09-22 14:08:56 +0300

    Changed to test via Chrome instead of PhantomJS

--- testem.js
+++ testem.js
@@ -4,10 +4,9 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+    "Chrome"
   ],
   "launch_in_dev": [
-    "PhantomJS",
     "Chrome"
   ]
 };
