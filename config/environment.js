0b6bcdf6b17c4b49256e2fc71a5e61452baa3b08
billybonks <sebastienstettler@gmail.com>
2016-09-25 13:31:50 +0800

    upgrade ember to 2.9 beta 2

--- config/environment.js
+++ config/environment.js
@@ -10,7 +10,11 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      }
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+       }
     },
 
     APP: {
