ff18fff09412f47585cb8aa2dd5d13e84ccfc27e
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-10-05 13:42:42 -0400

    Added clear for typeahead filter

--- app/components/typeahead-filter.js
+++ app/components/typeahead-filter.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 import TypeAhead from 'hospitalrun/mixins/typeahead';
 export default Ember.Component.extend(TypeAhead, {
-  classNames: ['form-group'],
+  classNames: ['input-group'],
   selectedAction: 'filter',
-  setOnBlur: false
+  setOnBlur: false,
+
+  actions: {
+    clearFilter() {
+      let $input = this.$('.tt-input');
+      $input.val('');
+      this.sendAction('selectedAction');
+    }
+  }
 });
