4c0cd10c6d2255f76f11bbffc3a19cc539ed7d9e
Bj <btecu@outlook.com>
2016-10-11 22:31:01 -0400

    Use `ember-suave` rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     sourceType: 'module'
   },
 
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended'
+  ],
 
   env: {
     'browser': true
@@ -27,6 +30,26 @@ module.exports = {
   },
 
   rules: {
-    'no-console': 0
+    'array-bracket-spacing': 0,
+    'camelcase': 0,
+    'comma-spacing': 0,
+    'ember-suave/no-const-outside-module-scope': 0,
+    'ember-suave/no-direct-property-access': 0,
+    'ember-suave/prefer-destructuring': 0,
+    'ember-suave/require-access-in-comments': 0,
+    'indent': 0,
+    'key-spacing': 0,
+    'keyword-spacing': 0,
+    'new-cap': 0,
+    'no-console': 0,
+    'object-curly-spacing': 0,
+    'object-shorthand': 0,
+    'one-var': 0,
+    'operator-linebreak': 0,
+    'prefer-spread': 0,
+    'prefer-template': 0,
+    'quotes': 0,
+    'spaced-comment': 0,
+    'space-infix-ops': 0
   }
 };
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
     'embertest': true
   },
 
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended'
+  ],
+
   globals: {
     '$': true,
     'authenticateUser': true,
@@ -19,6 +24,26 @@ module.exports = {
   },
 
   rules: {
-    'no-console': 0
+    'array-bracket-spacing': 0,
+    'camelcase': 0,
+    'comma-spacing': 0,
+    'ember-suave/no-const-outside-module-scope': 0,
+    'ember-suave/no-direct-property-access': 0,
+    'ember-suave/prefer-destructuring': 0,
+    'ember-suave/require-access-in-comments': 0,
+    'indent': 0,
+    'key-spacing': 0,
+    'keyword-spacing': 0,
+    'new-cap': 0,
+    'no-console': 0,
+    'object-curly-spacing': 0,
+    'object-shorthand': 0,
+    'one-var': 0,
+    'operator-linebreak': 0,
+    'prefer-spread': 0,
+    'prefer-template': 0,
+    'quotes': 0,
+    'spaced-comment': 0,
+    'space-infix-ops': 0
   }
 };
