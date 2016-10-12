d1981362ed31eb3ea98caee8ec0a0a5ca8e5a552
Bj <btecu@outlook.com>
2016-10-11 21:30:40 -0400

    Replace jshint with eslint. Closes #596 and closes #601.
    `jshint` has merged with `eslint` and only `eslint` will be supported.

---
+++ tests/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    'embertest': true
+  },
+
+  globals: {
+    '$': true,
+    'authenticateUser': true,
+    'invalidateSession': true,
+    'moment': true,
+    'require': true,
+    'runWithPouchDump': true,
+    'select': true,
+    'selectDate': true,
+    'typeAheadFillIn': true,
+    'wait': true,
+    'waitToAppear': true,
+    'waitToDisappear': true
+  }
+};
