38c13299e45a40b2cb2b25f676b03e8a75ed2478
Caitlin Flattery <caitlinflattery@gmail.com>
2016-10-16 22:10:51 -0500

    commented on the require function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* global require */
 /* global module */
 /* global __dirname */
+
+//this is what allows you to use the express build in the app
 var express = require('express');
 
 module.exports = function(app) {
