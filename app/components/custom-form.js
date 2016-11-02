6329d89fde6543fef9d5dad381ce4f75f8746cef
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-11-02 15:26:25 -0400

    Changed to work with new custom-form model.

--- app/components/custom-form.js
+++ app/components/custom-form.js
@@ -6,25 +6,27 @@ export default Ember.Component.extend(SelectValues, {
     let rows = [];
     let form = this.get('form');
     if (!Ember.isEmpty(form)) {
-      let { fields } = form;
+      let fields = form.get('fields');
       let numberOfColumns = this.getWithDefault('form.columns', 1);
       let currentRow = [];
 
       let colCount = 0;
       let colWidth = (12 / numberOfColumns);
       fields.forEach((field) => {
-        if (!field.classNames) {
-          field.classNames = '';
+        let classNames = field.get('classNames');
+        if (!classNames) {
+          classNames = '';
         }
-        let colSpan = field.colSpan || 1;
+        let colSpan = field.get('colSpan') || 1;
         if (colCount === numberOfColumns || (colCount + colSpan) > numberOfColumns) {
           rows.push(currentRow.slice());
           currentRow = [];
           colCount = 0;
         }
-        field.classNames += ` col-sm-${colWidth * colSpan}`;
-        if (field.type === 'select') {
-          field.mappedValues = field.values.map(this.selectValuesMap);
+        classNames += ` col-sm-${colWidth * colSpan}`;
+        field.set('displayClassNames', classNames);
+        if (field.get('type') === 'radio') {
+          field.set('mappedValues', field.get('values').map((value) => Ember.get(value,'label')));
         }
         currentRow.push(field);
         colCount += colSpan;
