74b323de9e2f9e2659defeb00495f32270cd400e
Bj <btecu@outlook.com>
2016-11-16 21:01:17 -0500

    Enable `ember-suave`

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     sourceType: 'module'
   },
 
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended'
+  ],
 
   env: {
     'browser': true
@@ -27,7 +30,9 @@ module.exports = {
   },
 
   rules: {
-    'no-console': 0,
-    'object-curly-spacing': [ 2, 'always']
+    'camelcase': 0,
+    'ember-suave/no-direct-property-access': 0,
+    'ember-suave/require-access-in-comments': 0,
+    'no-console': 0
   }
 };
