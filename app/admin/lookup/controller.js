47ef2a2e0768dd01e9f33e126eb0ca841d6f5529
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-11-28 15:37:48 -0500

    Diagnosis list should be tied to diagnosis model

--- app/admin/lookup/controller.js
+++ app/admin/lookup/controller.js
@@ -47,7 +47,7 @@ export default Ember.Controller.extend(BillingCategories, EKMixin,
         name: this.get('i18n').t('admin.lookup.diagnosisList'),
         value: 'diagnosis_list',
         models: {
-          visit: 'primaryDiagnosis'
+          diagnosis: 'diagnosis'
         }
       }, {
         name: this.get('i18n').t('admin.lookup.cptCodeList'),
