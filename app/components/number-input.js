2bc770e4fdee7ad696f799c223a89c735bf964fa
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-12-05 10:00:43 -0500

    Fixed number-input
    The sanitize function wasn’t firing.  This fix makes sure that it does.

--- app/components/number-input.js
+++ app/components/number-input.js
@@ -1,13 +1,24 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
+import InputComponent from 'ember-rapid-forms/components/em-input';
+export default InputComponent.extend({
   sanitizeFunction(value) {
     return value.replace(new RegExp(/([^0-9|.]+)/g), '');
   },
-  tagName: '',
-  actions: {
-    sanitize(value) {
-      return this.get('sanitizeFunction')(value);
-    }
+
+  focusOut() {
+    this._sanitizeValue();
+    return this._super();
+  },
+
+  keyUp() {
+    this._sanitizeValue();
+    return this._super();
+  },
+
+  _sanitizeValue() {
+    let model = this.get('model');
+    let property = this.get('property');
+    let currentValue = model.get(property);
+    let clean = this.get('sanitizeFunction')(currentValue);
+    model.set(property, clean);
   }
 });
