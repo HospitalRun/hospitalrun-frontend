72195eb77a9e2ab8a83b3742745bc792c3569ba9
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-12-07 11:00:52 -0500

    Fixed error on patient index.

--- app/patients/index/route.js
+++ app/patients/index/route.js
@@ -1,10 +1,11 @@
 import AbstractIndexRoute from 'hospitalrun/routes/abstract-index-route';
 import Ember from 'ember';
 import { translationMacro as t } from 'ember-i18n';
+import UserSession from 'hospitalrun/mixins/user-session';
 
 const { computed } = Ember;
 
-export default AbstractIndexRoute.extend({
+export default AbstractIndexRoute.extend(UserSession, {
   modelName: 'patient',
   newButtonAction: computed(function() {
     if (this.currentUserCan('add_patient')) {
