b89475fb6f2e500820e2c9da624a24aaaa9df9c7
Mike Lay <mike@mkly.io>
2016-12-09 13:05:08 -0500

    Add model unit test for imaging
    - Add model unit test for imaging

---
+++ tests/unit/models/imaging-test.js
@@ -0,0 +1,26 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('imaging', 'Unit | Model | imaging', {
+  needs: [
+    'model:proc-charge',
+    'model:pricing',
+    'model:patient',
+    'model:visit'
+  ]
+});
+
+test('imagingDateAsTime', function(assert) {
+  let imaging = this.subject({
+    imagingDate: new Date(1131593040000)
+  });
+
+  assert.strictEqual(imaging.get('imagingDateAsTime'), 1131593040000);
+});
+
+test('requestedDateAsTime', function(assert) {
+  let imaging = this.subject({
+    requestedDate: new Date(1131593040000)
+  });
+
+  assert.strictEqual(imaging.get('requestedDateAsTime'), 1131593040000);
+});
