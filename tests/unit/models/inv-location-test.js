b328dce15308264092684efe6883d04a723fb996
Mike Lay <mike@mkly.io>
2016-12-10 01:41:12 -0500

    Add availableQuantity translation
    - Add `locationNameWithQuantity` translation for InventoryLocation model
    - Add translation for items in inventory-typeahead component select list
    - Add en translation for `inventory.labels.availableQuantity`
    - Add model unit test for inv-location

---
+++ tests/unit/models/inv-location-test.js
@@ -0,0 +1,34 @@
+import { moduleForModel, test } from 'ember-qunit';
+import tHelper from 'ember-i18n/helper';
+import localeConfig from 'ember-i18n/config/en';
+
+moduleForModel('inv-location', 'Unit | Model | inv-location', {
+  needs: [
+    'ember-validations@validator:local/numericality',
+    'ember-validations@validator:local/acceptance',
+    'ember-validations@validator:local/presence',
+    'service:i18n',
+    'locale:en/translations',
+    'locale:en/config',
+    'util:i18n/missing-message',
+    'util:i18n/compile-template',
+    'config:environment'
+  ],
+  beforeEach() {
+    // set the locale and the config
+    this.container.lookup('service:i18n').set('locale', 'en');
+    this.registry.register('locale:en/config', localeConfig);
+
+    // register t helper
+    this.registry.register('helper:t', tHelper);
+  }
+});
+
+test('locationNameWithQuantity', function(assert) {
+  let invLocation = this.subject({
+    quantity: 3,
+    locationName: 'Test Location'
+  });
+
+  assert.equal(invLocation.get('locationNameWithQuantity'), 'Test Location (3 available)');
+});
