78d7abfadcb36ace6d0851961e2339f64d54bdbb
Mike Lay <mike@mkly.io>
2016-12-15 14:42:33 -0500

    Add unit model test for patient
    - Add unit model test for patient

---
+++ tests/unit/models/patient-test.js
@@ -0,0 +1,40 @@
+import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+import tHelper from 'ember-i18n/helper';
+import localeConfig from 'ember-i18n/config/en';
+
+moduleForModel('patient', 'Unit | Model | patient', {
+  needs: [
+    'ember-validations@validator:local/format',
+    'ember-validations@validator:local/presence',
+    'model:allergy',
+    'model:payment',
+    'model:price-profile',
+    'service:i18n',
+    'locale:en/translations',
+    'locale:en/config',
+    'util:i18n/missing-message',
+    'util:i18n/compile-template',
+    'config:environment'
+  ],
+  beforeEach() {
+    // set the locale and the config
+    this.container.lookup('service:i18n').set('locale', 'en');
+    this.registry.register('locale:en/config', localeConfig);
+
+    Ember.getOwner(this).inject('model', 'i18n', 'service:i18n');
+
+    // register t helper
+    this.registry.register('helper:t', tHelper);
+  }
+});
+
+test('displayAddress', function(assert) {
+  let patient = this.subject({
+    address: '123 Main St.',
+    address2: 'Apt #2',
+    address4: 'Test'
+  });
+
+  assert.strictEqual(patient.get('displayAddress'), '123 Main St., Apt #2, Test');
+});
