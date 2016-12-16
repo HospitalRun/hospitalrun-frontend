73792f29ae56ad12590d890b32231daa657d923e
Mike Lay <mike@mkly.io>
2016-12-16 12:03:42 -0500

    Add model unit test for payment (#880)
    - Add model unit test for payment

---
+++ tests/unit/models/payment-test.js
@@ -0,0 +1,25 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('payment', 'Unit | Model | payment', {
+  needs: [
+    'ember-validations@validator:local/numericality',
+    'ember-validations@validator:local/presence',
+    'model:invoice'
+  ]
+});
+
+test('canRemovePayment', function(assert) {
+  let payment = this.subject({
+    paymentType: 'Deposit'
+  });
+
+  assert.strictEqual(payment.get('canRemovePayment'), true);
+});
+
+test('canRemovePayment false', function(assert) {
+  let payment = this.subject({
+    paymentType: 'Test'
+  });
+
+  assert.strictEqual(payment.get('canRemovePayment'), false);
+});
