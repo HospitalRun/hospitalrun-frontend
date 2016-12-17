af38a215463682ef4f31b6774078cb3145ae4497
Mike Lay <mike@mkly.io>
2016-12-17 13:18:27 -0500

    Fix conditional validation in inventory-batch
    - Fix conditional validation in inventory-batch to fix of
      `haveInvoiceItems`.
    - Add tests for validations
    
    As 'haveInvoiceItems` method has been flipped to now return true
    when `invoiceItems` are empty the validations for the model were made
    incorrect. This commit adjusts those validations to align with the
    updated method.

--- tests/unit/models/inventory-batch-test.js
+++ tests/unit/models/inventory-batch-test.js
@@ -1,4 +1,8 @@
 import { moduleForModel, test } from 'ember-qunit';
+import {
+  testValidPropertyValues,
+  testInvalidPropertyValues
+} from '../../helpers/validate-properties';
 
 moduleForModel('inventory-batch', 'Unit | Model | inventory-batch', {
   needs: [
@@ -9,7 +13,7 @@ moduleForModel('inventory-batch', 'Unit | Model | inventory-batch', {
 
 test('haveInvoiceItems', function(assert) {
   let inventoryBatch = this.subject({
-    invoiceItems: ['test']
+    invoiceItems: ['test have']
   });
 
   assert.strictEqual(inventoryBatch.haveInvoiceItems(), true);
@@ -20,3 +24,30 @@ test('haveInvoiceItems false', function(assert) {
 
   assert.strictEqual(inventoryBatch.haveInvoiceItems(), false);
 });
+
+testValidPropertyValues('dateReceived', ['test dr']);
+testInvalidPropertyValues('dateReceived', [undefined]);
+
+testValidPropertyValues('inventoryItemTypeAhead', ['test']);
+testInvalidPropertyValues('inventoryItemTypeAhead', [undefined]);
+// Test skip validation
+testValidPropertyValues('inventoryItemTypeAhead', [undefined], function(subject) {
+  subject.set('invoiceItems', ['item']);
+});
+
+testValidPropertyValues('quantity', [0.001, 1, '123']);
+testInvalidPropertyValues('quantity', [-1, 0, '-1']);
+// Test skip validation
+testValidPropertyValues('quantity', [-1], function(subject) {
+  subject.set('invoiceItems', ['item']);
+});
+
+testValidPropertyValues('purchaseCost', [0.001, 1, '123']);
+testInvalidPropertyValues('purchaseCost', [-1, 0, '-1']);
+// Test skip validation
+testValidPropertyValues('purchaseCost', [-1], function(subject) {
+  subject.set('invoiceItems', ['item']);
+});
+
+testValidPropertyValues('vendor', ['test']);
+testInvalidPropertyValues('vendor', [undefined]);
