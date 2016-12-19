4d20f8a10e7c050dc40c28d128528fa308ad135d
Mike Lay <mike@mkly.io>
2016-12-19 09:44:26 -0500

    Remove downloadImageFromServer from photo model (#887)
    - Remove `downloadImageFromServer` from photo model
    
    Function will throw an error if used and is not currently used in code
    base

--- app/models/photo.js
+++ app/models/photo.js
@@ -1,6 +1,5 @@
 import AbstractModel from 'hospitalrun/models/abstract';
 import DS from 'ember-data';
-import Ember from 'ember';
 
 export default AbstractModel.extend({
   _attachments: DS.attr(), // Temporarily store file as attachment until it gets uploaded to the server
@@ -11,22 +10,5 @@ export default AbstractModel.extend({
     async: false
   }),
   caption: DS.attr('string'),
-  url: DS.attr('string'),
-
-  downloadImageFromServer(imageRecord) {
-    let me = this;
-    let url = imageRecord.get('url');
-    let xhr = new XMLHttpRequest();
-    if (!Ember.isEmpty(url)) {
-      // Make sure directory exists or is created before downloading.
-      this.getPatientDirectory(imageRecord.get('patientId'));
-      xhr.open('GET', url, true);
-      xhr.responseType = 'blob';
-      xhr.onload = function() {
-        let file = new Blob([xhr.response]);
-        me.addImageToFileStore(file, null, imageRecord);
-      };
-      xhr.send();
-    }
-  }
+  url: DS.attr('string')
 });
