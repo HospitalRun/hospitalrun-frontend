7c2940ed6d5bededfe87f3be33f4e655881d5f48
Hugo Soto <hsotoorellana@gmail.com>
2017-01-05 10:37:02 -0300

    Modified lab model

--- app/models/lab.js
+++ app/models/lab.js
@@ -2,36 +2,34 @@ import AbstractModel from 'hospitalrun/models/abstract';
 import CanEditRequested from 'hospitalrun/mixins/can-edit-requested';
 import DateFormat from 'hospitalrun/mixins/date-format';
 import DS from 'ember-data';
+import Ember from 'ember';
 import PatientValidation from 'hospitalrun/utils/patient-validation';
 import ResultValidation from 'hospitalrun/mixins/result-validation';
 
+const { computed, get } = Ember;
+
 export default AbstractModel.extend(CanEditRequested, DateFormat, ResultValidation, {
-  charges: DS.hasMany('proc-charge', {
-    async: false
-  }),
+  // Attributes
   labDate: DS.attr('date'),
-  labType: DS.belongsTo('pricing', {
-    async: false
-  }),
   notes: DS.attr('string'),
-  patient: DS.belongsTo('patient', {
-    async: false
-  }),
   requestedBy: DS.attr('string'),
   requestedDate: DS.attr('date'),
   result: DS.attr('string'),
   status: DS.attr('string'),
-  visit: DS.belongsTo('visit', {
-    async: false
+
+  // Associations
+  charges: DS.hasMany('proc-charge', { async: false }),
+  labType: DS.belongsTo('pricing', { async: false }),
+  patient: DS.belongsTo('patient', { async: false }),
+  visit: DS.belongsTo('visit', { async: false }),
+
+  labDateAsTime: computed('labDate', function() {
+    return this.dateToTime(get(this, 'labDate'));
   }),
 
-  labDateAsTime: function() {
-    return this.dateToTime(this.get('labDate'));
-  }.property('labDate'),
-
-  requestedDateAsTime: function() {
-    return this.dateToTime(this.get('requestedDate'));
-  }.property('requestedDate'),
+  requestedDateAsTime: computed('requestedDate', function() {
+    return this.dateToTime(get(this, 'requestedDate'));
+  }),
 
   validations: {
     labTypeName: {
