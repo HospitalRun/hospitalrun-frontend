65a0eb7794acae6f14a203a2486a742e29fbd830
Hugo Soto <hsotoorellana@gmail.com>
2017-01-09 11:27:19 -0300

    Modified patient model, using ember style guides (#911)
    * Modified patient model, using ember style guides
    
    * Modified allergy model
    
    * Modified diagnosis model, added comment
    
    * Deleted extra comma
    
    * Modified patient-note model

--- app/models/patient-note.js
+++ app/models/patient-note.js
@@ -1,30 +1,36 @@
 import AbstractModel from 'hospitalrun/models/abstract';
 import Ember from 'ember';
 import DS from 'ember-data';
+
+const { computed } = Ember;
+
 export default AbstractModel.extend({
-  authoredBy: function() {
+  // Attributes
+  /*
+   * if the note was written by one person but dictated or
+   * given on behalf of another, otherwise, this and createdBy are the same.
+   */
+  attribution: DS.attr('string'),
+
+  content: DS.attr('string'),
+  createdBy: DS.attr('string'),
+  date: DS.attr('date'),
+  /* custom list of noteTypes of mixins/patient-note-types */
+  noteType: DS.attr(),
+  /* who is this note about? */
+  patient: DS.belongsTo('patient', { async: false }),
+  /* if this note is related to a visit, make sure it's noted. */
+  visit: DS.belongsTo('visit', { async: false }),
+
+  authoredBy: computed('attribution', 'createdBy', function() {
     if (!Ember.isEmpty(this.get('attribution'))) {
       let i18n = this.get('i18n');
       return `${this.get('createdBy')} ${i18n.t('patients.notes.onBehalfOfCopy')} ${this.get('attribution')}`;
     } else {
       return this.get('createdBy');
     }
-  }.property('attribution', 'createdBy'),
-  // if the note was written by one person but dictated / given on behalf of another, otherwise, this and createdBy are the same
-  attribution: DS.attr('string'),
-  content: DS.attr('string'),
-  createdBy: DS.attr('string'),
-  date: DS.attr('date'),
-  // custom list of noteTypes of mixins/patient-note-types
-  noteType: DS.attr(),
-  // who is this note about?
-  patient: DS.belongsTo('patient', {
-    async: false
   }),
-  // if this note is related to a visit, make sure it's noted.
-  visit: DS.belongsTo('visit', {
-    async: false
-  }),
+
   validations: {
     patient: {
       presence: true
