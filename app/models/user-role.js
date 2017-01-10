9c62304e023cb1a2ea04c9e8f636ddc14647e759
Hugo Soto <hsotoorellana@gmail.com>
2017-01-10 13:46:45 -0300

    Minor models changes (#912)
    * Modified allergy model
    
    * Modified diagnosis model, added comment
    
    * Deleted extra comma
    
    * Modified appointment model
    
    * Modified billing-line-item model
    
    * Added some spaces to appointment
    
    * Modified imaging model
    
    * Modified inventory model
    
    * Modified option, override-price, payment, photo, pricing, procedure, sequence, social-expense, user-role, user and vital models. Only re-arrenge of attributes

--- app/models/user-role.js
+++ app/models/user-role.js
@@ -1,6 +1,8 @@
 import AbstractModel from 'hospitalrun/models/abstract';
 import DS from 'ember-data';
+
 export default AbstractModel.extend({
+  // Attributes
   name: DS.attr('string'),
   capabilities: DS.attr()
 });
