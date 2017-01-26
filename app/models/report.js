3dd403259591464d30ca1764e4b55ae6d745515d
Chima <xhimat123@gmail.com>
2017-01-26 16:25:20 +0100

    feat: added report model

---
+++ app/models/report.js
@@ -0,0 +1,26 @@
+import AbstractModel from 'hospitalrun/models/abstract';
+import DS from 'ember-data';
+
+export default AbstractModel.extend({
+  // Attributes
+  description: DS.attr('string'),
+  reportDate: DS.attr('date'),
+
+  // Associations
+  visit: DS.belongsTo('visit', { async: false }),
+
+  validations: {
+    description: {
+      presence: true
+    },
+
+    reportDate: {
+      presence: true
+    },
+    display_reportDate: {
+      presence: {
+        message: 'Please select a valid date'
+      }
+    }
+  }
+});
