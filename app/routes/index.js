bff9abc083f7875b93e7906598b9c95609476ab4
tangollama <tangollama@gmail.com>
2017-02-17 00:12:30 -0500

    adding beforeModel to index route
    This addresses the issue that there’s really nothing (presently) on the
    homepage.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,41 @@
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import Navigation from 'hospitalrun/mixins/navigation';
+import SetupUserRole from 'hospitalrun/mixins/setup-user-role';
+import UserRoles from 'hospitalrun/mixins/user-roles';
 import Ember from 'ember';
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+const { inject } = Ember;
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, Navigation, SetupUserRole, UserRoles, {
+  session: inject.service(),
+  beforeModel() {
+    // this.setupUserRole();
+    let session = this.get('session');
+    if (session != null) {
+      let role = session.get('data.authenticated.userRole');
+      console.log(`Looking for default route for ${role}`);
+      if (role != null) {
+        let userRole = this.findUserRole(role);
+        console.log('role: ');
+        console.dir(userRole);
+        if (userRole != null && userRole.defaultRoute != null) {
+          let navelement = this.findNavItemByRoute(userRole.defaultRoute);   
+          console.log('nav: ');
+          console.dir(navelement);
+          // this.controllerFor('navigation').set('allowSearch', false);
+          // console.log(`Navigating to ${navelement.defaultRoute}`);
+          // return this.transitionTo(navelement.defaultRoute);
+          console.log(`Navigating to ${navelement.route}`);
+          this.controllerFor('navigation').send('navAction', navelement);
+        }
+      } else {
+        console.log(`No ${role} on index route.`);
+      }
+    } else {
+      console.log('No session on index route.');
+    }
+    return this._super(...arguments);
+  },
+
   afterModel() {
     this.controllerFor('navigation').set('allowSearch', false);
   }
