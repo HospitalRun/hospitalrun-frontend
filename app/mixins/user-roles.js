aed7fb256e13b7683f5c50ac759b615b4a3b98c8
tangollama <tangollama@gmail.com>
2017-02-17 00:10:48 -0500

    Adding defaultRoute to roles

--- app/mixins/user-roles.js
+++ app/mixins/user-roles.js
@@ -3,19 +3,28 @@ export default Ember.Mixin.create({
   userRoles: [
     { name: 'Data Entry', roles: ['Data Entry', 'user'], defaultRoute: 'patients' },
     { name: 'Doctor', roles: ['Doctor', 'user'], defaultRoute: 'patients'  },
-    { name: 'Finance', roles: ['Finance', 'user'], defaultRoute: 'invoices'  },
-    { name: 'Finance Manager', roles: ['Finance Manager', 'user'], defaultRoute: 'invoices' },
-    { name: 'Hospital Administrator', roles: ['Hospital Administrator', 'user'], defaultRoute: 'invoices' },
+    { name: 'Finance', roles: ['Finance', 'user'], defaultRoute: 'invoices.index'  },
+    { name: 'Finance Manager', roles: ['Finance Manager', 'user'], defaultRoute: 'invoices.index' },
+    { name: 'Hospital Administrator', roles: ['Hospital Administrator', 'user'], defaultRoute: 'invoices.index' },
     { name: 'Inventory Manager', roles: ['Inventory Manager', 'user'], defaultRoute: 'inventory' },
-    { name: 'Imaging Technician', roles: ['Imaging Technician', 'user'], defaultRoute: 'imaging' },
-    { name: 'Lab Technician', roles: ['Lab Technician', 'user'], defaultRoute: 'labs' },
-    { name: 'Medical Records Officer', roles: ['Medical Records Officer', 'user'], defaultRoute: 'admin' },
+    { name: 'Imaging Technician', roles: ['Imaging Technician', 'user'], defaultRoute: 'imaging.index' },
+    { name: 'Lab Technician', roles: ['Lab Technician', 'user'], defaultRoute: 'labs.index' },
+    { name: 'Medical Records Officer', roles: ['Medical Records Officer', 'user'], defaultRoute: 'patients' },
     { name: 'Nurse', roles: ['Nurse', 'user'], defaultRoute: 'patients' },
     { name: 'Nurse Manager', roles: ['Nurse Manager', 'user'], defaultRoute: 'patients' },
     { name: 'Patient Administration', roles: ['Patient Administration', 'user'], defaultRoute: 'patients' },
-    { name: 'Pharmacist', roles: ['Pharmacist', 'user'], defaultRoute: 'medication' },
+    { name: 'Pharmacist', roles: ['Pharmacist', 'user'], defaultRoute: 'medication.index' },
     { name: 'Social Worker', roles: ['Social Worker', 'user'], defaultRoute: 'patients' },
-    { name: 'System Administrator', roles: ['System Administrator', 'admin', 'user'], defaultRoute: 'admin' },
-    { name: 'User Administrator', roles: ['User Administrator', 'admin', 'user'], defaultRoute: 'admin' }
-  ]
+    { name: 'System Administrator', roles: ['System Administrator', 'admin', 'user'], defaultRoute: 'patients' },
+    { name: 'User Administrator', roles: ['User Administrator', 'admin', 'user'], defaultRoute: 'users' }
+  ],
+
+  findUserRole(name) {
+    return this.userRoles.find(function(el) {
+      console.log(`Comparing ${el.name} and ${name}`);
+      if (el.name == name) {
+        return el;
+      }
+    });
+  }
 });
