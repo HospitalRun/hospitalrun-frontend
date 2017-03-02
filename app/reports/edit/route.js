397fb9acf73ea2f244b4e2478a1efa9a5742e89b
Adeola Badmus <badmusad@gmail.com>
2017-03-02 22:00:00 +0100

    Add diagnosis container

--- app/reports/edit/route.js
+++ app/reports/edit/route.js
@@ -1,7 +1,7 @@
 import AbstractEditRoute from 'hospitalrun/routes/abstract-edit-route';
 import AddToPatientRoute from 'hospitalrun/mixins/add-to-patient-route';
 import Ember from 'ember';
-import { translationMacro as t } from 'ember-i18n';
+import {translationMacro as t} from 'ember-i18n';
 import PatientVisits from 'hospitalrun/mixins/patient-visits';
 
 export default AbstractEditRoute.extend(AddToPatientRoute, PatientVisits, {
@@ -23,6 +23,13 @@ export default AbstractEditRoute.extend(AddToPatientRoute, PatientVisits, {
     return t(`reports.${type}.titles.${state}`);
   },
 
+  getDiagnosisContainer(visit) {
+    if (visit.get('outPatient')) {
+      return visit;
+    }
+    return null;
+  },
+
   afterModel(model) {
     if (!model.get('visit')) {
       return this.transitionTo('patients');
@@ -31,8 +38,10 @@ export default AbstractEditRoute.extend(AddToPatientRoute, PatientVisits, {
 
   setupController(controller, model) {
     this._super(controller, model);
-    let isOutPatient = model.get('visit.outPatient');
+    let visit = model.get('visit');
+    let isOutPatient = visit.get('outPatient');
     controller.set('isOutPatient', isOutPatient);
+    controller.set('diagnosisContainer', this.getDiagnosisContainer(visit));
     if (isOutPatient) {
       controller.set('nextAppointments', this.getPatientFutureAppointment(model.get('visit'), true));
     } else {
