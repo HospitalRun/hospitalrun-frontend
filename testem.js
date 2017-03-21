318377d2e53b2ff200abaa4a828c234b56db98c8
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-03-21 14:34:17 -0400

    Trying headless chrome

--- testem.js
+++ testem.js
@@ -4,9 +4,14 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+   "Chrome"
   ],
   "launch_in_dev": [
     "Chrome"
-  ]
+  ],
+  "browser_args": {
+    "Chrome": [
+      "--headless --disable-gpu"
+    ]
+  }
 };
