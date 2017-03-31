3e090c56fc68567f8d435c340b6a272fd04ac4d0
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-03-31 12:31:42 -0400

    Ignore login tests when testing Electron
    Electron doesn’t require login when there is only one or less users.

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -15,48 +15,50 @@ module('Acceptance | login', {
   }
 });
 
-test('visiting / redirects user to login', function(assert) {
-  assert.expect(1);
-  runWithPouchDump('default', function() {
-    visit('/');
+if (!window.ELECTRON) {
+  test('visiting / redirects user to login', function(assert) {
+    assert.expect(1);
+    runWithPouchDump('default', function() {
+      visit('/');
 
-    andThen(function() {
-      assert.equal(currentURL(), '/login');
+      andThen(function() {
+        assert.equal(currentURL(), '/login');
+      });
+
     });
-
   });
-});
 
-test('login with correct credentials', function(assert) {
-  login(assert);
-});
-test('login with correct credentials but space around username', function(assert) {
-  login(assert, true);
-});
-
-test('incorrect credentials shows an error message on the screen', function(assert) {
-  assert.expect(2);
-  runWithPouchDump('default', function() {
-    visit('/');
-
-    let errorMessage = 'Username or password is incorrect.';
-
-    stubRequest('post', '/db/_session', function(request) {
-      assert.equal(request.requestBody, 'name=hradmin&password=tset', 'credential are sent to the server');
-      request.error({ 'error': 'unauthorized', 'reason': errorMessage });
-    });
-
-    fillIn('#identification', 'hradmin');
-    fillIn('#password', 'tset');
-    click('button:contains(Sign in)');
-    waitToAppear('.form-signin-alert');
-
-    andThen(function() {
-      assert.equal(find('.form-signin-alert').text(), errorMessage, 'Error reason is shown');
-    });
-
+  test('login with correct credentials', function(assert) {
+    login(assert);
   });
-});
+  test('login with correct credentials but space around username', function(assert) {
+    login(assert, true);
+  });
+
+  test('incorrect credentials shows an error message on the screen', function(assert) {
+    assert.expect(2);
+    runWithPouchDump('default', function() {
+      visit('/');
+
+      let errorMessage = 'Username or password is incorrect.';
+
+      stubRequest('post', '/db/_session', function(request) {
+        assert.equal(request.requestBody, 'name=hradmin&password=tset', 'credential are sent to the server');
+        request.error({ 'error': 'unauthorized', 'reason': errorMessage });
+      });
+
+      fillIn('#identification', 'hradmin');
+      fillIn('#password', 'tset');
+      click('button:contains(Sign in)');
+      waitToAppear('.form-signin-alert');
+
+      andThen(function() {
+        assert.equal(find('.form-signin-alert').text(), errorMessage, 'Error reason is shown');
+      });
+
+    });
+  });
+}
 
 function login(assert, spaceAroundUsername) {
   assert.expect(3);
