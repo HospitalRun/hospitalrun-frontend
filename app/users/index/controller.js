ca94cab666c3080a7b39a940bdb395e15aac6147
tangollama <tangollama@gmail.com>
2017-04-07 13:44:36 -0400

    simplify the controller

--- app/users/index/controller.js
+++ app/users/index/controller.js
@@ -3,13 +3,6 @@ import UserSession from 'hospitalrun/mixins/user-session';
 import Ember from 'ember';
 export default AbstractPagedController.extend(UserSession, {
   addPermission: 'add_user',
-  config: Ember.inject.service(),
   deletePermission: 'delete_user',
-  init() {
-    let user = this.get('model');
-    if (!Ember.isEmpty(user)) {
-      this.get('config').markUserSetupComplete();
-    }
-  },
   sortProperties: ['displayName']
 });
