400314f2e582e785c6229f7b4a7d879363b07746
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-04-11 13:08:21 -0400

    Fix navigation errors during testing
    Resolves issue encountered in #1027 during testing.

--- app/mixins/navigation.js
+++ app/mixins/navigation.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 const { camelize } = Ember.String;
-const { isEqual } = Ember;
+const {
+  get,
+  isEqual,
+  set
+} = Ember;
 
 export default Ember.Mixin.create({
   navItems: [
@@ -365,19 +369,20 @@ export default Ember.Mixin.create({
       // i18n will return a SafeString object, not a string
       return typeof translation === 'string' ? original : translation;
     };
-    return this.get('navItems').map((nav) => {
+    let i18n = get(this, 'i18n');
+    let navItems = get(this, 'navItems');
+    return navItems.map((nav) => {
       let sectionKey = localizationPrefix + camelize(nav.title).toLowerCase();
-      let navTranslated = this.get('i18n').t(sectionKey);
+      let navTranslated = i18n.t(sectionKey);
 
-      Ember.set(nav, 'localizedTitle', translationOrOriginal(navTranslated, nav.title));
+      set(nav, 'localizedTitle', translationOrOriginal(navTranslated, nav.title));
       // Map all of the sub navs, too
-      nav.subnav = nav.subnav.map((sub) => {
+      set(nav, 'subnav', nav.subnav.map((sub) => {
         let subItemKey = `${localizationPrefix}subnav.${camelize(sub.title)}`;
-        let subTranslated = this.get('i18n').t(subItemKey);
-
-        sub.localizedTitle = translationOrOriginal(subTranslated, sub.title);
+        let subTranslated = i18n.t(subItemKey);
+        set(sub, 'localizedTitle', translationOrOriginal(subTranslated, sub.title));
         return sub;
-      });
+      }));
 
       return nav;
     });
