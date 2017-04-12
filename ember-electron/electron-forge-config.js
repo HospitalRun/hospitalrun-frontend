d4f6ae0d0ca34175aca1a0abe3853c9b7ef470a1
tangollama <tangollama@gmail.com>
2017-04-12 15:53:11 -0400

    upgrading to ember electron 2.
    I’m not sure everything is perfect yet, but we’re certainly not worse
    off.

--- ember-electron/electron-forge-config.js
+++ ember-electron/electron-forge-config.js
@@ -1,4 +1,6 @@
 /* eslint quotes: ["error", "double"] */
+const path = require('path');
+
 module.exports = {
   "make_targets": {
     "win32": [
@@ -15,28 +17,47 @@ module.exports = {
   },
   "electronPackagerConfig": {
     "all": true,
-    "appCopyright": "2014-2017 HospitalRun community",
-    "dir": ".",
-    "icon": "icons/icon.icns",
-    "osxSign": true
+    "appCopyright": "Copyright (c) 2017 HospitalRun Foundation",
+    "icon": "assets/icons/icon",
+    "name": "HospitalRun",
+    "osxSign": true,
+    "overwrite": true
   },
   "electronWinstallerConfig": {
+    "exe": "HospitalRun",
+    /* "certificateFile": "",
+    "certificatePassword": "", */
+    "setupIcon": "assets/icons/favicon.ico",
+    "setupExe": "HospitalRun",
     "name": "HospitalRun"
   },
   "electronInstallerDMG": {
-    "background": "icons/bg-img-patients.png",
-    "icon": "icons/favicon.icns",
-    "overwrite": true
+    "background": "assets/icons/bg-img-patients.png",
+    "icon": "assets/icons/favicon.icns",
+    "name": "HospitalRun",
+    "overwrite": true,
+    "window": {
+      "size": {
+        width: 600,
+        height: 600
+      }
+    }
   },
-  "electronInstallerDebian": {},
-  "electronInstallerRedhat": {},
+  "electronInstallerDebian": {
+    "productName": "HospitalRun"
+  },
+  "electronInstallerRedhat": {
+    "productName": "HospitalRun"
+  },
+  "electronInstallerFlatpak": {
+    "productName": "HospitalRun"
+  },
   "github_repository": {
-    "owner": "HospitalRun",
-    "name": "hospitalrun-releases",
+    "owner": "tangollama",
+    "name": "releases",
     "draft": true,
     "prerelease": true
-  }
-  /* ,
+  },
   "publish_targets": {
     "win32": [
       "github"
@@ -47,5 +68,5 @@ module.exports = {
     "linux": [
       "github"
     ]
-  }*/
+  }
 };
