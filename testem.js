4495f07f04c64eed1adbff38d82e84f9c0df80dd
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-05-12 10:10:26 -0400

    Try using chrome beta headless

--- testem.js
+++ testem.js
@@ -1,10 +1,15 @@
 /*jshint node:true*/
 module.exports = {
+  "launchers": {
+    "Chrome Beta": {
+      "command": "google-chrome-beta"
+    }
+  },
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "Chrome"
+    "Chrome Beta"
   ],
   "launch_in_dev": [
     "Chrome"
