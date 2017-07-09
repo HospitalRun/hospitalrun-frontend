98bdf05fc7776c0e5c8d02b6df95eae9f5bf2e2d
Richie Thomas <richie.thomas@blueapron.com>
2017-07-09 00:26:09 -0400

    Initialize i18n preferences upon login; preparing to delete similar logic from app/routes/application.js

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,9 +3,33 @@ import { isAbortError, isTimeoutError } from 'ember-ajax/errors';
 
 let LoginController = Ember.Controller.extend({
   session: Ember.inject.service(),
+  config: Ember.inject.service(),
+  i18n: Ember.inject.service(),
+
   errorMessage: null,
   identification: null,
   password: null,
+
+  _initUserI18nPreference: function() {
+    let configDB = this.get('config.configDB');
+    let username;
+    configDB.get('current_user').then((user) => {
+      username = user.value.name;
+      let db_key = `${username}_i18n`;
+      configDB.get('preferences').then((doc) => {
+        let language = doc.options[db_key] || 'en';
+        this.set('i18n.locale', language);
+      }).catch(err => {
+        let doc = {
+          _id: 'preferences',
+          options: {
+          }
+        };
+        doc.options[db_key] = 'en';
+        configDB.put(doc);
+      })
+    });
+  },
 
   actions: {
     authenticate() {
@@ -13,7 +37,7 @@ let LoginController = Ember.Controller.extend({
       this.get('session').authenticate('authenticator:custom', {
         identification,
         password
-      }).catch((err) => {
+      }).then(this._initUserI18nPreference.bind(this)).catch((err) => {
         if (isAbortError(err) || isTimeoutError(err)) {
           this.set('errorMessage', false);
           this.set('offlineError', true);
