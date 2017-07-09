4f3133c731ed93f3ac8f062e54a58f3433735616
ArtificialQualia <Kendall.Masse@gmail.com>
2017-07-09 19:55:11 -0400

    Added photos to visits and procedures (#1106)
    Updated photo model with visit and procedure field
    
    Changed photo controller to have a editController set by whomever is
      updating the photos, rather than a hardcoded 'patients/edit'
    
    Added pouch views for photos_by_visit and photos_by_procedure
    
    Added photo functionality to route and controllers for visits and 
      procedures
      
    Added photo tab to visit template
    
    Added photo view to procedure template
      
    Updated test fixtures with new pouch views

--- app/patients/photo/controller.js
+++ app/patients/photo/controller.js
@@ -15,7 +15,7 @@ export default AbstractEditController.extend({
   showUpdateButton: true,
 
   database: inject.service(),
-  editController: inject.controller('patients/edit'),
+  editController: null,
   filesystem: inject.service(),
 
   photoFileNotSet: computed('model.photoFile', function() {
@@ -53,7 +53,7 @@ export default AbstractEditController.extend({
 
   afterUpdate(model) {
     let isNew = get(this, 'newModel');
-    let editController = get(this, 'editController');
+    let editController = get(model, 'editController');
     if (isNew) {
       let photoFile = get(model, 'photoFile');
       let saveToDir = get(model, 'saveToDir');
