f22231d9044954c3dc480304c57d70f47020d445
Kendall Masse <kendall.masse@gmail.com>
2017-08-23 15:58:17 -0400

    Added photos to visits and procedures (#1106) (#1121)
    * Added photos to visits and procedures (#1106)
    
    Updated photo model with visit and procedure field
    
    Changed photo controller to have a editController set by whomever is
      updating the photos, rather than a hardcoded 'patients/edit'
    
    Added pouch views for photos_by_visit and photos_by_procedure
    
    Added photo functionality to route and controllers for visits and 
      procedures
      
    Added photo tab to visit template
    
    Added photo view to procedure template
      
    Updated test fixtures with new pouch views
    
    * Updating test fixtures with new photos views
    
    * Reverting text fixtures
    
    * Removing unnessecary line

--- app/procedures/edit/route.js
+++ app/procedures/edit/route.js
@@ -4,12 +4,17 @@ import ChargeRoute from 'hospitalrun/mixins/charge-route';
 import Ember from 'ember';
 import { translationMacro as t } from 'ember-i18n';
 
+const {
+  get
+} = Ember;
+
 export default AbstractEditRoute.extend(AddToPatientRoute, ChargeRoute, {
   editTitle: t('procedures.titles.edit'),
   modelName: 'procedure',
   newTitle: t('procedures.titles.new'),
   pricingCategory: 'Procedure',
   database: Ember.inject.service(),
+  photos: null,
 
   getNewData() {
     return Ember.RSVP.resolve({
@@ -29,5 +34,21 @@ export default AbstractEditRoute.extend(AddToPatientRoute, ChargeRoute, {
       });
       controller.set('medicationList', medicationList);
     });
+    this.store.query('photo', {
+      options: {
+        key: get(model, 'id')
+      },
+      mapReduce: 'photo_by_procedure'
+    }).then(function(photos) {
+      let procedurePhotos = [];
+      procedurePhotos.addObjects(photos);
+      model.set('photos', procedurePhotos);
+    });
+  },
+
+  actions: {
+    deletePhoto(model) {
+      this.controller.send('deletePhoto', model);
+    }
   }
 });
