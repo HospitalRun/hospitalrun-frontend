10b67195bd40d077456b288bfce5205a037f2791
NuttasitBoonwat <nuttasit_lom@hotmail.com>
2017-09-12 10:06:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* global require */
 /* global module */
 /* global __dirname */
-var express = require('express');
+const express = require('express');
 
 module.exports = function(app) {
   app.use(express.static(__dirname + '/prod'));
