396c34330600c24af85a7a36302662911a602e82
Andrey Stukalin <apstukalin@gmail.com>
2018-01-26 14:39:47 +0100

    fix action handler for the date picker

--- app/components/date-picker.js
+++ app/components/date-picker.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    dateSetActionWrapper(newDate) {
+      if (!Ember.isEmpty(this.get('dateSetAction'))) {
+        this.sendAction('dateSetAction', newDate);
+      }
+    }
+  }
 });
