f14aae1897bed4242aaa44957b77dc712dc70c50
Matthew Dorner <mstar444@mail.com>
2018-05-24 03:52:03 -0500

    Acceptance tests for invoice fixes.

--- tests/acceptance/invoices-test.js
+++ tests/acceptance/invoices-test.js
@@ -178,22 +178,162 @@ test('Searching invoices', function(assert) {
     await authenticateUser();
     await visit('/invoices');
 
-    await fillIn('[role="search"] div input', 'Joe');
+    await fillIn('[role="search"] input', 'Joe');
     await click('.glyphicon-search');
 
     assert.equal(currentURL(), '/invoices/search/Joe', 'Searched for Joe');
     assert.dom('.invoice-number').exists({ count: 1 }, 'There is one search item');
 
-    await fillIn('[role="search"] div input', 'joe');
+    await fillIn('[role="search"] input', 'joe');
     await click('.glyphicon-search');
 
     assert.equal(currentURL(), '/invoices/search/joe', 'Searched for all lower case joe');
     assert.dom('.invoice-number').exists({ count: 1 }, 'There is one search item');
 
-    await fillIn('[role="search"] div input', 'ItemNotFound');
+    await fillIn('[role="search"] input', 'ItemNotFound');
     await click('.glyphicon-search');
 
     assert.equal(currentURL(), '/invoices/search/ItemNotFound', 'Searched for ItemNotFound');
     assert.dom('.invoice-number').doesNotExist('There is no search result');
+  });
+});
+
+test('Delete invoice line item detail', function(assert) {
+  return runWithPouchDump('billing', async function() {
+    await authenticateUser();
+    await visit('/invoices/edit/new');
+    assert.equal(currentURL(), '/invoices/edit/new');
+
+    await typeAheadFillIn('.invoice-patient', 'Joe Bagadonuts - TCH 00001');
+    await waitToAppear('.invoice-visit option:contains((Admission))');
+    await select('.invoice-visit', '(Admission)');
+
+    await click('.glyphicon-plus');
+    await click('button:contains(Add Charge)');
+
+    // delete the first line item detail (second Delete button from top, as the very first is for the line item itself)
+    await click($('button:contains(Delete):eq(1)')[0]);
+    await waitToAppear('.modal-dialog');
+    assert.dom('.modal-title').hasText('Delete Charge', 'Delete Charge modal displays');
+    await click('.modal-footer button:contains(Ok)');
+    await waitToDisappear('.modal-dialog');
+
+    // topmost detail showing should now be the blank one we added at beginning of this test
+    assert.equal(find('.detail-quantity').val(), '', 'First line item detail no longer appears.');
+  });
+});
+
+test('Delete invoice line item', function(assert) {
+  return runWithPouchDump('billing', async function() {
+    await authenticateUser();
+    await visit('/invoices/edit/new');
+    assert.equal(currentURL(), '/invoices/edit/new');
+
+    await typeAheadFillIn('.invoice-patient', 'Joe Bagadonuts - TCH 00001');
+    await waitToAppear('.invoice-visit option:contains((Admission))');
+    await select('.invoice-visit', '(Admission)');
+
+    await click('button:contains(Delete)');
+    await waitToAppear('.modal-dialog');
+    assert.dom('.modal-title').hasText('Delete Line Item', 'Delete Line Item modal displays');
+    await click('.modal-footer button:contains(Ok)');
+    await waitToDisappear('.modal-dialog');
+    assert.equal(find('.item-name').val(), 'Pharmacy', 'First line item no longer appears.');
+  });
+});
+
+test('Calculate totals', function(assert) {
+  return runWithPouchDump('billing', async function() {
+    await authenticateUser();
+    await visit('/invoices/edit/new');
+    assert.equal(currentURL(), '/invoices/edit/new');
+
+    await typeAheadFillIn('.invoice-patient', 'Joe Bagadonuts - TCH 00001');
+    await waitToAppear('.invoice-visit option:contains((Admission))');
+    await select('.invoice-visit', '(Admission)');
+
+    // first item, discounts and details
+    await fillIn('.item-discount:eq(0)', '1');
+    await fillIn('.item-national-insurance:eq(0)', '2');
+    await fillIn('.item-private-insurance:eq(0)', '3');
+    await click('.glyphicon-plus');
+    await fillIn('.detail-price:eq(0)', '3');
+    await click('button:contains(Add Charge)');
+    await fillIn($('.detail-quantity:eq(1)'), '60');
+    await fillIn($('.detail-price:eq(1)'), '4');
+
+    // second item, discounts and details
+    await click('.glyphicon-plus');
+    await fillIn($('.item-discount:eq(1)'), '4');
+    await fillIn($('.item-national-insurance:eq(1)'), '6');
+    await fillIn($('.item-private-insurance:eq(1)'), '1');
+    await fillIn($('.detail-price:eq(2)'), '14');
+
+    // add a payment
+    await click('button:contains(Add Payment)');
+    await waitToAppear('.modal-dialog');
+    assert.dom('.modal-title').hasText('Add Payment', 'Add Payment modal displays');
+    await fillIn('.payment-amount input', '100');
+    await click('.update-payment-btn');
+    await waitToAppear('.modal-title:contains(Payment Added)');
+    assert.dom('.modal-title').hasText('Payment Added', 'Payment was saved successfully');
+    await click('.modal-footer button:contains(Ok)');
+    await waitToDisappear('.modal-dialog');
+
+    // total for first 2 items (2 details per item)
+    let expected = +$('.detail-amount-owed:eq(0)').text() + +$('.detail-amount-owed:eq(1)').text();
+    assert.dom($('.item-total:eq(0)')[0]).hasText(expected.toString(), 'First item total is correct');
+    expected = +$('.detail-amount-owed:eq(2)').text() + +$('.detail-amount-owed:eq(3)').text();
+    assert.dom($('.item-total:eq(1)')[0]).hasText(expected.toString(), 'Second item total is correct');
+
+    // amount owed for first 2 items (2 details per item)
+    expected = +$('.item-total:eq(0)').text() - (+$('.item-discount:eq(0)').val() + +$('.item-national-insurance:eq(0)').val() + +$('.item-private-insurance:eq(0)').val());
+    assert.dom($('.item-amount-owed:eq(0)')[0]).hasText(expected.toString(), 'First item amount owed is correct');
+    expected = +$('.item-total:eq(1)').text() - (+$('.item-discount:eq(1)').val() + +$('.item-national-insurance:eq(1)').val() + +$('.item-private-insurance:eq(1)').val());
+    assert.dom($('.item-amount-owed:eq(1)')[0]).hasText(expected.toString(), 'Second item amount owed is correct');
+
+    // detail amount owed for first 4 details
+    expected = $('.detail-quantity:eq(0)').val() * $('.detail-price:eq(0)').val();
+    assert.dom($('.detail-amount-owed:eq(0)')[0]).hasText(expected.toString(), 'First detail amount owed is correct');
+    expected = $('.detail-quantity:eq(1)').val() * $('.detail-price:eq(1)').val();
+    assert.dom($('.detail-amount-owed:eq(1)')[0]).hasText(expected.toString(), 'Second detail amount owed is correct');
+    expected = $('.detail-quantity:eq(2)').val() * $('.detail-price:eq(2)').val();
+    assert.dom($('.detail-amount-owed:eq(2)')[0]).hasText(expected.toString(), 'Third detail amount owed is correct');
+    expected = $('.detail-quantity:eq(3)').val() * $('.detail-price:eq(3)').val();
+    assert.dom($('.detail-amount-owed:eq(3)')[0]).hasText(expected.toString(), 'Fourth detail amount owed is correct');
+
+    // category group and invoice total (same thing as this test only uses 1 category)
+    expected = +$('.item-total:eq(0)').text() + +$('.item-total:eq(1)').text() + +$('.item-total:eq(2)').text() + +$('.item-total:eq(3)').text();
+    assert.dom('.category-group-total').hasText(expected.toString(), 'Category group total is correct');
+    expected = +$('.item-total:eq(0)').text() + +$('.item-total:eq(1)').text() + +$('.item-total:eq(2)').text() + +$('.item-total:eq(3)').text();
+    assert.dom('.total').hasText(expected.toString(), 'Total is correct');
+
+    // category group and invoice discount
+    expected = +$('.item-discount:eq(0)').val() + +$('.item-discount:eq(1)').val();
+    assert.dom('.category-group-discount').hasText(expected.toString(), 'Category group discount is correct');
+    expected = +$('.item-discount:eq(0)').val() + +$('.item-discount:eq(1)').val();
+    assert.dom('.discount').hasText(expected.toString(), 'Discount is correct');
+
+    // category group and invoice national insurance
+    expected = +$('.item-national-insurance:eq(0)').val() + +$('.item-national-insurance:eq(1)').val();
+    assert.dom('.category-group-national-insurance').hasText(expected.toString(), 'Category group national insurance is correct');
+    expected = +$('.item-national-insurance:eq(0)').val() + +$('.item-national-insurance:eq(1)').val();
+    assert.dom('.national-insurance').hasText(expected.toString(), 'National insurance is correct');
+
+    // category group and invoice private insurance
+    expected = +$('.item-private-insurance:eq(0)').val() + +$('.item-private-insurance:eq(1)').val();
+    assert.dom('.category-group-private-insurance').hasText(expected.toString(), 'Category group private insurance is correct');
+    expected = +$('.item-private-insurance:eq(0)').val() + +$('.item-private-insurance:eq(1)').val();
+    assert.dom('.private-insurance').hasText(expected.toString(), 'Private insurance is correct');
+
+    // category group and invoice amount owed
+    expected = +$('.item-amount-owed:eq(0)').text() + +$('.item-amount-owed:eq(1)').text() + +$('.item-amount-owed:eq(2)').text() + +$('.item-amount-owed:eq(3)').text();
+    assert.dom('.category-group-amount-owed').hasText(expected.toString(), 'Category group amount owed is correct');
+    expected = +$('.item-amount-owed:eq(0)').text() + +$('.item-amount-owed:eq(1)').text() + +$('.item-amount-owed:eq(2)').text() + +$('.item-amount-owed:eq(3)').text();
+    assert.dom('.final-patient-responsibility').hasText(expected.toString(), 'Final patient responsibility is correct');
+
+    // remaining balance after paid total is taken off
+    expected = +$('.final-patient-responsibility:eq(0)').text() - Math.abs(+$('.paid-total:eq(0)').text());
+    assert.dom('.remaining-balance').hasText(expected.toString(), 'Remaining balance is correct');
   });
 });
