5d7866930a492909ea703ba5343b8f028a5763b5
Lily Hoskin <lily.hoskin@red-badger.com>
2018-07-18 16:57:10 +0100

    Tidy up missing message util linting issues

--- app/utils/i18n/missing-message.js
+++ app/utils/i18n/missing-message.js
@@ -5,23 +5,20 @@ import config from '../../config/environment';
 const DEFAULT_LOCALE = config.i18n.defaultLocale;
 
 let missingMessage = function(locale, key, data) {
-
-  console.log(locale)
-
   if (locale === DEFAULT_LOCALE || window.env === 'development') {
     return `Missing translation: ${key}`;
   } else {
 
-    Ember.Logger.warn("Missing translation: " + key);
+    Ember.Logger.warn(`Missing translation: ${key}`);
 
     // NOTE This relies on internal APIs and is brittle.
     // Emulating the internals of ember-i18n's translate method
-    const i18n = this;
-    const count = Ember.get(data, 'count');
-    const defaults = Ember.makeArray(Ember.get(data, 'default'));
+    let i18n = this;
+    let count = Ember.get(data, 'count');
+    let defaults = Ember.makeArray(Ember.get(data, 'default'));
     defaults.unshift(key);
-    const localeObj = new Locale(DEFAULT_LOCALE, Ember.getOwner(i18n));
-    const template = localeObj.getCompiledTemplate(defaults, count);
+    let localeObj = new Locale(DEFAULT_LOCALE, Ember.getOwner(i18n));
+    let template = localeObj.getCompiledTemplate(defaults, count);
     return template(data);
   }
 };
