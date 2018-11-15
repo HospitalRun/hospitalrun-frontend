557e0b1113b198c3e5bd0bf6374a920a0e20430a
Chad Carbert <chadcarbert@me.com>
2018-11-15 21:23:27 +0100

    Move nav-menu-test to integration/components

--- tests/integration/components/nav-menu-test.js
+++ tests/integration/components/nav-menu-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 
-module('NavMenuComponent', function(hooks) {
+module('Integration | Component | nav menu', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
