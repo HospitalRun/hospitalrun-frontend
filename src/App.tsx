d198de23cadbcf9de602147103341d4c6c08ed71
Jack Meyer <jackcmeyer@gmail.com>
2019-09-09 22:43:33 -0500

    chore: removes react branding and adds hospital run branding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './hospitalrun-icon-transparent.png';
 import './App.css';
 
 const App: React.FC = () => {
@@ -7,17 +7,6 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
