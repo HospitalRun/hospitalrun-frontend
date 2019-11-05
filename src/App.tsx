75e175665ef744cac1d6ea639e0a732a4c566d7b
Stefano Casasola <stepanello@gmail.com>
2019-11-05 19:00:00 +0100

    fix(lint): fixes eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './hospitalrun-icon-transparent.png';
-import './App.css';
+import React from "react";
+import logo from "./hospitalrun-icon-transparent.png";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  </div>
+);
 
 export default App;
