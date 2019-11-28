2edfe1b247ec5d0e5d5e372e0facb82be535af3f
Jack Meyer <jackcmeyer@gmail.com>
2019-11-28 11:03:06 -0600

    fix(tests): fix matchMediaMock issue when running tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
+import '../__mocks__/matchMediaMock'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from '../App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
