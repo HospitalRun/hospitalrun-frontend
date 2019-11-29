e6c262a08d06f6a6524f612a53da78a1e3c6ce7f
Matteo Vivona <kapa89@me.com>
2019-11-29 15:58:25 +0100

    Update pouchdb.ts

--- src/config/pouchdb.ts
+++ src/config/pouchdb.ts
@@ -4,6 +4,7 @@ export const patients = new PouchDB('patients')
 PouchDB.sync(
   'patients',
   'https://a27fa3db-db4d-4456-8465-da953aee0f5b-bluemix:cd6f332d39f24d2b7cfc89d82f6836d46012ef3188698319b0d5fff177cb2ddc@a27fa3db-db4d-4456-8465-da953aee0f5b-bluemix.cloudantnosqldb.appdomain.cloud/patients',
+  // IBM Cloudant testing and developing database
   {
     live: true,
     retry: true,
