98c25b99c9bdc16078ee6552925a2c183a511c6f
Bruno Costa <brunocgmxus@gmail.com>
2020-01-07 03:09:08 -0300

    fix(i18n): fix file type and import
    fix #1668

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import './i18n'
+import './i18n.tsx'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
