761deda4a2ea4dd5dbf4bbdacad72fac55cd6f3c
Bruno Costa <brunocgmxus@gmail.com>
2020-01-07 00:40:20 -0300

    feat(i18n): reorganize public translation resources
    re #1668

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,9 @@ import Backend from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
+import * as en from './example.json';
 i18n
-  // load translation using xhr -> see /public/app-sections/languages
+  // load translation using xhr -> see /public/languages
   // learn more: https://github.com/i18next/i18next-xhr-backend
   .use(Backend)
   // detect user language
@@ -15,6 +16,24 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    // we init with resources
+    resources: {
+      en: {
+        translations: {
+          "To get started, edit <1>src/App.js</1> and save to reload.":
+            "To get started, edit <1>src/App.js</1> and save to reload.",
+          "Welcome to React": "Welcome to React and react-i18next",
+          welcome: "Hello <br/> <strong>World</strong>"
+        }
+      },
+      de: {
+        translations: {
+          "To get started, edit <1>src/App.js</1> and save to reload.":
+            "Starte in dem du, <1>src/App.js</1> editierst und speicherst.",
+          "Welcome to React": "Willkommen bei React und react-i18next"
+        }
+      }
+    },
     fallbackLng: 'en',
     debug: true,
 
