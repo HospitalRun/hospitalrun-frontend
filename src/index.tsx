45cb83f1712d2b7370388a7c9c269353a18914c4
Jack Meyer <jackcmeyer@gmail.com>
2020-01-11 13:48:39 -0600

    fix(scss): import scss from components library

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '@hospitalrun/components/scss/main.scss'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
