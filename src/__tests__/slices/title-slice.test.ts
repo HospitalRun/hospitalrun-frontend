8574c1167dc94b3c5745e9398de7c8768f73c3e6
Jack Meyer <jackcmeyer@gmail.com>
2020-01-12 17:50:55 -0600

    test: add missing test for title-slice

---
+++ src/__tests__/slices/title-slice.test.ts
@@ -0,0 +1,33 @@
+import title, { updateTitle, changeTitle } from '../../slices/title-slice'
+import { AnyAction } from 'redux'
+
+describe('title slice', () => {
+  describe('title reducer', () => {
+    it('should create the proper initial title reducer', () => {
+      const patientsStore = title(undefined, {} as AnyAction)
+      expect(patientsStore.title).toEqual('')
+    })
+  })
+
+  it('should handle the CHANGE_TITLE action', () => {
+    const expectedTitle = 'expected title'
+    const patientsStore = title(undefined, {
+      type: changeTitle.type,
+      payload: expectedTitle,
+    })
+
+    expect(patientsStore.title).toEqual(expectedTitle)
+  })
+
+  describe('updateTitle', () => {
+    it('should dispatch the CHANGE_TITLE event', async () => {
+      const dispatch = jest.fn()
+      const getState = jest.fn()
+      const expectedTitle = 'expected title'
+
+      await updateTitle(expectedTitle)(dispatch, getState, null)
+
+      expect(dispatch).toHaveBeenCalledWith({ type: changeTitle.type, payload: expectedTitle })
+    })
+  })
+})
