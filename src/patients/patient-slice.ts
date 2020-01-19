687d125bb139d29dd2d6c281363ddc46130fb176
Jack Meyer <jackcmeyer@gmail.com>
2020-01-18 22:40:29 -0600

    feat(patients): add update patient reducers/actions

--- src/patients/patient-slice.ts
+++ src/patients/patient-slice.ts
@@ -24,20 +24,35 @@ const patientSlice = createSlice({
   initialState,
   reducers: {
     getPatientStart: startLoading,
+    updatePatientStart: startLoading,
     getPatientSuccess(state, { payload }: PayloadAction<Patient>) {
       state.isLoading = false
       state.patient = payload
     },
-    updateStart: startLoading,
+    updatePatientSuccess(state, { payload }: PayloadAction<Patient>) {
+      state.isLoading = false
+      state.patient = payload
+    },
   },
 })
 
-export const { getPatientStart, getPatientSuccess, updateStart } = patientSlice.actions
+export const {
+  getPatientStart,
+  getPatientSuccess,
+  updatePatientStart,
+  updatePatientSuccess,
+} = patientSlice.actions
 
 export const fetchPatient = (id: string): AppThunk => async (dispatch) => {
   dispatch(getPatientStart())
   const patient = await PatientRepository.find(id)
   dispatch(getPatientSuccess(patient))
+}
+
+export const updatePatient = (patient: Patient): AppThunk => async (dispatch) => {
+  dispatch(updatePatientStart())
+  const updatedPatient = await PatientRepository.saveOrUpdate(patient)
+  dispatch(updatePatientSuccess(updatedPatient))
 }
 
 export default patientSlice.reducer
