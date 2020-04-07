b214078e146058e28f3d62a0e3e6aa652932ed39
Roy Stewart <roythomasstewart@gmail.com>
2020-04-06 20:49:42 -0400

    refactor(debounce): added more informative types to the debounce hook

--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-export default function (value: any, delayInMilliseconds: number) {
+export default function <T>(value: T, delayInMilliseconds: number): T {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
