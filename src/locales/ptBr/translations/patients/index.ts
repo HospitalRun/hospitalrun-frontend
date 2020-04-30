3fd9d0d445ed8433368a485fa7aa49ea8ab3257f
marcosvega91 <marcomoretti0103@gmail.com>
2020-04-30 15:45:49 +0200

    fix(i18n): add translations of edit patient
    I added the translation of edit patient for all languages

--- src/locales/ptBr/translations/patients/index.ts
+++ src/locales/ptBr/translations/patients/index.ts
@@ -5,6 +5,7 @@ export default {
     viewPatients: 'Exibir pacientes',
     viewPatient: 'Ver paciente',
     newPatient: 'Novo Paciente',
+    editPatient: 'Mudar paciente',
     successfullyCreated: 'Paciente criado com sucesso',
     successfullyAddedNote: 'Adicionou uma nova nota com sucesso',
     successfullyAddedRelatedPerson: 'Adicionou uma pessoa relacionada com sucesso',
