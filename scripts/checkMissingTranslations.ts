9ca5232632503fbefb5ffae4e4d23a10d44fc62c
marcosvega91 <marcomoretti0103@gmail.com>
2020-05-02 09:37:50 +0200

    feat(add new script for checking missing translations): translations
    I add new dep for running typscript script(ts-node). I update the linter path for checking the new
    folder scripts. With the command yarn translation:check you can check if some translations are
    missing. All erros will print in the console
    
    fix #1919

---
+++ scripts/checkMissingTranslations.ts
@@ -0,0 +1,59 @@
+import { Resource, ResourceKey } from 'i18next'
+import i18n from '../src/i18n'
+
+const checkRecursiveTranslation = (
+  searchingPath: string,
+  defaultLanguageObject: ResourceKey,
+  translatedLanguageObject: ResourceKey,
+) => {
+  if (typeof defaultLanguageObject === 'string' || typeof translatedLanguageObject === 'string') {
+    if (typeof defaultLanguageObject === 'object') {
+      console.log(
+        `📙 Found a string for path ${searchingPath} while is and object for the default language`,
+      )
+    }
+    return
+  }
+  const defaultKeys: string[] = Object.keys(defaultLanguageObject)
+  const translatedKeys: string[] = Object.keys(translatedLanguageObject)
+  if (defaultKeys.length === 0 || translatedKeys.length === 0) {
+    return
+  }
+  defaultKeys.forEach((key) => {
+    if (!translatedLanguageObject[key]) {
+      console.log(`📙 The key ${key} is not present for path ${searchingPath}`)
+    } else {
+      checkRecursiveTranslation(
+        `${searchingPath}-->${key}`,
+        defaultLanguageObject[key],
+        translatedLanguageObject[key],
+      )
+    }
+  })
+}
+
+const run = () => {
+  const resources: Resource = i18n.options.resources ?? {}
+  const languages = Object.keys(resources)
+  const defaultLanguage = 'en'
+  console.log(
+    '🏁 Start finding translation problem comparing all languages with the default one (English)',
+  )
+  console.log('')
+  if (!resources[defaultLanguage]) {
+    console.log('📕 We have a big problem.... the english language is not found!')
+    process.exit(1)
+  }
+
+  languages.forEach((language) => {
+    if (language === defaultLanguage) {
+      return
+    }
+    console.log(`🤞Checking ${language}`)
+    console.log('')
+    checkRecursiveTranslation(language, resources[defaultLanguage], resources[language])
+    console.log('')
+  })
+}
+
+run()
