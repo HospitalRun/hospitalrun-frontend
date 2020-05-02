5966cc3d7ffacbca6d53b47a05b247c51f6a89d6
marcosvega91 <marcomoretti0103@gmail.com>
2020-05-02 11:19:06 +0200

    feat(i18n): add italian translation

---
+++ src/locales/it/translations/actions/index.ts
@@ -0,0 +1,15 @@
+export default {
+  actions: {
+    label: 'Azioni',
+    edit: 'Modifica',
+    save: 'Salva',
+    update: 'Aggiorna',
+    complete: 'Completo',
+    delete: 'Rimuovi',
+    cancel: 'Cancella',
+    new: 'Nuovo',
+    list: 'Lista',
+    search: 'Cerca',
+    confirmDelete: 'Cancella conferma',
+  },
+}
