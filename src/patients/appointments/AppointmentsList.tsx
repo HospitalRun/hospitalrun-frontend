cb3cea5d6e1c3c568ecd6536c55d8aa5e6d6e62d
Sourab Banka <58421086+sourabbanka22@users.noreply.github.com>
2020-05-13 07:35:31 +0530

    fix: standardized react router imports in tests and source code (#2067)

--- src/patients/appointments/AppointmentsList.tsx
+++ src/patients/appointments/AppointmentsList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useHistory } from 'react-router'
+import { useHistory } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { TextInput, Button, List, ListItem, Container, Row, Column } from '@hospitalrun/components'
 import { RootState } from '../../store'
