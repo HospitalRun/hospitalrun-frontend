5309a859a4b19617f44183b1d70d0f36af6206d8
Marco Moretti <marcomoretti0103@gmail.com>
2020-05-20 02:41:01 +0200

    feat(incidents): filter incidents (#2087)

--- src/clients/db/IncidentRepository.ts
+++ src/clients/db/IncidentRepository.ts
@@ -1,10 +1,28 @@
 import { incidents } from '../../config/pouchdb'
+import IncidentFilter from '../../incidents/IncidentFilter'
 import Incident from '../../model/Incident'
 import Repository from './Repository'
 
+interface SearchOptions {
+  status: IncidentFilter
+}
 class IncidentRepository extends Repository<Incident> {
   constructor() {
     super(incidents)
+  }
+
+  async search(options: SearchOptions): Promise<Incident[]> {
+    return super.search(IncidentRepository.getSearchCriteria(options))
+  }
+
+  private static getSearchCriteria(options: SearchOptions): any {
+    const statusFilter = options.status !== IncidentFilter.all ? [{ status: options.status }] : []
+    const selector = {
+      $and: statusFilter,
+    }
+    return {
+      selector,
+    }
   }
 }
 
