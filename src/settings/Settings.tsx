60b877af9e7770cdd287719fa9c5b8b1ab4f0bea
kumikokashii <kkashii@uyahoo.com>
2020-05-25 13:46:07 -0700

    feat(settings page): add title and example content to settings page

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -1,5 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Settings = () => <div>Hello</div>
+import SelectWithLabelFormGroup from '../components/input/SelectWithLableFormGroup'
+import useTitle from '../page-header/useTitle'
+
+const Settings = () => {
+  useTitle('Settings') // todo: get from translation
+
+  // todo: pull from real file
+  const languageOptions = [
+    { label: 'English', value: 'en' },
+    { label: 'Italian', value: 'it' },
+  ]
+
+  const [language, setLanguage] = useState(languageOptions[0].value)
+
+  const onLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setLanguage(event.target.value)
+  }
+
+  // Temp
+  const tempOptions = [
+    { label: 'Option A', value: 'option-a' },
+    { label: 'Option B', value: 'option-b' },
+  ]
+
+  const [temp, setTemp] = useState(tempOptions[0].value)
+
+  const onTempChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setTemp(event.target.value)
+  }
+
+  return (
+    <>
+      {/* language selection */}
+      <div className="w-75" style={{ minWidth: '350px', maxWidth: '500px' }}>
+        <SelectWithLabelFormGroup
+          name="language"
+          value={language}
+          label="Language"
+          isEditable
+          options={languageOptions}
+          onChange={onLanguageChange}
+        />
+      </div>
+
+      {/* temp: another setting */}
+      <div className="w-75" style={{ minWidth: '350px', maxWidth: '500px' }}>
+        <SelectWithLabelFormGroup
+          name="temp"
+          value={temp}
+          label="Temp setting"
+          isEditable
+          options={tempOptions}
+          onChange={onTempChange}
+        />
+      </div>
+    </>
+  )
+}
 
 export default Settings
