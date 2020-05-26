ce25987e60d0fb2760430137463bd8b011131637
Linunaut <linunaut@gmail.com>
2020-05-26 02:19:31 +0200

    (i18n) German Translation
    Updated German Translation

--- src/locales/de/translations/index.ts
+++ src/locales/de/translations/index.ts
@@ -1,11 +1,21 @@
 import actions from './actions'
 import dashboard from './dashboard'
+import incidents from './incidents'
+import labs from './labs'
 import patient from './patient'
 import patients from './patients'
+import scheduling from './scheduling'
+import sex from './sex'
+import states from './states'
 
 export default {
   ...actions,
   ...dashboard,
   ...patient,
   ...patients,
+  ...scheduling,
+  ...states,
+  ...sex,
+  ...labs,
+  ...incidents,
 }
