9c0376f797a85c0ebc2982a6538995851ee95371
Alessio Prestileo <alessioprestileo@gmail.com>
2020-05-30 22:13:43 +0200

    feat(network-status): complete implementation of NetworkStatus component

--- src/components/network-status/NetworkStatus.tsx
+++ src/components/network-status/NetworkStatus.tsx
@@ -3,18 +3,27 @@ import React, { useState, useEffect } from 'react'
 import { NetworkStatusMessage } from './NetworkStatusMessage'
 
 export const NetworkStatus = () => {
-  const [isOnline, setIsOnline] = useState(true)
-  const setOnline = () => setIsOnline(true)
-  const setOffline = () => setIsOnline(false)
+  const [networkStatus, setNetworkStatus] = useState({
+    isOnline: true,
+    wasOffline: false,
+  })
+  const handleOnline = () => {
+    setNetworkStatus((prevState) => ({ ...prevState, isOnline: true }))
+  }
+  const handleOffline = () => {
+    setNetworkStatus((prevState) => ({ ...prevState, isOnline: false, wasOffline: true }))
+  }
   useEffect(() => {
-    window.addEventListener('online', setOnline)
-    window.addEventListener('offline', setOffline)
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
 
     return () => {
-      window.removeEventListener('online', setOnline)
-      window.removeEventListener('offline', setOffline)
+      window.removeEventListener('online', handleOnline)
+      window.removeEventListener('offline', handleOffline)
     }
   }, [])
 
-  return <NetworkStatusMessage online={isOnline} />
+  return (
+    <NetworkStatusMessage online={networkStatus.isOnline} wasOffline={networkStatus.wasOffline} />
+  )
 }
--- src/components/network-status/NetworkStatusMessage.tsx
+++ src/components/network-status/NetworkStatusMessage.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles.css'
 
 interface Props {
   online: boolean
+  wasOffline: boolean
 }
 
 const OFFLINE_MESSAGE = 'you are working in offline mode'
 const ONLINE_MESSAGE = 'you are back online'
+const OPACITY_TRANSITION_TIME = 4000
 
 export const NetworkStatusMessage = (props: Props) => {
-  const { online } = props
+  const { online, wasOffline } = props
+  const [display, setDisplay] = useState('flex')
+  const [opacity, setOpacity] = useState(1)
+
+  if (online && !wasOffline) {
+    return null
+  }
+
+  if (!online && opacity !== 1) {
+    setDisplay('flex')
+    setOpacity(1)
+  }
+
+  if (online && wasOffline && opacity !== 0) {
+    setOpacity(0)
+    setTimeout(() => {
+      setDisplay('none')
+    }, OPACITY_TRANSITION_TIME)
+  }
 
   return (
-    <div className={`offline-indicator ${online ? 'online' : 'offline'}`}>
+    <div
+      className={`network-status-message ${online ? 'online' : 'offline'}`}
+      style={{ display, opacity, transition: `opacity ${OPACITY_TRANSITION_TIME}ms ease-in` }}
+    >
       {online ? ONLINE_MESSAGE : OFFLINE_MESSAGE}
     </div>
   )
