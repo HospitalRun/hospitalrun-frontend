35f99ad7d3dc7343af59787113e44ed53620ef3b
Alessio Prestileo <alessioprestileo@gmail.com>
2020-06-06 16:54:04 +0200

    feat(network-status): get rid of css

--- src/components/network-status/NetworkStatusMessage.tsx
+++ src/components/network-status/NetworkStatusMessage.tsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { useNetworkStatus } from './useNetworkStatus'
-import './styles.css'
 
+const ONLINE_COLOR = 'rgba(0, 255, 0, 0.55)'
+const OFFLINE_COLOR = 'rgba(255, 0, 0, 0.65)'
 const OPACITY_TRANSITION_TIME = 4000
+const BASE_STYLE = {
+  height: '50px',
+  pointerEvents: 'none' as 'none',
+  transition: `opacity ${OPACITY_TRANSITION_TIME}ms ease-in`,
+}
 
 export const NetworkStatusMessage = () => {
   const { t } = useTranslation()
   const { isOnline, wasOffline } = useNetworkStatus()
-  const [display, setDisplay] = useState('flex')
+  const [shouldRender, setShouldRender] = useState(true)
   const [opacity, setOpacity] = useState(1)
 
   if (isOnline && !wasOffline) {
@@ -17,21 +23,33 @@ export const NetworkStatusMessage = () => {
   }
 
   if (!isOnline && opacity !== 1) {
-    setDisplay('flex')
+    setShouldRender(true)
     setOpacity(1)
   }
 
   if (isOnline && wasOffline && opacity !== 0) {
     setOpacity(0)
     setTimeout(() => {
-      setDisplay('none')
+      setShouldRender(false)
     }, OPACITY_TRANSITION_TIME)
+  }
+
+  if (!shouldRender) {
+    return null
+  }
+
+  const style = {
+    ...BASE_STYLE,
+    backgroundColor: isOnline ? ONLINE_COLOR : OFFLINE_COLOR,
+    opacity,
   }
 
   return (
     <div
-      className={`network-status-message ${isOnline ? 'online' : 'offline'}`}
-      style={{ display, opacity, transition: `opacity ${OPACITY_TRANSITION_TIME}ms ease-in` }}
+      className={`fixed-bottom d-flex justify-content-center align-items-center ${
+        isOnline ? 'online' : 'offline'
+      }`}
+      style={style}
     >
       {isOnline ? t('networkStatus.online') : t('networkStatus.offline')}
     </div>
