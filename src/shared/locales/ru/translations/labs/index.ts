7fa152bd2367884941e615f02073c0e1f66a4f56
trott <trott.alik@gmail.com>
2020-07-06 16:42:51 +0500

    feat(incidents, scheduling, other): add russian locale

--- src/shared/locales/ru/translations/labs/index.ts
+++ src/shared/locales/ru/translations/labs/index.ts
@@ -1,7 +1,40 @@
 export default {
   labs: {
+    label: 'Анализы',
+    filterTitle: 'Фильтровать по статусу',
+    search: 'Поиск анализов',
+    status: {
+      requested: 'Запрошен',
+      completed: 'Завершен',
+      canceled: 'Отменен',
+    },
+    filter: {
+      all: 'Все статусы',
+    },
+    requests: {
+      label: 'Запросы анализов',
+      new: 'Новый запрос анализа',
+      view: 'Посмотреть анализ',
+      cancel: 'Отменить анализ',
+      complete: 'Завершить анализ',
+      error: {
+        unableToRequest: 'Невозможно создать новый запрос анализа.',
+        unableToComplete: 'Невозможно завершить анализ.',
+        typeRequired: 'Необходимо указать тип.',
+        resultRequiredToComplete: 'Необходимо указать результат.',
+      },
+    },
     lab: {
-      code: 'Лабораторный код',
+      code: 'Код анализа',
+      status: 'Статус',
+      for: 'Для',
+      type: 'Тип',
+      result: 'Результат',
+      notes: 'Заметки',
+      requestedOn: 'Когда запрошен',
+      completedOn: 'Когда завершен',
+      canceledOn: 'Когда отменен',
+      patient: 'Пациент',
     },
   },
 }
