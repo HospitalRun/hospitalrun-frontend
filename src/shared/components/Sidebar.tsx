d756cb68c34eb6aaff020edf69430d4fd3e8ad59
Trott Albert <trott.alik@gmail.com>
2020-07-08 05:44:44 +0500

    feat: add new useTranslator (#2197)

--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -1,9 +1,9 @@
 import { List, ListItem, Icon } from '@hospitalrun/components'
 import React, { useState, CSSProperties } from 'react'
-import { useTranslation } from 'react-i18next'
 import { useSelector, useDispatch } from 'react-redux'
 import { useLocation, useHistory } from 'react-router-dom'
 
+import useTranslator from '../hooks/useTranslator'
 import Permissions from '../model/Permissions'
 import { RootState } from '../store'
 import { updateSidebar } from './component-slice'
@@ -13,7 +13,7 @@ const Sidebar = () => {
   const { sidebarCollapsed } = useSelector((state: RootState) => state.components)
   const permissions = useSelector((state: RootState) => state.user.permissions)
 
-  const { t } = useTranslation()
+  const { t } = useTranslator()
   const path = useLocation()
   const history = useHistory()
   const { pathname } = path
---
+++ src/shared/hooks/useTranslator.ts
@@ -0,0 +1,17 @@
+import { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+
+export default function useTranslator() {
+  const { t } = useTranslation()
+
+  const translate = useCallback(
+    (key: any): any => {
+      return key !== undefined ? t(key) : undefined
+    },
+    [t],
+  )
+
+  return {
+    t: translate,
+  }
+}
