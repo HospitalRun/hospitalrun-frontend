dbb765e5487f14e45c906e1bdb6795cdc556f2a0
Alex Tan <AlexTAN331@outlook.com>
2020-07-09 10:48:50 -0400

    feat(navbar component): add icons to 'quick add' icon in navbar
    fix #2208

--- src/shared/components/navbar/pageMap.tsx
+++ src/shared/components/navbar/pageMap.tsx
@@ -1,6 +1,6 @@
 import Permissions from '../../model/Permissions'
 
-type Page = { permission: Permissions | null; label: string; path: string }
+type Page = { permission: Permissions | null; label: string; path: string; icon: string }
 
 const pageMap: {
   [key: string]: Page
@@ -9,51 +9,61 @@ const pageMap: {
     permission: null,
     label: 'dashboard.label',
     path: '/',
+    icon: 'dashboard',
   },
   newPatient: {
     permission: Permissions.WritePatients,
     label: 'patients.newPatient',
     path: '/patients/new',
+    icon: 'patient-add',
   },
   viewPatients: {
     permission: Permissions.ReadPatients,
     label: 'patients.patientsList',
     path: '/patients',
+    icon: 'patients',
   },
   newAppointment: {
     permission: Permissions.WriteAppointments,
     label: 'scheduling.appointments.new',
     path: '/appointments/new',
+    icon: 'appointment-add',
   },
   viewAppointments: {
     permission: Permissions.ReadAppointments,
     label: 'scheduling.appointments.schedule',
     path: '/appointments',
+    icon: 'appointment',
   },
   newLab: {
     permission: Permissions.RequestLab,
     label: 'labs.requests.new',
     path: '/labs/new',
+    icon: 'add',
   },
   viewLabs: {
     permission: Permissions.ViewLabs,
     label: 'labs.requests.label',
     path: '/labs',
+    icon: 'lab',
   },
   newIncident: {
     permission: Permissions.ReportIncident,
     label: 'incidents.reports.new',
     path: '/incidents/new',
+    icon: 'add',
   },
   viewIncidents: {
     permission: Permissions.ViewIncidents,
     label: 'incidents.reports.label',
     path: '/incidents',
+    icon: 'incident',
   },
   settings: {
     permission: null,
     label: 'settings.label',
     path: '/settings',
+    icon: 'setting',
   },
 }
 
