58b069c6bb0bd835fffea0139ab3fe6182ce34ff
Jan Markus Milan <jmarkusm@me.com>
2020-07-29 16:10:24 -0400

    fix(util): simplified function to return boolean

--- src/patients/util/is-possible-duplicate-patient.ts
+++ src/patients/util/is-possible-duplicate-patient.ts
@@ -1,13 +1,10 @@
 import Patient from '../../shared/model/Patient'
 
 export function isPossibleDuplicatePatient(newPatient: Patient, existingPatient: Patient) {
-  if (
+  return (
     newPatient.givenName === existingPatient.givenName &&
     newPatient.familyName === existingPatient.familyName &&
     newPatient.sex === existingPatient.sex &&
     newPatient.dateOfBirth === existingPatient.dateOfBirth
-  ) {
-    return newPatient
-  }
-  return null
+  )
 }
