21c63987c962d81ca7be568eb0f18df2e768552a
Jack Meyer <jackcmeyer@gmail.com>
2020-07-30 23:47:35 -0500

    chore: add tests for new incident hooks

---
+++ src/__tests__/incidents/hooks/useIncident.test.tsx
@@ -0,0 +1,28 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+
+import useIncident from '../../../incidents/hooks/useIncident'
+import IncidentRepository from '../../../shared/db/IncidentRepository'
+import Incident from '../../../shared/model/Incident'
+import waitUntilQueryIsSuccessful from '../../wait-for-query-test-util'
+
+describe('useIncident', () => {
+  it('should get an incident by id', async () => {
+    const expectedIncidentId = 'some id'
+    const expectedIncident = {
+      id: expectedIncidentId,
+    } as Incident
+    jest.spyOn(IncidentRepository, 'find').mockResolvedValue(expectedIncident)
+
+    let actualData: any
+    await act(async () => {
+      const renderHookResult = renderHook(() => useIncident(expectedIncidentId))
+      const { result } = renderHookResult
+      await waitUntilQueryIsSuccessful(renderHookResult)
+      actualData = result.current.data
+    })
+
+    expect(IncidentRepository.find).toHaveBeenCalledTimes(1)
+    expect(IncidentRepository.find).toBeCalledWith(expectedIncidentId)
+    expect(actualData).toEqual(expectedIncident)
+  })
+})
