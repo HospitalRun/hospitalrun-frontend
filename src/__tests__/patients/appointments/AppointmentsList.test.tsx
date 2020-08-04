28338720eb4a57ee468b0127af2e0f9bca061704
Jan Markus Milan <jmarkusm@me.com>
2020-08-04 19:20:13 -0400

    feat(test): add more test cases for appointments

--- src/__tests__/patients/appointments/AppointmentsList.test.tsx
+++ src/__tests__/patients/appointments/AppointmentsList.test.tsx
@@ -60,26 +60,58 @@ const setup = (patient = expectedPatient, appointments = expectedAppointments) =
 }
 
 describe('AppointmentsList', () => {
-  it('should render a table of appointments', () => {
-    const wrapper = setup()
+  describe('Table', () => {
+    it('should render a list of appointments', () => {
+      const wrapper = setup()
 
-    const table = wrapper.find(Table)
-    const columns = table.prop('columns')
+      const table = wrapper.find(Table)
+      const columns = table.prop('columns')
+      const actions = table.prop('actions') as any
 
-    expect(table).toHaveLength(1)
+      expect(table).toHaveLength(1)
 
-    expect(columns[0]).toEqual(
-      expect.objectContaining({ label: 'scheduling.appointment.startDate', key: 'startDateTime' }),
-    )
-    expect(columns[1]).toEqual(
-      expect.objectContaining({ label: 'scheduling.appointment.endDate', key: 'endDateTime' }),
-    )
-    expect(columns[2]).toEqual(
-      expect.objectContaining({ label: 'scheduling.appointment.location', key: 'location' }),
-    )
-    expect(columns[3]).toEqual(
-      expect.objectContaining({ label: 'scheduling.appointment.type', key: 'type' }),
-    )
+      expect(columns[0]).toEqual(
+        expect.objectContaining({
+          label: 'scheduling.appointment.startDate',
+          key: 'startDateTime',
+        }),
+      )
+      expect(columns[1]).toEqual(
+        expect.objectContaining({ label: 'scheduling.appointment.endDate', key: 'endDateTime' }),
+      )
+      expect(columns[2]).toEqual(
+        expect.objectContaining({ label: 'scheduling.appointment.location', key: 'location' }),
+      )
+      expect(columns[3]).toEqual(
+        expect.objectContaining({ label: 'scheduling.appointment.type', key: 'type' }),
+      )
+
+      expect(actions[0]).toEqual(expect.objectContaining({ label: 'actions.view' }))
+      expect(table.prop('actionsHeaderText')).toEqual('actions.label')
+    })
+
+    it('should navigate to appointment profile on appointment click', async () => {
+      const wrapper = setup()
+      const tr = wrapper.find('tr').at(1)
+
+      act(() => {
+        const onClick = tr.find('button').at(0).prop('onClick') as any
+        onClick({ stopPropagation: jest.fn() })
+      })
+
+      expect(history.location.pathname).toEqual('/appointments/456')
+    })
+  })
+
+  describe('Empty list', () => {
+    it('should render a warning message if there are no appointments', () => {
+      const wrapper = setup(expectedPatient, [])
+      const alert = wrapper.find(components.Alert)
+
+      expect(alert).toHaveLength(1)
+      expect(alert.prop('title')).toEqual('patient.appointments.warning.noAppointments')
+      expect(alert.prop('message')).toEqual('patient.appointments.addAppointmentAbove')
+    })
   })
 
   describe('New appointment button', () => {
