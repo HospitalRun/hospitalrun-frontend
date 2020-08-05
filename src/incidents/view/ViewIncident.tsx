9bff9b0f5b964318cc6752fc8fa3473c9f618a70
Jack Meyer <jackcmeyer@gmail.com>
2020-08-04 22:21:50 -0500

    chore: finished tests for incidents

--- src/incidents/view/ViewIncident.tsx
+++ src/incidents/view/ViewIncident.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
+import useAddBreadcrumbs from '../../page-header/breadcrumbs/useAddBreadcrumbs'
+import useTitle from '../../page-header/title/useTitle'
+import { RootState } from '../../shared/store'
 import ViewIncidentDetails from './ViewIncidentDetails'
 
 const ViewIncident = () => {
   const { id } = useParams()
+  const { permissions } = useSelector((root: RootState) => root.user)
+  useTitle('View Incident')
+  useAddBreadcrumbs([
+    {
+      i18nKey: 'View Incident',
+      location: `/incidents/${id}`,
+    },
+  ])
 
-  return <ViewIncidentDetails incidentId={id} />
+  if (id === undefined || permissions === undefined) {
+    return <></>
+  }
+
+  return <ViewIncidentDetails incidentId={id} permissions={permissions} />
 }
 
 export default ViewIncident
