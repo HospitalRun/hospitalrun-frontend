c4f1e09fb9f8cb52fb526eb9f15106d090c4a35e
Jack Meyer <jackcmeyer@gmail.com>
2020-08-09 16:23:09 -0500

    chore: add user query test util

---
+++ src/__tests__/patients/use-query-test-util.ts
@@ -0,0 +1,15 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+
+import waitUntilQueryIsSuccessful from '../wait-for-query-test-util'
+
+export default async function executeQuery(callback: any) {
+  let actualData: any
+  await act(async () => {
+    const renderHookResult = renderHook(callback)
+    const { result } = renderHookResult
+    await waitUntilQueryIsSuccessful(renderHookResult)
+    actualData = (result as any).current.data
+  })
+
+  return actualData
+}
