9dc78558affd1419801a91e060d0b412b4c09072
Yoseph Ahmed <yosephahmed@aol.com>
2020-08-12 14:11:27 -0400

    feat: added route for visualize component and performed sanity check

---
+++ src/incidents/visualize/VisualizeIncidents.tsx
@@ -0,0 +1,31 @@
+import {} from '@hospitalrun/components'
+import React from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { useParams, useHistory } from 'react-router-dom'
+
+// import useAddBreadcrumbs from '../../page-header/breadcrumbs/useAddBreadcrumbs'
+// import useTitle from '../../page-header/title/useTitle'
+// import useTranslator from '../../shared/hooks/useTranslator'
+// import { RootState } from '../../shared/store'
+
+const VisualizeIncidents = () => (
+  // const dispatch = useDispatch()
+  // const { t } = useTranslator()
+  // const history = useHistory()
+  // const { id } = useParams()
+  // const { incident } = useSelector((state: RootState) => state.incident)
+  // useTitle(incident ? incident.code : '')
+  // const breadcrumbs = [
+  //   {
+  //     i18nKey: incident ? incident.code : '',
+  //     location: `/incidents/${id}`,
+  //   },
+  // ]
+  // useAddBreadcrumbs(breadcrumbs)
+
+  <>
+    <h1>Hello from VisualizeIncidents.tsx!</h1>
+  </>
+)
+
+export default VisualizeIncidents
