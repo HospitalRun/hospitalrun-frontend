3844cd3322f68e12318e90c54161324f1509c8df
Jack Meyer <jackcmeyer@gmail.com>
2020-09-14 22:15:38 -0500

    chore(deps): bump @hospitalrun/components to v3.0.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import '@hospitalrun/components/scss/main.scss'
 
 import './index.css'
 import App from './App'
