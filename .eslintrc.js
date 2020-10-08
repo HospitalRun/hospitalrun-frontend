58d61957d98f492830adefa4cdcd1047c0c73d40
WinstonPoh <winst_940116@hotmail.my>
2020-10-09 12:40:24 +1300

    fix(remove tweaks to misc files)

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'prettier',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
