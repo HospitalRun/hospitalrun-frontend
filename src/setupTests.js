46a5bcdbba32ac782d9d76c0d2efe084c5ea4329
ho1234c <ho1234c@gmail.com>
2020-10-26 22:03:42 +0900

    fix(incidents): running Incidents.test.tsx

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import 'jest-canvas-mock'
 
 import './__mocks__/i18next'
 import './__mocks__/matchMediaMock'
