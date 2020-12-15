949cd401c26fc4ea1b02fdcfe81e54c9d324eb48
Jacob Evans <cloud887@gmail.com>
2020-12-14 19:28:13 -0600

    linter unsued vars

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import React from 'react'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
