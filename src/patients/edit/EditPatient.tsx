28cd6c534311edb050d2cd2ad64ea442f3e92430
Adarsh710 <adarshagrawal777@gmail.com>
2020-12-15 22:53:58 +0530

    fix: alignment of buttons in two more forms
    re #2518

--- src/patients/edit/EditPatient.tsx
+++ src/patients/edit/EditPatient.tsx
@@ -77,7 +77,7 @@ const EditPatient = () => {
         error={updateError}
       />
       <div className="row float-right">
-        <div className="btn-group btn-group-lg">
+        <div className="btn-group btn-group-lg mt-3 mr-3">
           <Button className="btn-save mr-2" color="success" onClick={onSave}>
             {t('actions.save')}
           </Button>
