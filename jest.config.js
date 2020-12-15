65eedab3832d4a8b38e7f9a43d6f323e67f9f392
Jacob Evans <cloud887@gmail.com>
2020-12-14 19:46:00 -0600

    Trying different config for CI/CD for Jest DOM matchers

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx)', '**/?(*.)+(spec|test).+(ts|tsx)'],
   coverageDirectory: './coverage',
   testPathIgnorePatterns: ['<rootDir>/jest.config.js'],
