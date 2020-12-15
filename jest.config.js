8ccc0cd3a57d09c3a58aa25dbe90ea120b809e73
Jacob Evans <cloud887@gmail.com>
2020-12-14 21:36:39 -0600

    Create React App has a file convention

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx)', '**/?(*.)+(spec|test).+(ts|tsx)'],
   coverageDirectory: './coverage',
   testPathIgnorePatterns: ['<rootDir>/jest.config.js'],
