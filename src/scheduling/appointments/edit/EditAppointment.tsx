785fb49fec558fae51b57897c7546fe531c7a938
danilojha <danilojha@gmail.com>
2020-12-18 23:00:32 -0500

    perf: import specific functions from lodash

--- src/scheduling/appointments/edit/EditAppointment.tsx
+++ src/scheduling/appointments/edit/EditAppointment.tsx
@@ -1,5 +1,5 @@
 import { Spinner, Button, Toast } from '@hospitalrun/components'
-import _ from 'lodash'
+import { isEmpty } from 'lodash'
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 
@@ -56,7 +56,7 @@ const EditAppointment = () => {
   }
 
   const onSave = () => {
-    if (_.isEmpty(updateMutateError) && !isErrorUpdate) {
+    if (isEmpty(updateMutateError) && !isErrorUpdate) {
       updateMutate(newAppointment).then(() => {
         Toast('success', t('states.success'), t('scheduling.appointment.successfullyUpdated'))
         history.push(`/appointments/${newAppointment.id}`)
