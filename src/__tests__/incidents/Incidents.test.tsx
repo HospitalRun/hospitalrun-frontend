b560f5670148dd8a790f76bd734db7ca237d2aa9
Jacob Evans <cloud887@gmail.com>
2020-12-24 10:41:11 -0600

    consistent naming
    changed naming of render & renderRTL to setup and render

--- src/__tests__/incidents/Incidents.test.tsx
+++ src/__tests__/incidents/Incidents.test.tsx
@@ -1,4 +1,4 @@
-import { render as rtlRender } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import React from 'react'
 import type { ReactElement, ReactNode } from 'react'
 import { Provider } from 'react-redux'
@@ -21,7 +21,7 @@ type WrapperProps = {
 }
 
 describe('Incidents', () => {
-  function render(permissions: Permissions[], path: string) {
+  function setup(permissions: Permissions[], path: string) {
     const expectedIncident = {
       id: '1234',
       code: '1234',
@@ -47,55 +47,53 @@ describe('Incidents', () => {
       )
     }
 
-    const results = rtlRender(<Incidents />, { wrapper: Wrapper })
-
-    return results
+    return render(<Incidents />, { wrapper: Wrapper })
   }
 
   describe('title', () => {
     it('should have called the useUpdateTitle hook', () => {
-      render([Permissions.ViewIncidents], '/incidents')
+      setup([Permissions.ViewIncidents], '/incidents')
       expect(titleUtil.useUpdateTitle).toHaveBeenCalledTimes(1)
     })
   })
 
   describe('routing', () => {
     describe('/incidents/new', () => {
-      it('should render the new incident screen when /incidents/new is accessed', () => {
-        const { container } = render([Permissions.ReportIncident], '/incidents/new')
+      it('The new incident screen when /incidents/new is accessed', () => {
+        const { container } = setup([Permissions.ReportIncident], '/incidents/new')
 
         expect(container).toHaveTextContent('incidents.reports')
       })
 
       it('should not navigate to /incidents/new if the user does not have ReportIncident permissions', () => {
-        const { container } = render([], '/incidents/new')
+        const { container } = setup([], '/incidents/new')
 
         expect(container).not.toHaveTextContent('incidents.reports')
       })
     })
 
     describe('/incidents/visualize', () => {
-      it('should render the incident visualize screen when /incidents/visualize is accessed', () => {
-        const { container } = render([Permissions.ViewIncidentWidgets], '/incidents/visualize')
+      it('The incident visualize screen when /incidents/visualize is accessed', () => {
+        const { container } = setup([Permissions.ViewIncidentWidgets], '/incidents/visualize')
 
         expect(container.querySelector('.chartjs-render-monitor')).toBeInTheDocument()
       })
 
       it('should not navigate to /incidents/visualize if the user does not have ViewIncidentWidgets permissions', () => {
-        const { container } = render([], '/incidents/visualize')
+        const { container } = setup([], '/incidents/visualize')
 
         expect(container.querySelector('.chartjs-render-monitor')).not.toBeInTheDocument()
       })
     })
 
     describe('/incidents/:id', () => {
-      it('should render the view incident screen when /incidents/:id is accessed', () => {
-        const { container } = render([Permissions.ViewIncident], '/incidents/1234')
+      it('The view incident screen when /incidents/:id is accessed', () => {
+        const { container } = setup([Permissions.ViewIncident], '/incidents/1234')
         expect(container.querySelectorAll('div').length).toBe(3)
       })
 
       it('should not navigate to /incidents/:id if the user does not have ViewIncident permissions', () => {
-        const { container } = render([], '/incidents/1234')
+        const { container } = setup([], '/incidents/1234')
         expect(container.querySelectorAll('div').length).not.toBe(3)
       })
     })
