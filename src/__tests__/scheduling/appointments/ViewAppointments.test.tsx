2152f4f81aa76b4999488a3a68530f7216243691
kiranjd <kiranjd8@gmail.com>
2020-12-26 01:35:43 +0530

    fix: coverts tests to RTL

--- src/__tests__/scheduling/appointments/ViewAppointments.test.tsx
+++ src/__tests__/scheduling/appointments/ViewAppointments.test.tsx
@@ -1,5 +1,4 @@
-// import { Calendar } from '@hospitalrun/components'
-import { act, render as rtlRender } from '@testing-library/react'
+import { act, render as rtlRender, waitFor, screen } from '@testing-library/react'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
@@ -33,19 +32,17 @@ const expectedPatient = {
   fullName: 'patient full name',
 } as Patient
 
-beforeAll(() => {
-  jest.spyOn(titleUtil, 'useUpdateTitle').mockReturnValue(jest.fn())
-  jest.spyOn(ButtonBarProvider, 'useButtonToolbarSetter').mockImplementation(() => jest.fn())
-  jest.spyOn(AppointmentRepository, 'findAll').mockResolvedValue(expectedAppointments)
-  jest.spyOn(PatientRepository, 'find').mockResolvedValue(expectedPatient)
-})
-
 beforeEach(() => {
   jest.clearAllMocks()
 })
 
 describe('ViewAppointments', () => {
   const render = () => {
+    jest.spyOn(titleUtil, 'useUpdateTitle').mockReturnValue(jest.fn())
+    jest.spyOn(ButtonBarProvider, 'useButtonToolbarSetter').mockImplementation(() => jest.fn())
+    jest.spyOn(AppointmentRepository, 'findAll').mockResolvedValue(expectedAppointments)
+    jest.spyOn(PatientRepository, 'find').mockResolvedValue(expectedPatient)
+
     const mockStore = createMockStore<RootState, any>([thunk])
 
     return rtlRender(
@@ -61,6 +58,7 @@ describe('ViewAppointments', () => {
 
   it('should have called the useUpdateTitle hook', () => {
     render()
+
     expect(titleUtil.useUpdateTitle).toHaveBeenCalled()
   })
 
@@ -68,26 +66,15 @@ describe('ViewAppointments', () => {
     await act(async () => {
       await render()
     })
+
     expect(ButtonBarProvider.useButtonToolbarSetter).toHaveBeenCalled()
   })
 
   it('should render a calendar with the proper events', async () => {
-    await act(async () => {
-      await render()
+    render()
+
+    await waitFor(() => {
+      expect(screen.getByText(expectedPatient.fullName as string)).toBeInTheDocument()
     })
-
-    // const expectedEvents = [
-    //   {
-    //     id: expectedAppointments[0].id,
-    //     start: new Date(expectedAppointments[0].startDateTime),
-    //     end: new Date(expectedAppointments[0].endDateTime),
-    //     title: 'patient full name',
-    //     allDay: false,
-    //   },
-    // ]
-
-    // const calendar = wrapper.find(Calendar)
-    // expect(calendar).toHaveLength(1)
-    // expect(calendar.prop('events')).toEqual(expectedEvents)
   })
 })
