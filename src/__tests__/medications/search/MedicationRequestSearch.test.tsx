3c85370f96499456ec33908442d3b7673af16a3c
codyarose <crose992@gmail.com>
2020-12-27 14:11:07 -0600

    Convert MedicationRequestSearch.test.tsx to RTL

--- src/__tests__/medications/search/MedicationRequestSearch.test.tsx
+++ src/__tests__/medications/search/MedicationRequestSearch.test.tsx
@@ -1,87 +1,81 @@
-import { mount, ReactWrapper } from 'enzyme'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
-import { act } from 'react-dom/test-utils'
 
 import MedicationSearchRequest from '../../../medications/models/MedicationSearchRequest'
 import MedicationRequestSearch from '../../../medications/search/MedicationRequestSearch'
-import SelectWithLabelFormGroup from '../../../shared/components/input/SelectWithLabelFormGroup'
-import TextInputWithLabelFormGroup from '../../../shared/components/input/TextInputWithLabelFormGroup'
 
 describe('Medication Request Search', () => {
   const setup = (givenSearchRequest: MedicationSearchRequest = { text: '', status: 'draft' }) => {
     const onChangeSpy = jest.fn()
 
-    const wrapper = mount(
-      <MedicationRequestSearch searchRequest={givenSearchRequest} onChange={onChangeSpy} />,
-    )
-    wrapper.update()
-
-    return { wrapper: wrapper as ReactWrapper, onChangeSpy }
+    return {
+      onChangeSpy,
+      ...render(
+        <MedicationRequestSearch searchRequest={givenSearchRequest} onChange={onChangeSpy} />,
+      ),
+    }
   }
 
-  it('should render a select component with the default value', () => {
-    const expectedSearchRequest: MedicationSearchRequest = { text: '', status: 'draft' }
-    const { wrapper } = setup(expectedSearchRequest)
+  it('should render a select component with the default value', async () => {
+    setup()
+    const select = screen.getByRole('combobox')
+    expect(select).not.toBeDisabled()
+    expect(select).toHaveDisplayValue(/medications\.status\.draft/i)
+    expect(screen.getByText(/medications\.filterTitle/i)).toBeInTheDocument()
+    userEvent.click(select)
 
-    const select = wrapper.find(SelectWithLabelFormGroup)
-    expect(select.prop('label')).toEqual('medications.filterTitle')
-    expect(select.prop('options')).toEqual([
-      { label: 'medications.filter.all', value: 'all' },
-      { label: 'medications.status.draft', value: 'draft' },
-      { label: 'medications.status.active', value: 'active' },
-      { label: 'medications.status.onHold', value: 'on hold' },
-      { label: 'medications.status.completed', value: 'completed' },
-      { label: 'medications.status.enteredInError', value: 'entered in error' },
-      { label: 'medications.status.canceled', value: 'canceled' },
-      { label: 'medications.status.unknown', value: 'unknown' },
-    ])
-    expect(select.prop('defaultSelected')).toEqual([
-      {
-        label: 'medications.status.draft',
-        value: 'draft',
-      },
-    ])
-    expect(select.prop('isEditable')).toBeTruthy()
+    const optionsContent = screen
+      .getAllByRole('option')
+      .map((option) => option.lastElementChild?.innerHTML)
+
+    expect(
+      optionsContent.includes(
+        'medications.filter.all' &&
+          'medications.status.draft' &&
+          'medications.status.active' &&
+          'medications.status.onHold' &&
+          'medications.status.completed' &&
+          'medications.status.enteredInError' &&
+          'medications.status.canceled' &&
+          'medications.status.unknown',
+      ),
+    ).toBe(true)
   })
 
   it('should update the search request when the filter updates', () => {
     const expectedSearchRequest: MedicationSearchRequest = { text: '', status: 'draft' }
     const expectedNewValue = 'canceled'
-    const { wrapper, onChangeSpy } = setup(expectedSearchRequest)
+    const { onChangeSpy } = setup()
+    const select = screen.getByRole('combobox')
 
-    act(() => {
-      const select = wrapper.find(SelectWithLabelFormGroup)
-      const onChange = select.prop('onChange') as any
-      onChange([expectedNewValue])
-    })
+    userEvent.type(select, `{selectall}{backspace}${expectedNewValue}`)
+    userEvent.click(screen.getByText(expectedNewValue))
 
-    expect(onChangeSpy).toHaveBeenCalledTimes(1)
+    expect(onChangeSpy).toHaveBeenCalled()
     expect(onChangeSpy).toHaveBeenCalledWith({ ...expectedSearchRequest, status: expectedNewValue })
   })
 
   it('should render a text input with the default value', () => {
     const expectedSearchRequest: MedicationSearchRequest = { text: '', status: 'draft' }
-    const { wrapper } = setup(expectedSearchRequest)
+    setup(expectedSearchRequest)
+    const textInput = screen.getByLabelText(/medications\.search/i)
 
-    const textInput = wrapper.find(TextInputWithLabelFormGroup)
-    expect(textInput.prop('label')).toEqual('medications.search')
-    expect(textInput.prop('placeholder')).toEqual('medications.search')
-    expect(textInput.prop('value')).toEqual(expectedSearchRequest.text)
-    expect(textInput.prop('isEditable')).toBeTruthy()
+    expect(textInput).toBeInTheDocument()
+    expect(textInput).toHaveAttribute('placeholder', 'medications.search')
+    expect(textInput).toHaveAttribute('value', expectedSearchRequest.text)
+    expect(textInput).not.toBeDisabled()
   })
 
   it('should update the search request when the text input is updated', () => {
     const expectedSearchRequest: MedicationSearchRequest = { text: '', status: 'draft' }
     const expectedNewValue = 'someNewValue'
-    const { wrapper, onChangeSpy } = setup(expectedSearchRequest)
+    const { onChangeSpy } = setup(expectedSearchRequest)
+    const textInput = screen.getByLabelText(/medications\.search/i)
 
-    act(() => {
-      const textInput = wrapper.find(TextInputWithLabelFormGroup)
-      const onChange = textInput.prop('onChange') as any
-      onChange({ target: { value: expectedNewValue } })
-    })
+    userEvent.type(textInput, `{selectall}{backspace}${expectedNewValue}`)
 
-    expect(onChangeSpy).toHaveBeenCalledTimes(1)
+    expect(onChangeSpy).toHaveBeenCalled()
     expect(onChangeSpy).toHaveBeenCalledWith({ ...expectedSearchRequest, text: expectedNewValue })
   })
 })
