a6791431f4e306a37acfa5ca12f35d5f881c333b
codyarose <crose992@gmail.com>
2020-12-27 00:14:32 -0600

    Convert NewLabRequest.test.tsx to RTL

--- src/__tests__/labs/requests/NewLabRequest.test.tsx
+++ src/__tests__/labs/requests/NewLabRequest.test.tsx
@@ -1,6 +1,6 @@
-import { Button, Typeahead, Label, Alert } from '@hospitalrun/components'
+import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import format from 'date-fns/format'
-import { mount, ReactWrapper } from 'enzyme'
 import { createMemoryHistory } from 'history'
 import React from 'react'
 import { act } from 'react-dom/test-utils'
@@ -13,9 +13,6 @@ import NewLabRequest from '../../../labs/requests/NewLabRequest'
 import * as validationUtil from '../../../labs/utils/validate-lab'
 import { LabError } from '../../../labs/utils/validate-lab'
 import * as titleUtil from '../../../page-header/title/TitleContext'
-import SelectWithLabelFormGroup from '../../../shared/components/input/SelectWithLabelFormGroup'
-import TextFieldWithLabelFormGroup from '../../../shared/components/input/TextFieldWithLabelFormGroup'
-import TextInputWithLabelFormGroup from '../../../shared/components/input/TextInputWithLabelFormGroup'
 import LabRepository from '../../../shared/db/LabRepository'
 import PatientRepository from '../../../shared/db/PatientRepository'
 import Lab from '../../../shared/model/Lab'
@@ -27,192 +24,141 @@ import { expectOneConsoleError } from '../../test-utils/console.utils'
 const mockStore = createMockStore<RootState, any>([thunk])
 describe('New Lab Request', () => {
   let history: any
-  const setup = async (
-    store = mockStore({ title: '', user: { user: { id: 'userId' } } } as any),
+  const setup = (
+    store = mockStore({
+      title: '',
+      user: { user: { id: 'userId' } },
+    } as any),
   ) => {
     history = createMemoryHistory()
     history.push(`/labs/new`)
     jest.spyOn(titleUtil, 'useUpdateTitle').mockImplementation(() => jest.fn())
 
-    let wrapper: any
-    await act(async () => {
-      wrapper = await mount(
-        <Provider store={store}>
-          <Router history={history}>
-            <titleUtil.TitleProvider>
-              <NewLabRequest />
-            </titleUtil.TitleProvider>
-          </Router>
-        </Provider>,
-      )
-    })
-
-    wrapper.find(NewLabRequest).props().updateTitle = jest.fn()
-    wrapper.update()
-    return { wrapper: wrapper as ReactWrapper }
+    return render(
+      <Provider store={store}>
+        <Router history={history}>
+          <titleUtil.TitleProvider>
+            <NewLabRequest />
+          </titleUtil.TitleProvider>
+        </Router>
+      </Provider>,
+    )
   }
 
   describe('form layout', () => {
+    const expectedDate = new Date()
+    const expectedNotes = 'expected notes'
+    const expectedLab = {
+      patient: '1234567',
+      type: 'expected type',
+      status: 'requested',
+      notes: [expectedNotes],
+      id: '1234',
+      requestedOn: expectedDate.toISOString(),
+    } as Lab
+
+    const visits = [
+      {
+        startDateTime: new Date().toISOString(),
+        id: 'visit_id',
+        type: 'visit_type',
+      },
+    ] as Visit[]
+    const expectedPatient = { id: expectedLab.patient, fullName: 'Jim Bob', visits } as Patient
+
+    beforeAll(() => {
+      jest.resetAllMocks()
+      jest.spyOn(PatientRepository, 'search').mockResolvedValue([expectedPatient])
+    })
     it('should have called the useUpdateTitle hook', async () => {
-      await setup()
+      setup()
       expect(titleUtil.useUpdateTitle).toHaveBeenCalledTimes(1)
     })
 
     it('should render a patient typeahead', async () => {
-      const { wrapper } = await setup()
-      const typeaheadDiv = wrapper.find('.patient-typeahead')
+      setup()
+      const typeaheadInput = screen.getByPlaceholderText(/labs.lab.patient/i)
 
-      expect(typeaheadDiv).toBeDefined()
-
-      const label = typeaheadDiv.find(Label)
-      const typeahead = typeaheadDiv.find(Typeahead)
-
-      expect(label).toBeDefined()
-      expect(label.prop('text')).toEqual('labs.lab.patient')
-      expect(typeahead).toBeDefined()
-      expect(typeahead.prop('placeholder')).toEqual('labs.lab.patient')
-      expect(typeahead.prop('searchAccessor')).toEqual('fullName')
+      expect(screen.getByText(/labs\.lab\.patient/i)).toBeInTheDocument()
+      userEvent.type(typeaheadInput, 'Jim Bob')
+      expect(typeaheadInput).toHaveDisplayValue('Jim Bob')
     })
 
     it('should render a type input box', async () => {
-      const { wrapper } = await setup()
-      const typeInputBox = wrapper.find(TextInputWithLabelFormGroup)
-
-      expect(typeInputBox).toBeDefined()
-      expect(typeInputBox.prop('label')).toEqual('labs.lab.type')
-      expect(typeInputBox.prop('isRequired')).toBeTruthy()
-      expect(typeInputBox.prop('isEditable')).toBeTruthy()
+      setup()
+      expect(screen.getByText(/labs\.lab\.type/i)).toHaveAttribute(
+        'title',
+        'This is a required input',
+      )
+      expect(screen.getByLabelText(/labs\.lab\.type/i)).toBeInTheDocument()
+      expect(screen.getByLabelText(/labs\.lab\.type/i)).not.toBeDisabled()
     })
 
     it('should render a notes text field', async () => {
-      const { wrapper } = await setup()
-      const notesTextField = wrapper.find(TextFieldWithLabelFormGroup)
-
-      expect(notesTextField).toBeDefined()
-      expect(notesTextField.prop('label')).toEqual('labs.lab.notes')
-      expect(notesTextField.prop('isRequired')).toBeFalsy()
-      expect(notesTextField.prop('isEditable')).toBeTruthy()
+      setup()
+      expect(screen.getByLabelText(/labs\.lab\.notes/i)).not.toBeDisabled()
+      expect(screen.getByText(/labs\.lab\.notes/i)).not.toHaveAttribute(
+        'title',
+        'This is a required input',
+      )
     })
 
     it('should render a visit select', async () => {
-      const { wrapper } = await setup()
-      const visitSelect = wrapper.find(SelectWithLabelFormGroup)
+      setup()
+      const selectLabel = screen.getByText(/patient\.visit/i)
+      const selectInput = screen.getByPlaceholderText('-- Choose --')
 
-      expect(visitSelect).toBeDefined()
-      expect(visitSelect.prop('label') as string).toEqual('patient.visit')
-      expect(visitSelect.prop('isRequired')).toBeFalsy()
-      expect(visitSelect.prop('defaultSelected')).toEqual([])
+      expect(selectInput).toBeInTheDocument()
+      expect(selectInput).toHaveDisplayValue([''])
+      expect(selectLabel).toBeInTheDocument()
+      expect(selectLabel).not.toHaveAttribute('title', 'This is a required input')
     })
 
     it('should render a save button', async () => {
-      const { wrapper } = await setup()
-      const saveButton = wrapper.find(Button).at(0)
-      expect(saveButton).toBeDefined()
-      expect(saveButton.text().trim()).toEqual('labs.requests.new')
+      setup()
+      expect(screen.getByRole('button', { name: /labs\.requests\.new/i })).toBeInTheDocument()
     })
 
     it('should render a cancel button', async () => {
-      const { wrapper } = await setup()
-      const cancelButton = wrapper.find(Button).at(1)
-      expect(cancelButton).toBeDefined()
-      expect(cancelButton.text().trim()).toEqual('actions.cancel')
+      setup()
+      expect(screen.getByRole('button', { name: /actions\.cancel/i })).toBeInTheDocument()
     })
 
     it('should clear visit when patient is changed', async () => {
-      const { wrapper } = await setup()
-      const patientTypeahead = wrapper.find(Typeahead)
-      const expectedDate = new Date()
-      const expectedNotes = 'expected notes'
-      const expectedLab = {
-        patient: '1234567',
-        type: 'expected type',
-        status: 'requested',
-        notes: [expectedNotes],
-        id: '1234',
-        requestedOn: expectedDate.toISOString(),
-      } as Lab
+      setup()
+      const typeaheadInput = screen.getByPlaceholderText(/labs.lab.patient/i)
+      const visitsInput = screen.getByPlaceholderText('-- Choose --')
 
-      const visits = [
-        {
-          startDateTime: new Date().toISOString(),
-          id: 'visit_id',
-          type: 'visit_type',
-        },
-      ] as Visit[]
-
-      await act(async () => {
-        const onChange = patientTypeahead.prop('onChange') as any
-        await onChange([{ id: expectedLab.patient, visits }] as Patient[])
-      })
-      wrapper.update()
+      userEvent.type(typeaheadInput, 'Jim Bob')
+      expect(await screen.findByText(/Jim Bob/i)).toBeVisible()
+      userEvent.click(screen.getByText(/Jim Bob/i))
+      expect(typeaheadInput).toHaveDisplayValue(/Jim Bob/i)
+      userEvent.click(visitsInput)
 
       // The visits dropdown should be populated with the patient's visits.
-      expect(wrapper.find(SelectWithLabelFormGroup).prop('options')).toEqual([
-        {
-          label: `${visits[0].type} at ${format(
+      userEvent.click(
+        await screen.findByRole('link', {
+          name: `${visits[0].type} at ${format(
             new Date(visits[0].startDateTime),
             'yyyy-MM-dd hh:mm a',
           )}`,
-          value: 'visit_id',
-        },
-      ])
-      await act(async () => {
-        const onChange = patientTypeahead.prop('onChange')
-        await onChange([] as Patient[])
-      })
+        }),
+      )
+      expect(visitsInput).toHaveDisplayValue(
+        `${visits[0].type} at ${format(new Date(visits[0].startDateTime), 'yyyy-MM-dd hh:mm a')}`,
+      )
 
-      wrapper.update()
+      userEvent.clear(typeaheadInput)
       // The visits dropdown option should be reset when the patient is changed.
-      expect(wrapper.find(SelectWithLabelFormGroup).prop('options')).toEqual([])
-    })
-
-    it('should support selecting a visit', async () => {
-      const { wrapper } = await setup()
-      const patientTypeahead = wrapper.find(Typeahead)
-      const expectedDate = new Date()
-      const expectedNotes = 'expected notes'
-      const expectedLab = {
-        patient: '123456789',
-        type: 'expected type',
-        status: 'requested',
-        notes: [expectedNotes],
-        id: '1234',
-        requestedOn: expectedDate.toISOString(),
-      } as Lab
-
-      const visits = [
-        {
-          startDateTime: new Date().toISOString(),
-          id: 'visit_id',
-          type: 'visit_type',
-        },
-      ] as Visit[]
-      const visitOptions = [
-        {
-          label: `${visits[0].type} at ${format(
+      expect(visitsInput).toHaveDisplayValue('')
+      expect(
+        screen.queryByRole('link', {
+          name: `${visits[0].type} at ${format(
             new Date(visits[0].startDateTime),
             'yyyy-MM-dd hh:mm a',
           )}`,
-          value: 'visit_id',
-        },
-      ]
-      expect(wrapper.find(SelectWithLabelFormGroup).prop('defaultSelected')).toEqual([])
-      await act(async () => {
-        const onChange = patientTypeahead.prop('onChange') as any
-        await onChange([{ id: expectedLab.patient, visits }] as Patient[])
-      })
-      wrapper.update()
-      expect(wrapper.find(SelectWithLabelFormGroup).prop('defaultSelected')).toEqual([])
-      const dropdown = wrapper.find(SelectWithLabelFormGroup)
-      await act(async () => {
-        // The onChange method takes a list of possible values,
-        // but our dropdown is single-select, so the argument passed to onChange()
-        // is a list with just one element. This is why we pass
-        // the whole array `visitOptions` as opposed to `visitOptions[0]`.
-        await (dropdown.prop('onChange') as any)(visitOptions.map((option) => option.value))
-      })
-      wrapper.update()
-      expect(wrapper.find(SelectWithLabelFormGroup).prop('defaultSelected')).toEqual(visitOptions)
+        }),
+      ).not.toBeInTheDocument()
     })
   })
 
@@ -223,43 +169,41 @@ describe('New Lab Request', () => {
       type: 'some type error',
     } as LabError
 
-    expectOneConsoleError(error)
-    jest.spyOn(validationUtil, 'validateLabRequest').mockReturnValue(error)
+    beforeAll(() => {
+      jest.resetAllMocks()
+      jest.spyOn(validationUtil, 'validateLabRequest').mockReturnValue(error)
+      expectOneConsoleError(error)
+    })
 
     it('should display errors', async () => {
-      const { wrapper } = await setup()
-
-      const saveButton = wrapper.find(Button).at(0)
+      setup()
+      const saveButton = screen.getByRole('button', { name: /labs\.requests\.new/i })
       await act(async () => {
-        const onClick = saveButton.prop('onClick') as any
-        await onClick()
+        userEvent.click(saveButton)
       })
+      const alert = screen.findByRole('alert')
+      const { getByText: getByTextInAlert } = within(await alert)
+      const patientInput = screen.getByPlaceholderText(/labs\.lab\.patient/i)
+      const typeInput = screen.getByPlaceholderText(/labs\.lab\.type/i)
 
-      wrapper.update()
-
-      const alert = wrapper.find(Alert)
-      const typeInput = wrapper.find(TextInputWithLabelFormGroup)
-      const patientTypeahead = wrapper.find(Typeahead)
-
-      expect(alert.prop('message')).toEqual(error.message)
-      expect(alert.prop('title')).toEqual('states.error')
-      expect(alert.prop('color')).toEqual('danger')
-
-      expect(patientTypeahead.prop('isInvalid')).toBeTruthy()
-
-      expect(typeInput.prop('feedback')).toEqual(error.type)
-      expect(typeInput.prop('isInvalid')).toBeTruthy()
+      expect(getByTextInAlert(error.message)).toBeInTheDocument()
+      expect(getByTextInAlert(/states\.error/i)).toBeInTheDocument()
+      expect(await alert).toHaveClass('alert-danger')
+      expect(patientInput).toHaveClass('is-invalid')
+      expect(typeInput).toHaveClass('is-invalid')
+      if (error.type) {
+        expect(typeInput.nextSibling).toHaveTextContent(error.type)
+      }
     })
   })
 
   describe('on cancel', () => {
     it('should navigate back to /labs', async () => {
-      const { wrapper } = await setup()
-      const cancelButton = wrapper.find(Button).at(1)
+      setup()
+      const cancelButton = screen.getByRole('button', { name: /actions\.cancel/i })
 
-      act(() => {
-        const onClick = cancelButton.prop('onClick') as any
-        onClick({} as React.MouseEvent<HTMLButtonElement>)
+      await act(async () => {
+        userEvent.click(cancelButton)
       })
 
       expect(history.location.pathname).toEqual('/labs')
@@ -268,57 +212,47 @@ describe('New Lab Request', () => {
 
   describe('on save', () => {
     let labRepositorySaveSpy: any
+    const store = mockStore({
+      lab: { status: 'loading', error: {} },
+      user: { user: { id: 'fake id' } },
+    } as any)
+
     const expectedDate = new Date()
     const expectedNotes = 'expected notes'
     const expectedLab = {
-      patient: '12345',
+      patient: '1234567',
       type: 'expected type',
       status: 'requested',
       notes: [expectedNotes],
       id: '1234',
       requestedOn: expectedDate.toISOString(),
     } as Lab
-    const store = mockStore({
-      lab: { status: 'loading', error: {} },
-      user: { user: { id: 'fake id' } },
-    } as any)
 
-    beforeEach(() => {
+    const visits = [
+      {
+        startDateTime: new Date().toISOString(),
+        id: 'visit_id',
+        type: 'visit_type',
+      },
+    ] as Visit[]
+    const expectedPatient = { id: expectedLab.patient, fullName: 'Billy', visits } as Patient
+
+    beforeAll(() => {
       jest.resetAllMocks()
-      Date.now = jest.fn(() => expectedDate.valueOf())
+      jest.spyOn(PatientRepository, 'search').mockResolvedValue([expectedPatient])
       labRepositorySaveSpy = jest.spyOn(LabRepository, 'save').mockResolvedValue(expectedLab as Lab)
-
-      jest
-        .spyOn(PatientRepository, 'search')
-        .mockResolvedValue([{ id: expectedLab.patient, fullName: 'some full name' }] as Patient[])
     })
 
     it('should save the lab request and navigate to "/labs/:id"', async () => {
-      const { wrapper } = await setup(store)
+      setup(store)
+      userEvent.type(screen.getByPlaceholderText(/labs.lab.patient/i), 'Billy')
+      expect(await screen.findByText(/billy/i)).toBeVisible()
+      userEvent.click(screen.getByText(/billy/i))
+      userEvent.type(screen.getByPlaceholderText(/labs\.lab\.type/i), expectedLab.type)
+      userEvent.type(screen.getByLabelText(/labs\.lab\.notes/i), expectedNotes)
 
-      const patientTypeahead = wrapper.find(Typeahead)
       await act(async () => {
-        const onChange = patientTypeahead.prop('onChange')
-        await onChange([{ id: expectedLab.patient, visits: [] as Visit[] }] as Patient[])
-      })
-
-      const typeInput = wrapper.find(TextInputWithLabelFormGroup)
-      act(() => {
-        const onChange = typeInput.prop('onChange') as any
-        onChange({ currentTarget: { value: expectedLab.type } })
-      })
-
-      const notesTextField = wrapper.find(TextFieldWithLabelFormGroup)
-      act(() => {
-        const onChange = notesTextField.prop('onChange') as any
-        onChange({ currentTarget: { value: expectedNotes } })
-      })
-      wrapper.update()
-
-      const saveButton = wrapper.find(Button).at(0)
-      await act(async () => {
-        const onClick = saveButton.prop('onClick') as any
-        await onClick()
+        userEvent.click(screen.getByRole('button', { name: /labs\.requests\.new/i }))
       })
 
       expect(labRepositorySaveSpy).toHaveBeenCalledTimes(1)
@@ -328,7 +262,6 @@ describe('New Lab Request', () => {
           type: expectedLab.type,
           notes: expectedLab.notes,
           status: 'requested',
-          requestedOn: expectedDate.toISOString(),
         }),
       )
       expect(history.location.pathname).toEqual(`/labs/${expectedLab.id}`)
