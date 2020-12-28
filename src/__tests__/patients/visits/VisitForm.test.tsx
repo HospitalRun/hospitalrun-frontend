0545855ed8bb03b8387977ab78c5b9358ca4bc16
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-12-29 00:57:34 +0200

    Converted VisitForm.test.tsx to RTL

--- src/__tests__/patients/visits/VisitForm.test.tsx
+++ src/__tests__/patients/visits/VisitForm.test.tsx
@@ -1,8 +1,8 @@
-import { Alert } from '@hospitalrun/components'
-import { screen, render } from '@testing-library/react'
-import addDays from 'date-fns/addDays'
+import { fireEvent, screen, render, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { format } from 'date-fns'
 import React from 'react'
-import { act } from 'react-dom/test-utils'
+import selectEvent from 'react-select-event'
 
 import VisitForm from '../../../patients/visits/VisitForm'
 import Patient from '../../../shared/model/Patient'
@@ -18,7 +18,8 @@ describe('Visit Form', () => {
     status: VisitStatus.Arrived,
     reason: 'routine visit',
     location: 'main',
-  }
+  } as Visit
+
   const setup = (disabled = false, initializeVisit = true, error?: any) => {
     onVisitChangeSpy = jest.fn()
     const mockPatient = { id: '123' } as Patient
@@ -34,166 +35,210 @@ describe('Visit Form', () => {
   }
 
   it('should render a start date picker', () => {
-    setup()
+    const { container } = setup()
 
-    // const startDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'startDateTime')
+    const startDateLabel = screen.getByText(/patient.visits.startdatetime/i)
+    expect(startDateLabel).toBeInTheDocument()
+    const requiredIcon = within(startDateLabel).getByRole('img', {
+      hidden: true,
+    })
+    expect(requiredIcon).toBeInTheDocument()
+    expect(requiredIcon.getAttribute('data-icon')).toEqual('asterisk')
 
-    // expect(startDateTimePicker).toHaveLength(1)
-    // expect(startDateTimePicker.prop('patient.visit.startDateTime'))
-    // expect(startDateTimePicker.prop('isRequired')).toBeTruthy()
-    // expect(startDateTimePicker.prop('value')).toEqual(new Date(visit.startDateTime))
+    const startDateTimePicker = container.querySelectorAll(
+      '.react-datepicker__input-container input',
+    )[0]
+    expect(startDateTimePicker).toBeInTheDocument()
+    expect(startDateTimePicker).toHaveDisplayValue(
+      format(new Date(visit.startDateTime), 'MM/dd/yyyy h:mm aa'),
+    )
   })
 
-  it('should call the on change handler when start date changes', () => {
-    const expectedNewStartDateTime = addDays(1, new Date().getDate())
-    setup(false, false)
+  it('should call the on change handler when start date changes', async () => {
+    const expectedNewStartDateTime = new Date('01/01/2021 2:56 PM')
+    const { container } = setup(false, false)
 
-    // const startDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'startDateTime')
-    // act(() => {
-    //   const onChange = startDateTimePicker.prop('onChange') as any
-    //   onChange(expectedNewStartDateTime)
-    // })
+    const startDateTimePicker = container.querySelectorAll(
+      '.react-datepicker__input-container input',
+    )[0]
+    fireEvent.change(startDateTimePicker, {
+      target: { value: expectedNewStartDateTime },
+    })
 
-    // expect(onVisitChangeSpy).toHaveBeenCalledWith({
-    //   startDateTime: expectedNewStartDateTime.toISOString(),
-    // })
+    expect(onVisitChangeSpy).toHaveBeenCalledWith({
+      startDateTime: expectedNewStartDateTime.toISOString(),
+    })
   })
 
   it('should render an end date picker', () => {
-    setup()
+    const { container } = setup()
 
-    // const endDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'endDateTime')
+    const endDateLabel = screen.getByText(/patient.visits.enddatetime/i)
+    expect(endDateLabel).toBeInTheDocument()
+    const requiredIcon = within(endDateLabel).getByRole('img', {
+      hidden: true,
+    })
+    expect(requiredIcon).toBeInTheDocument()
+    expect(requiredIcon.getAttribute('data-icon')).toEqual('asterisk')
 
-    // expect(endDateTimePicker).toHaveLength(1)
-    // expect(endDateTimePicker.prop('patient.visit.endDateTime'))
-    // expect(endDateTimePicker.prop('isRequired')).toBeTruthy()
-    // expect(endDateTimePicker.prop('value')).toEqual(new Date(visit.endDateTime))
+    const endDateTimePicker = container.querySelectorAll(
+      '.react-datepicker__input-container input',
+    )[1]
+    expect(endDateTimePicker).toBeInTheDocument()
+    expect(endDateTimePicker).toHaveDisplayValue(
+      format(new Date(visit.endDateTime), 'MM/dd/yyyy h:mm aa'),
+    )
   })
 
   it('should call the on change handler when end date changes', () => {
-    const expectedNewEndDateTime = addDays(1, new Date().getDate())
-    setup(false, false)
+    const expectedNewEndDateTime = new Date('01/01/2021 2:56 PM')
+    const { container } = setup(false, false)
 
-    // const endDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'endDateTime')
-    // act(() => {
-    //   const onChange = endDateTimePicker.prop('onChange') as any
-    //   onChange(expectedNewEndDateTime)
-    // })
+    const endDateTimePicker = container.querySelectorAll(
+      '.react-datepicker__input-container input',
+    )[1]
+    userEvent.type(endDateTimePicker, format(expectedNewEndDateTime, 'MM/dd/yyyy h:mm aa'))
+    expect(endDateTimePicker).toHaveDisplayValue(
+      format(new Date(expectedNewEndDateTime), 'MM/dd/yyyy h:mm aa'),
+    )
+    fireEvent.change(endDateTimePicker, {
+      target: { value: expectedNewEndDateTime },
+    })
 
-    // expect(onVisitChangeSpy).toHaveBeenCalledWith({
-    //   endDateTime: expectedNewEndDateTime.toISOString(),
-    // })
+    expect(onVisitChangeSpy).toHaveBeenCalledWith({
+      endDateTime: expectedNewEndDateTime.toISOString(),
+    })
   })
 
   it('should render a type input', () => {
     setup()
 
-    // const typeInput = wrapper.findWhere((w) => w.prop('name') === 'type')
-    // expect(typeInput).toHaveLength(1)
-    // expect(typeInput.prop('patient.visit.type'))
-    // expect(typeInput.prop('value')).toEqual(visit.type)
+    const typeLabel = screen.getByText(/patient.visits.type/i)
+    expect(typeLabel).toBeInTheDocument()
+
+    const typeInput = screen.getByPlaceholderText(/patient.visits.type/i)
+    expect(typeInput).toBeInTheDocument()
+
+    expect(typeInput).toHaveDisplayValue(visit.type)
   })
 
   it('should call the on change handler when type changes', () => {
     const expectedNewType = 'some new type'
     setup(false, false)
 
-    // const typeInput = wrapper.findWhere((w) => w.prop('name') === 'type')
-    // act(() => {
-    //   const onChange = typeInput.prop('onChange') as any
-    //   onChange({ currentTarget: { value: expectedNewType } })
-    // })
+    const typeInput = screen.getByPlaceholderText(/patient.visits.type/i)
+    userEvent.type(typeInput, expectedNewType)
 
-    // expect(onVisitChangeSpy).toHaveBeenCalledWith({ type: expectedNewType })
+    expect(typeInput).toHaveDisplayValue(expectedNewType)
+
+    expect(onVisitChangeSpy).toHaveBeenCalledWith({ type: expectedNewType })
   })
 
   it('should render a status selector', () => {
     setup()
 
-    // const statusSelector = wrapper.findWhere((w) => w.prop('name') === 'status')
+    const statusLabel = screen.getByText(/patient.visits.status/i)
+    expect(statusLabel).toBeInTheDocument()
 
-    // expect(statusSelector).toHaveLength(1)
-    // expect(statusSelector.prop('patient.visit.status'))
-    // expect(statusSelector.prop('isRequired')).toBeTruthy()
-    // expect(statusSelector.prop('defaultSelected')[0].value).toEqual(visit.status)
-    // expect(statusSelector.prop('options')).toEqual(
-    //   Object.values(VisitStatus).map((v) => ({ label: v, value: v })),
-    // )
+    const requiredIcon = within(statusLabel).getByRole('img', {
+      hidden: true,
+    })
+    expect(requiredIcon).toBeInTheDocument()
+    expect(requiredIcon.getAttribute('data-icon')).toEqual('asterisk')
+
+    const statusSelector = screen.getByPlaceholderText('-- Choose --')
+    expect(statusSelector).toBeInTheDocument()
+
+    expect(statusSelector).toHaveDisplayValue(visit.status)
+
+    selectEvent.openMenu(statusSelector)
+    Object.values(VisitStatus).forEach((status) => {
+      expect(screen.getByRole('option', { name: status })).toBeInTheDocument()
+    })
   })
 
-  it('should call the on change handler when status changes', () => {
+  it('should call the on change handler when status changes', async () => {
     const expectedNewStatus = VisitStatus.Finished
     setup(false, false)
-    // act(() => {
-    //   const statusSelector = wrapper.findWhere((w) => w.prop('name') === 'status')
-    //   const onChange = statusSelector.prop('onChange') as any
-    //   onChange([expectedNewStatus])
-    // })
 
-    // expect(onVisitChangeSpy).toHaveBeenCalledWith({ status: expectedNewStatus })
+    const statusInput = screen.getByPlaceholderText('-- Choose --')
+
+    await selectEvent.select(statusInput, expectedNewStatus)
+
+    expect(onVisitChangeSpy).toHaveBeenCalledWith({ status: expectedNewStatus })
   })
 
   it('should render a reason input', () => {
     setup()
+    const reasonLabel = screen.getByText(/patient.visits.reason/i)
+    expect(reasonLabel).toBeInTheDocument()
 
-    // const reasonInput = wrapper.findWhere((w) => w.prop('name') === 'reason')
+    const requiredIcon = within(reasonLabel).getByRole('img', {
+      hidden: true,
+    })
+    expect(requiredIcon).toBeInTheDocument()
+    expect(requiredIcon.getAttribute('data-icon')).toEqual('asterisk')
 
-    // expect(reasonInput).toHaveLength(1)
-    // expect(reasonInput.prop('patient.visit.reason'))
-    // expect(reasonInput.prop('isRequired')).toBeTruthy()
-    // expect(reasonInput.prop('value')).toEqual(visit.reason)
+    const reasonInput = screen.getAllByRole('textbox')[3]
+    expect(reasonInput).toBeInTheDocument()
+
+    expect(reasonInput).toHaveDisplayValue(visit.reason)
   })
 
   it('should call the on change handler when reason changes', () => {
     const expectedNewReason = 'some new reason'
     setup(false, false)
-    // act(() => {
-    //   const reasonInput = wrapper.findWhere((w) => w.prop('name') === 'reason')
-    //   const onChange = reasonInput.prop('onChange') as any
-    //   onChange({ currentTarget: { value: expectedNewReason } })
-    // })
 
-    // expect(onVisitChangeSpy).toHaveBeenCalledWith({ reason: expectedNewReason })
+    const reasonInput = screen.getAllByRole('textbox')[3]
+
+    userEvent.clear(reasonInput)
+    userEvent.paste(reasonInput, expectedNewReason)
+
+    expect(onVisitChangeSpy).toHaveBeenCalledWith({ reason: expectedNewReason })
   })
 
   it('should render a location input', () => {
     setup()
 
-    // const locationInput = wrapper.findWhere((w) => w.prop('name') === 'location')
+    const locationLabel = screen.getByText(/patient.visits.location/i)
+    const requiredIcon = within(locationLabel).getByRole('img', {
+      hidden: true,
+    })
+    expect(requiredIcon.getAttribute('data-icon')).toEqual('asterisk')
 
-    // expect(locationInput).toHaveLength(1)
-    // expect(locationInput.prop('patient.visit.location'))
-    // expect(locationInput.prop('isRequired')).toBeTruthy()
-    // expect(locationInput.prop('value')).toEqual(visit.location)
+    const locationInput = screen.getByPlaceholderText(/patient.visits.location/i)
+    expect(locationInput).toHaveDisplayValue(visit.location)
   })
 
   it('should call the on change handler when location changes', () => {
     const expectedNewLocation = 'some new location'
     setup(false, false)
-    // act(() => {
-    //   const locationInput = wrapper.findWhere((w) => w.prop('name') === 'location')
-    //   const onChange = locationInput.prop('onChange') as any
-    //   onChange({ currentTarget: { value: expectedNewLocation } })
-    // })
 
-    // expect(onVisitChangeSpy).toHaveBeenCalledWith({ location: expectedNewLocation })
+    const locationInput = screen.getByPlaceholderText(/patient.visits.location/i)
+    userEvent.type(locationInput, expectedNewLocation)
+
+    expect(onVisitChangeSpy).toHaveBeenCalledWith({ location: expectedNewLocation })
   })
 
-  it('should render all of the fields as disabled if the form is disabled', () => {
-    setup(true)
-    // const startDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'startDateTime')
-    // const endDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'endDateTime')
-    // const typeInput = wrapper.findWhere((w) => w.prop('name') === 'type')
-    // const statusSelector = wrapper.findWhere((w) => w.prop('name') === 'status')
-    // const reasonInput = wrapper.findWhere((w) => w.prop('name') === 'reason')
-    // const locationInput = wrapper.findWhere((w) => w.prop('name') === 'location')
+  it('should render all of the fields as disabled if the form is disabled', async () => {
+    const { container } = setup(true)
+    const startDateTimePicker = container.querySelectorAll(
+      '.react-datepicker__input-container input',
+    )[0]
+    const endDateTimePicker = container.querySelectorAll(
+      '.react-datepicker__input-container input',
+    )[1]
+    const typeInput = screen.getByPlaceholderText(/patient.visits.type/i)
+    const statusSelector = screen.getByPlaceholderText('-- Choose --')
+    const reasonInput = screen.getAllByRole('textbox')[3]
+    const locationInput = screen.getByPlaceholderText(/patient.visits.location/i)
 
-    // expect(startDateTimePicker.prop('isEditable')).toBeFalsy()
-    // expect(endDateTimePicker.prop('isEditable')).toBeFalsy()
-    // expect(typeInput.prop('isEditable')).toBeFalsy()
-    // expect(statusSelector.prop('isEditable')).toBeFalsy()
-    // expect(reasonInput.prop('isEditable')).toBeFalsy()
-    // expect(locationInput.prop('isEditable')).toBeFalsy()
+    expect(startDateTimePicker.hasAttribute('disabled')).toBeTruthy()
+    expect(endDateTimePicker.hasAttribute('disabled')).toBeTruthy()
+    expect(typeInput.hasAttribute('disabled')).toBeTruthy()
+    expect(statusSelector.hasAttribute('disabled')).toBeTruthy()
+    expect(reasonInput.hasAttribute('disabled')).toBeTruthy()
+    expect(locationInput.hasAttribute('disabled')).toBeTruthy()
   })
 
   it('should render the form fields in an error state', () => {
@@ -209,32 +254,15 @@ describe('Visit Form', () => {
 
     setup(false, false, expectedError)
 
-    // const alert = wrapper.find(Alert)
-    // const startDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'startDateTime')
-    // const endDateTimePicker = wrapper.findWhere((w) => w.prop('name') === 'endDateTime')
-    // const typeInput = wrapper.findWhere((w) => w.prop('name') === 'type')
-    // const statusSelector = wrapper.findWhere((w) => w.prop('name') === 'status')
-    // const reasonInput = wrapper.findWhere((w) => w.prop('name') === 'reason')
-    // const locationInput = wrapper.findWhere((w) => w.prop('name') === 'location')
+    const alert = screen.getByRole('alert')
+    const statusSelector = screen.getByPlaceholderText('-- Choose --')
 
-    // expect(alert).toHaveLength(1)
-    // expect(alert.prop('message')).toEqual(expectedError.message)
-
-    // expect(startDateTimePicker.prop('isInvalid')).toBeTruthy()
-    // expect(startDateTimePicker.prop('feedback')).toEqual(expectedError.startDateTime)
-
-    // expect(endDateTimePicker.prop('isInvalid')).toBeTruthy()
-    // expect(endDateTimePicker.prop('feedback')).toEqual(expectedError.endDateTime)
-
-    // expect(typeInput.prop('isInvalid')).toBeTruthy()
-    // expect(typeInput.prop('feedback')).toEqual(expectedError.type)
-
-    // expect(statusSelector.prop('isInvalid')).toBeTruthy()
-
-    // expect(reasonInput.prop('isInvalid')).toBeTruthy()
-    // expect(reasonInput.prop('feedback')).toEqual(expectedError.reason)
-
-    // expect(locationInput.prop('isInvalid')).toBeTruthy()
-    // expect(locationInput.prop('feedback')).toEqual(expectedError.location)
+    expect(alert).toHaveTextContent(expectedError.message)
+    expect(screen.getByText(expectedError.startDateTime)).toBeInTheDocument()
+    expect(screen.getByText(expectedError.endDateTime)).toBeInTheDocument()
+    expect(screen.getByText(expectedError.type)).toBeInTheDocument()
+    expect(statusSelector.classList).toContain('is-invalid')
+    expect(screen.getByText(expectedError.reason)).toBeInTheDocument()
+    expect(screen.getByText(expectedError.location)).toBeInTheDocument()
   })
 })
