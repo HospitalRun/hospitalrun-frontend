6018c4a192fc8900cd2e1a7a3ba6fdc730897501
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-12-29 09:16:48 +0200

    Fix lint error

--- src/__tests__/patients/visits/VisitForm.test.tsx
+++ src/__tests__/patients/visits/VisitForm.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen, render, within, waitFor } from '@testing-library/react'
+import { fireEvent, screen, render, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { format } from 'date-fns'
 import React from 'react'
