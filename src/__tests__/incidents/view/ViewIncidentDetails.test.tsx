e707c77288ab004046fc592e32c3ccf95228a241
Emma Slight <emma.slight@icloud.com>
2021-01-09 16:10:08 +1300

    test(viewincidentdetails.test.tsx): finish improving test descriptions and tests
    Fixes #250

--- src/__tests__/incidents/view/ViewIncidentDetails.test.tsx
+++ src/__tests__/incidents/view/ViewIncidentDetails.test.tsx
@@ -23,6 +23,7 @@ describe('View Incident Details', () => {
     department: 'some department',
     description: 'some description',
     category: 'some category',
+    categoryItem: 'some categoryItem',
     status: 'reported',
     reportedBy: 'some user id',
     reportedOn: reportedDate.toISOString(),
@@ -52,110 +53,115 @@ describe('View Incident Details', () => {
     return { ...renderResults, history }
   }
 
-  describe('view details', () => {
-    it('should render the date of the incident', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident])
-      expect(
-        await screen.findByRole('heading', {
-          name: /incidents\.reports\.dateofincident/i,
-        }),
-      ).toBeInTheDocument()
+  describe('view incident details', () => {
+    describe('view incident details header', () => {
+      it('should render the date of the incident', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident])
+        expect(
+          await screen.findByRole('heading', {
+            name: /incidents\.reports\.dateofincident/i,
+          }),
+        ).toBeInTheDocument()
 
-      expect(
-        await screen.findByRole('heading', { name: /2020-06-01 07:48 PM/i }),
-      ).toBeInTheDocument()
+        expect(
+          await screen.findByRole('heading', { name: /2020-06-01 07:48 PM/i }),
+        ).toBeInTheDocument()
+      })
+
+      it('should render the status of the incident', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident])
+
+        expect(
+          await screen.findByRole('heading', {
+            name: /incidents\.reports\.status/i,
+          }),
+        ).toBeInTheDocument()
+
+        expect(await screen.findByRole('heading', { name: 'reported' })).toBeInTheDocument()
+      })
+
+      it('should render who reported the incident', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident])
+
+        expect(
+          await screen.findByRole('heading', {
+            name: /incidents\.reports\.reportedby/i,
+          }),
+        ).toBeInTheDocument()
+
+        expect(await screen.findByRole('heading', { name: 'some user id' }))
+      })
+      it('should render the date the incident was reported', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident])
+
+        expect(
+          await screen.findByRole('heading', {
+            name: /incidents\.reports\.reportedon/i,
+          }),
+        ).toBeInTheDocument()
+
+        expect(
+          await screen.findByRole('heading', { name: /2020-06-01 07:50 PM/i }),
+        ).toBeInTheDocument()
+      })
     })
+    describe('form elements should not be editable', () => {
+      it('should render the department input with label and display value', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident])
+        expect(await screen.findByText(/incidents\.reports\.department/i)).toBeInTheDocument()
 
-    it('should render the status of the incident', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident])
+        expect(
+          await screen.findByRole('textbox', { name: /incidents\.reports\.department/i }),
+        ).not.toBeEnabled()
 
-      expect(
-        await screen.findByRole('heading', {
-          name: /incidents\.reports\.status/i,
-        }),
-      ).toBeInTheDocument()
+        expect(
+          await screen.findByRole('textbox', { name: /incidents\.reports\.department/i }),
+        ).toHaveDisplayValue('some department')
+      })
 
-      expect(await screen.findByRole('heading', { name: 'reported' })).toBeInTheDocument()
+      it('should render the category input with label and display value', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident, Permissions.ResolveIncident])
+        expect(await screen.findByText(/incidents\.reports\.category$/i)).toBeInTheDocument()
+
+        expect(
+          await screen.findByRole('textbox', { name: /incidents\.reports\.category$/i }),
+        ).not.toBeEnabled()
+
+        expect(
+          await screen.findByRole('textbox', { name: /incidents\.reports\.category$/i }),
+        ).toHaveDisplayValue('some category')
+      })
+      it('should render the categoryItem input with label and display value', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident, Permissions.ResolveIncident])
+        expect(await screen.findByText(/incidents\.reports\.categoryItem$/i)).toBeInTheDocument()
+
+        expect(
+          await screen.findByRole('textbox', { name: /incidents\.reports\.categoryItem$/i }),
+        ).not.toBeEnabled()
+
+        expect(
+          await screen.findByRole('textbox', { name: /incidents\.reports\.categoryItem$/i }),
+        ).toHaveDisplayValue('some categoryItem')
+      })
+
+      it('should render the description input with label and display value', async () => {
+        setup(expectedIncident, [Permissions.ViewIncident, Permissions.ResolveIncident])
+        expect(
+          await screen.findByRole('textbox', {
+            name: /incidents\.reports\.description/i,
+          }),
+        ).toBeInTheDocument()
+        expect(
+          screen.getByRole('textbox', {
+            name: /incidents\.reports\.description/i,
+          }),
+        ).not.toBeEnabled()
+
+        expect(
+          await screen.findByRole('textbox', { name: /incidents\.reports\.description/i }),
+        ).toHaveDisplayValue('some description')
+      })
     })
-
-    it('should render who reported the incident', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident])
-
-      expect(
-        await screen.findByRole('heading', {
-          name: /incidents\.reports\.reportedby/i,
-        }),
-      ).toBeInTheDocument()
-
-      expect(await screen.findByRole('heading', { name: 'some user id' }))
-    })
-    it('should render the date the incident was reported', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident])
-
-      expect(
-        await screen.findByRole('heading', {
-          name: /incidents\.reports\.reportedon/i,
-        }),
-      ).toBeInTheDocument()
-
-      expect(
-        await screen.findByRole('heading', { name: /2020-06-01 07:50 PM/i }),
-      ).toBeInTheDocument()
-    })
-    test('type into department field', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident, Permissions.ResolveIncident])
-
-      expect(
-        await screen.findByRole('textbox', { name: /incidents\.reports\.department/i }),
-      ).toBeInTheDocument()
-      expect(
-        await screen.findByRole('textbox', { name: /incidents\.reports\.department/i }),
-      ).not.toBeEnabled()
-    })
-
-    test('type into category field', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident, Permissions.ResolveIncident])
-
-      expect(
-        await screen.findByRole('textbox', {
-          name: /incidents\.reports\.category\b/i,
-        }),
-      ).toBeInTheDocument()
-      expect(
-        screen.getByRole('textbox', {
-          name: /incidents\.reports\.category\b/i,
-        }),
-      ).not.toBeEnabled()
-    })
-
-    test('type into category item field', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident, Permissions.ResolveIncident])
-      expect(
-        await screen.findByRole('textbox', {
-          name: /incidents\.reports\.categoryitem/i,
-        }),
-      ).toBeInTheDocument()
-      expect(
-        screen.getByRole('textbox', {
-          name: /incidents\.reports\.categoryitem/i,
-        }),
-      ).not.toBeEnabled()
-    })
-
-    test('type into description field', async () => {
-      setup(expectedIncident, [Permissions.ViewIncident, Permissions.ResolveIncident])
-      expect(
-        await screen.findByRole('textbox', {
-          name: /incidents\.reports\.description/i,
-        }),
-      ).toBeInTheDocument()
-      expect(
-        screen.getByRole('textbox', {
-          name: /incidents\.reports\.description/i,
-        }),
-      ).not.toBeEnabled()
-    })
-
     describe('on resolve', () => {
       it('should mark the status as resolved and fill in the resolved date with the current time', async () => {
         const { history } = setup(expectedIncident, [
