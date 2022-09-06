17fb52c3f4a5ebb5450ed35432747cc9874ec799
Xhefri Bala <70955537+xhefribala@users.noreply.github.com>
2022-09-06 11:32:03 +1000

    fixed user login issue

--- src/incidents/hooks/useReportIncident.tsx
+++ src/incidents/hooks/useReportIncident.tsx
@@ -6,16 +6,18 @@ import IncidentRepository from '../../shared/db/IncidentRepository'
 import Incident from '../../shared/model/Incident'
 import validateIncident from '../util/validate-incident'
 
+
 const getIncidentCode = (): string => `I-${shortid.generate()}`
 
 export function reportIncident(incident: Incident): Promise<Incident> {
   const error = validateIncident(incident)
+  
   if (isEmpty(error)) {
     const updatedIncident: Incident = {
       ...incident,
       code: getIncidentCode(),
       status: 'reported',
-      reportedBy: 'some user',
+      //reportedBy: 'some user',
       reportedOn: new Date(Date.now()).toISOString(),
     }
     return IncidentRepository.save(updatedIncident)
